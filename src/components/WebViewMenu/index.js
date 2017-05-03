d57640db854d51cc69d8983f3f28a935b6385123
Sential <sentialx@gmail.com>
2017-05-03 16:17:20 +0200

    Fix stupid mistake
    Former-commit-id: 7231f0dc8e4053b550742cbc2f3d00e41c0f3169

--- src/components/WebViewMenu/index.js
+++ src/components/WebViewMenu/index.js
@@ -105,7 +105,7 @@ export default class WebViewMenu extends React.Component {
 
     function onRefreshClick () {
       const webview = self.props.getApp().getTabs().getSelectedTab().getPage().getWebView()
-      webview.goForward()
+      webview.reload()
     }
 
     return (
