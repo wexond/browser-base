9631c1227d641d4afe8118a664bcfa3cdbdb3c90
Sential <sentialx@gmail.com>
2017-05-06 20:27:42 +0200

    Fix errors with onClick in MenuItem
    Former-commit-id: 8fe9d0ba73b72dc823f8ca1eb35167b05daec974

--- src/components/MenuItem/index.js
+++ src/components/MenuItem/index.js
@@ -6,8 +6,15 @@ export default class MenuItem extends React.Component {
   }
 
   render () {
+    function onClick () {
+      this.props.getMenu().hide()
+      if (typeof this.props.onClick === 'function') {
+        this.props.onClick()
+      }
+    }
+
     return (
-      <div style={{display: (this.props.show) ? 'block' : 'none'}} className='menu-item' onClick={() => { this.props.getMenu().hide(); this.props.onClick() }}>
+      <div style={{display: (this.props.show) ? 'block' : 'none'}} className='menu-item' onClick={onClick}>
         <div className='menu-item-text'>
           {this.props.children}
         </div>
