b57edf62cb0aa308caa23017586efd3ae52243f7
Sential <sentialx@gmail.com>
2017-06-04 17:58:27 +0200

    Fix window maximizing

--- src/views/App/index.js
+++ src/views/App/index.js
@@ -1,6 +1,22 @@
 import '../../app.scss'
 import Tabs from '../../components/Tabs'
 
+const {remote, clipboard} = require('electron')
+const {dialog, nativeImage} = remote
+const fs = require('fs')
+const path = require('path')
+const homedir = require('os').homedir()
+
+const userData = path.join(homedir, '.wexond')
+
+const requiredFiles = [
+  {
+    path: 'history.json',
+    defaultContent: '[]'
+  }
+]
+
+window.currentWindow = remote.getCurrentWindow()
 window.tabs = []
 window.defaultTabOptions = {
   select: true,
