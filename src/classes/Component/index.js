7654bdee8a8d6eb68a4d3444ff66e72ee26f9693
Sential <sentialx@gmail.com>
2017-06-11 01:09:53 +0200

    Add JSX syntax for components rendering

--- src/classes/Component/index.js
+++ src/classes/Component/index.js
@@ -1,49 +1,65 @@
 export default class Component {
   _render (rootElement, props = null) {
     if (typeof this.beforeRender === 'function') this.beforeRender(props)
+
     let tempElements = this.render(props)
     this.elements = {
       root: rootElement
     }
     this._createElements(tempElements, rootElement)
+
     if (typeof this.afterRender === 'function') this.afterRender(props)
   }
 
   renderComponent (elements, rootElement) {
-    let tempElements = elements
     this._createElements(elements, rootElement)
   }
 
-  _createElement (tag, props, ref, parent) {
-    let el = document.createElement(tag)
-    Object.assign(el, props)
-    parent.appendChild(el)
-    
-    if (ref != null) {
-      this.elements[ref] = el
+  _createElements(elements, rootElement) {
+    let element
+    if (typeof elements === 'object') {
+      element = this._createElement(elements.elementName, elements.attributes, rootElement)
+    } else if (typeof elements === 'string') {
+      rootElement.innerHTML += elements
+      return
     }
 
-    return el
-  }
-
-  _createComponent (component, parent, props, ref) {
-    component._render(parent, props)
-    if (ref != null) {
-      this.elements[ref] = component
-    }
-  }
-
-  _createElements (element, domElement) {
-    if (element != null && element.children != null) {
-      for (var i = 0; i < element.children.length; i++) {
-        if (element.children[i].tag != null) {
-          let el = this._createElement(element.children[i].tag, element.children[i].props, element.children[i].ref, domElement)
-          this._createElements(element.children[i], el)
-        }
-        if (element.children[i].component != null) {
-          this._createComponent(element.children[i].component, domElement, element.children[i].props, element.children[i].ref)
+    if (elements.children != null) {
+      for (var i = 0; i < elements.children.length; i++) {
+        if (element.isDOM) {
+          this._createElements(elements.children[i], element.element)
         }
       }
+    }
+  }
+
+  _createElement (elementName, props, rootElement) {
+    if (typeof elementName === 'function') {
+      let component = new elementName()
+      component._render(rootElement, props)
+
+      if (props.ref != null) {
+        if (typeof props.ref === 'function') {
+          props.ref(component)
+        } else if (typeof props.ref === 'string') {
+          this.elements[props.ref] = component
+        }
+      }
+
+      return {element: component, isDOM: false}
+    } else {
+      let element = document.createElement(elementName)
+      Object.assign(element, props)
+      rootElement.appendChild(element)
+
+      if (props.ref != null) {
+        if (typeof props.ref === 'function') {
+          props.ref(element)
+        } else if (typeof props.ref === 'string') {
+          this.elements[props.ref] = element
+        }
+      }
+      return {element: element, isDOM: true}
     }
   }
 }
