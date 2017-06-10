8992954c97024a3cb37645e13fb8910810512fe8
Sential <sentialx@gmail.com>
2017-06-10 19:54:30 +0200

    Small code cleanuos
    Former-commit-id: e7d4ba0f693513c1ae11ed39f078e7afd40d287d

--- src/helpers/DOM/index.js
+++ src/helpers/DOM/index.js
@@ -1,29 +1,4 @@
 /**
- * Creates div element.
- * @param {Object} data 
- * @param {DOMElement} parent 
- */
-function div (data = {}, parent = null) {
-  let element = document.createElement('div')
-  Object.assign(element, data)
-  if (parent != null) parent.appendChild(element)
-  return element
-}
-
-/**
- * Creates element.
- * @param {String} type 
- * @param {Object} data 
- * @param {DOMElement} parent 
- */
-function createElement (type, data = {}, parent = null) {
-  let element = document.createElement(type)
-  Object.assign(element, data)
-  if (parent != null) parent.appendChild(element)
-  return element
-}
-
-/**
  * Changes style attribute or gets value of the attribute.
  * @param {Object | String} data
  * @param {*} value
@@ -62,17 +37,15 @@ Element.prototype.remove = function () {
   this.parentNode.removeChild(this)
 }
 
-class DOMHelper {
-  /**
-   * Loads an array of scripts URLs to document's body.
-   * @static
-   * @param {array}
-   */
-  static loadScripts (scripts) {
-    for (var i = 0; i < scripts.length; i++) {
-      var script = document.createElement('script')
-      script.src = scripts[i]
-      document.body.appendChild(script)
-    }
+/**
+* Loads an array of scripts URLs to document's body.
+* @static
+* @param {array}
+*/
+function loadScripts (scripts) {
+  for (var i = 0; i < scripts.length; i++) {
+    var script = document.createElement('script')
+    script.src = scripts[i]
+    document.body.appendChild(script)
   }
 }
