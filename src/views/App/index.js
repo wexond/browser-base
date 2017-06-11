2c7c8b4fabe76efd367b6cfe4c58c539850cae2f
Sential <sentialx@gmail.com>
2017-06-11 21:05:54 +0200

    Fix rendering framework bugs
    Former-commit-id: 6472b6d9fee339e886931709393984bfce774c06

--- src/views/App/index.js
+++ src/views/App/index.js
@@ -17,9 +17,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Tabs ref='tabs'>
-          <div ref='s'>e</div>
-        </Tabs>
+        <Tabs ref='tabs' />
         <div className='pages' ref='pages' />
       </div>
     )
