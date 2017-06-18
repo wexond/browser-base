035548a6374f14666113ad84e456404791eb75d2
Sential <sentialx@gmail.com>
2017-06-18 12:26:56 +0200

    Add bar icons
    Former-commit-id: c53bcdedd5690efe754e2d4d9aae5abd1899a017

---
+++ src/components/Bar/index.js
@@ -0,0 +1,22 @@
+import Component from '../../classes/Component'
+
+export default class Bar extends Component {
+  beforeRender () {
+
+  }
+
+  render () {
+    return (
+      <div className='bar'>
+        <div className='bar-icon bar-icon-back' />
+        <div className='bar-icon bar-icon-forward' />
+        <div className='bar-icon bar-icon-refresh' />
+        <div className='bar-icon bar-icon-menu' />
+      </div>
+    )
+  }
+
+  afterRender () {
+
+  }
+}
