15f290f0d8f2969f7bdedfa1dccb991b1fa2d1b2
xNerhu22 <xnerhu22@onet.pl>
2017-06-29 22:07:36 +0200

    When you click address bar then input shows

--- src/components/Bar/index.js
+++ src/components/Bar/index.js
@@ -2,7 +2,24 @@ import Component from '../../classes/Component'
 
 export default class Bar extends Component {
   beforeRender () {
+    this.isAddressbarBarToggled = false
+  }
 
+  onAddressBarClick = (e) => {
+    e.stopPropagation()
+    if (!this.isAddressbarBarToggled) {
+      this.toggleInput(true)
+    }
+  }
+
+  onFocus = (e) => {
+    this.elements.input.setSelectionRange(0, this.elements.input.value.length)
+  }
+
+  onKeyPress = (e) => {
+    if (e.which === 13) {
+      app.getSelectedTab().getWebView().loadURL(this.elements.input.value)
+    }
   }
 
   render () {
@@ -11,12 +28,12 @@ export default class Bar extends Component {
         <div className='bar-icon bar-icon-back' />
         <div className='bar-icon bar-icon-forward' />
         <div className='bar-icon bar-icon-refresh' />
-        
-        <div className='bar-addressbar'>
+        <div className='bar-addressbar' onClick={this.onAddressBarClick}>
           <div className='bar-addressbar-icon-info' />
           <div ref='title' className='bar-addressbar-title' />
           <div className='bar-addressbar-divider' />
           <div ref='shortUrl' className='bar-addressbar-shorturl' />
+          <input ref='input' onFocus={this.onFocus} onKeyPress={this.onKeyPress} className='bar-input' />
         </div>
 
         <div className='bar-icon bar-icon-menu' />
@@ -25,7 +42,12 @@ export default class Bar extends Component {
   }
 
   afterRender () {
+    const self = this
 
+    window.addEventListener('mouseup', function (e) {
+      self.elements.input.style.display = 'none'
+      self.isAddressbarBarToggled = false
+    })
   }
 
   setTitle (title) {
@@ -34,5 +56,21 @@ export default class Bar extends Component {
 
   setDomain (domain) {
     this.elements.shortUrl.textContent = domain
+  }
+
+  toggleInput (flag) {
+    const self = this
+
+    this.isAddressbarBarToggled = flag
+
+    this.elements.input.style.display = (flag) ? 'block' : 'none'
+
+    if (flag) {
+      this.elements.input.focus()
+    }
+  }
+
+  setURL (url) {
+    this.elements.input.value = url
   }
 }
--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -232,6 +232,7 @@ export default class Tabs extends Component {
       }
     })
     tabToSelect.select()
+    this.selectedTab = tabToSelect
   }
 
   /**
