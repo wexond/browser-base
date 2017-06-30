1e5469251c8ca020d5d656a441a62fd4093f478d
Sential <sentialx@gmail.com>
2017-06-30 17:46:40 +0200

    Add ContextMenu
    Former-commit-id: 19768db225724a5fd673bcbdeb8329db410f8bf4

--- src/classes/Component/index.js
+++ src/classes/Component/index.js
@@ -8,7 +8,16 @@ export default class Component {
 
     if (typeof this.beforeRender === 'function') this.beforeRender()
 
+    if (this.props != null) {
+      for (var key in this.defaultProps) {
+        if (this.props[key] == null) {
+          this.props[key] = this.defaultProps[key]
+        }
+      }
+    }
+
     let tempElements = this.render()
+
     UI.render(tempElements, parentElement, this)
 
     if (typeof this.afterRender === 'function') this.afterRender()
