71e9b87a1886b1c3a61fab9f9aefcf15787bb04f
Sential <sentialx@gmail.com>
2017-07-14 01:54:05 +0200

    Separate functions to different files (part 3)

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,3 +1,6 @@
+import Store from '../store'
+import { request as httpsRequest } from 'https' 
+
 export default class Network {
   /**
    * Requests URL and gets response.
@@ -63,5 +66,61 @@ export default class Network {
     }
 
     return hostname
+  }
+
+  static getCertificate (url, callback = null) {
+    const domain = Network.getDomain(url)
+
+    let certificateExists = false
+
+    for (var i = 0; i < Store.certificates.length; i++) {
+      if (Store.certificates[i].domain === domain) {
+        let certificate = Store.certificates[i].certificate
+        if (certificate.subject == null) return
+
+        const data = {
+          type: 'Secure',
+          title: certificate.subject.O,
+          country: certificate.subject.C
+        }
+        callback(data)
+
+        certificateExists = true
+      }
+    }
+
+    if (certificateExists) return
+
+    let options = {
+      host: domain,
+      port: 443,
+      method: 'GET'
+    }
+
+    let req = httpsRequest(options, (res) => {
+      let certificate = res.connection.getPeerCertificate()
+      if (certificate.subject == null) return
+
+      const data = {
+        type: 'Secure',
+        title: certificate.subject.O,
+        country: certificate.subject.C
+      }
+      callback(data)
+
+      Store.certificates.push({
+        domain: domain,
+        certificate: certificate
+      })
+    })
+
+    req.on('error', (e) => {
+      const data = {
+        type: url.startsWith('wexond') ? 'Wexond' : 'Normal'
+      }
+      callback(data)
+    })
+
+    req.end()
   }
 }
