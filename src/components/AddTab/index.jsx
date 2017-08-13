101543900ab98f24c835edc01fe2ba7008149f3a
Sential <sentialx@gmail.com>
2017-08-13 19:17:35 +0200

    Add animations to AddTab

--- src/components/AddTab/index.jsx
+++ src/components/AddTab/index.jsx
@@ -1,21 +1,43 @@
 import Component from 'inferno-component'
 
-import { defaultOptions } from '../../defaults/tabs'
+import { defaultOptions, transitions } from '../../defaults/tabs'
 
 import { addTab } from '../../actions/tabs'
 
 import Store from '../../store'
 import { connect } from 'inferno-mobx'
 
+import Transitions from '../../utils/transitions'
+
 @connect
 export default class AddTab extends Component {
+  constructor () {
+    super ()
+
+    this.state = {
+      animateLeft: true
+    }
+  }
+
   getWidth () {
     return this.addTab.offsetWidth
   }
 
   render () {
+    const {
+      animateLeft
+    } = this.state
+
+    let transition = ''
+
+    if (animateLeft) {
+      const newTransition = transitions.left.duration + 's' + ' left ' + transitions.left.easing 
+      transition = Transitions.appendTransition(transition, newTransition)
+    }
+
     const addTabStyle = {
-      left: Store.addTabLeft
+      left: Store.addTabLeft,
+      transition: transition
     }
 
     const onClick = e => {
