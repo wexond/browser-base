d5dc5381eba52108d50ab76c66361618f673ccae
Sential <sentialx@gmail.com>
2017-12-05 21:07:15 +0100

    Add suggestions design, small design fixes

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -4,6 +4,7 @@ import SystemBar from '../SystemBar'
 import Tabs from '../Tabs'
 import Pages from '../Pages'
 import Bar from '../Bar'
+import Suggestions from '../Suggestions'
 
 import Store from '../../store'
 
@@ -22,6 +23,7 @@ export default class App extends Component {
           <Tabs></Tabs>
         </SystemBar>
         <Bar ref={(r) => { this.bar = r }}></Bar>
+        <Suggestions></Suggestions>
         <Pages></Pages>
       </div>
     )
---
+++ src/defaults/wexond-urls.js
@@ -0,0 +1,3 @@
+export default {
+    newtab: "wexond://newtab"
+}
