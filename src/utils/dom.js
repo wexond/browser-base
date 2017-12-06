408b0fcbf45ca995b9c57e541a49e9f9f01a67cf
Sential <sentialx@gmail.com>
2017-12-06 16:09:27 +0100

    Remove redutant code
    Former-commit-id: 8510f0ec16884ef31fbe3c85a1ec5bdca5e5f961

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,23 +1,3 @@
-Element.prototype.setCSS = function (data) {
-  for (var key in data) {
-    if (key === 'top' || key === 'bottom' || key === 'left' || key === 'right' || key === 'width' || key === 'height' || key.indexOf('margin') !== -1) {
-      if (typeof data[key] === 'number') {
-        data[key] = data[key].toString() + 'px'
-      }
-    }
-  }
-  Object.assign(this.style, data)
-}
-
-Element.prototype.getCSS = function (attribute) {
-  if (this.currentStyle) return this.currentStyle[attribute]
-  return document.defaultView.getComputedStyle(this, null)[attribute]
-}
-
-Element.prototype.remove = function () {
-  this.parentNode.removeChild(this)
-}
-
 /**
  * Loads an array of scripts URLs to document's body.
  * @param {Array}
@@ -27,14 +7,5 @@ function loadScripts (scripts) {
     let script = document.createElement('script')
     script.src = scripts[i]
     document.body.appendChild(script)
-  }
-}
-
-Element.prototype.setAttributes = function (attributes) {
-  for (var key in attributes) {
-    const attribute = key
-    const value = attributes[key]
-
-    this.setAttribute(attribute, value)
   }
 }
