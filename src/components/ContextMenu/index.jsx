799873f9d4b860eb738fa20232b77039c8c10d99
xNerhu22 <xnerhu22@onet.pl>
2017-12-06 17:37:25 +0100

    Add basics of context-menu

---
+++ src/components/ContextMenu/index.jsx
@@ -0,0 +1,11 @@
+import Component from 'inferno-component'
+
+export default class ContextMenu extends Component {
+  render () {
+    return (
+      <div className='context-menu'>
+          
+      </div>
+    )
+  }
+}
