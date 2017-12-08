ca8809e428e326d9932a62b4cc44b5dc426eaf7d
Sential <sentialx@gmail.com>
2017-12-08 22:27:35 +0100

    Add saving history

---
+++ src/actions/files.js
@@ -0,0 +1,17 @@
+import { paths } from '../defaults/files'
+import fs from 'fs'
+
+export const checkFiles = () => {
+  for (var key in paths.directories) {
+    let path = paths.directories[key]
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path)
+    }
+  }
+  for (var key in paths.files) {
+    let path = paths.files[key]
+    if (!fs.existsSync(path)) {
+      fs.writeFileSync(path, '[]')
+    }
+  }
+}
