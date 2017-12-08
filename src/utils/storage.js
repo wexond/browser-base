6d7464005e5793bd7c2c6e713741acab4879db28
Sential <sentialx@gmail.com>
2017-12-08 22:27:35 +0100

    Add saving history

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,66 +1,60 @@
 import fs from 'fs'
+import { paths } from '../defaults/files'
 
 export default class Storage {
   static async addHistoryItem (title, url) {
-    return new Promise((resolve, reject) => {
-      if (title != null && url != null) {
-        // Get today's date.
-        let today = new Date()
-        let dd = today.getDate()
-        let mm = today.getMonth() + 1
-        let yyyy = today.getFullYear()
-  
-        if (dd < 10) {
-          dd = '0' + dd
-        }
-  
-        if (mm < 10) {
-          mm = '0' + mm
-        }
-  
-        today = mm + '-' + dd + '-' + yyyy
-  
-        let data = Storage.getHistory()
-  
-        if (!url.startsWith('wexond://') && !url.startsWith('about:blank')) {
-          // Get current time.
-          let date = new Date()
-          let currentHour = date.getHours()
-          let currentMinute = date.getMinutes()
-          let time = `${currentHour}:${currentMinute}`
-  
-          // Configure newItem's data.
-          let newItem = {
-            'url': url,
-            'title': title,
-            'date': today,
-            'time': time
-          }
-  
-          // Get newItem's new id.
-          if (data[data.length - 1] == null) {
-            newItem.id = 0
-          } else {
-            newItem.id = data[data.length - 1].id + 1
-          }
-  
-          // Push new history item.
-          data.push(newItem)
-  
-          try {
-            await Storage.saveHistory(data)
-            resolve()
-          } catch (error) {
-            reject(error)
-          }
-        }
+    if (title != null && url != null) {
+      // Get today's date.
+      let today = new Date()
+      let dd = today.getDate()
+      let mm = today.getMonth() + 1
+      let yyyy = today.getFullYear()
+
+      if (dd < 10) {
+        dd = '0' + dd
       }
-    }) 
+
+      if (mm < 10) {
+        mm = '0' + mm
+      }
+
+      today = mm + '-' + dd + '-' + yyyy
+
+      let data = await Storage.getHistory()
+
+      if (!url.startsWith('wexond://') && !url.startsWith('about:blank')) {
+        // Get current time.
+        let date = new Date()
+        let currentHour = date.getHours()
+        let currentMinute = date.getMinutes()
+        let time = `${currentHour}:${currentMinute}`
+
+        // Configure newItem's data.
+        let newItem = {
+          'url': url,
+          'title': title,
+          'date': today,
+          'time': time
+        }
+
+        // Get newItem's new id.
+        if (data[data.length - 1] == null) {
+          newItem.id = 0
+        } else {
+          newItem.id = data[data.length - 1].id + 1
+        }
+
+        // Push new history item.
+        data.push(newItem)
+
+        await Storage.saveHistory(data)
+      }
+    }
   }
 
   static async saveHistory (jsonObject) {
     return new Promise((resolve, reject) => {
-      fs.writeFile(global.historyPath, JSON.stringify(jsonObject), function (error) {
+      fs.writeFile(paths.files.history, JSON.stringify(jsonObject), function (error) {
         if (error) {
           reject(error)
         } else {
@@ -72,18 +66,17 @@ export default class Storage {
 
   static async resetHistory () {
     return new Promise((resolve, reject) => {
-      try {
-        await Storage.saveHistory([])
+      Storage.saveHistory([]).then(() => {
         resolve()
-      } catch (error) {
+      }).catch(() => {
         reject(error)
-      }
+      })
     })
   }
 
   static getHistory () {
     return new Promise((resolve, reject) => {
-      fs.readFile(global.historyPath, function (error, data) {
+      fs.readFile(paths.files.history, function (error, data) {
         if (error) {
           reject(error)
         } else {
