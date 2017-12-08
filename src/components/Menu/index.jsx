21888831ca0fb75aec342cc843613fa461a26531
Sential <sentialx@gmail.com>
2017-12-08 20:21:14 +0100

    Simplify show method in menu
    Former-commit-id: 85814d13747d6de74bf7cb07d0f999aa96dbf882

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -28,50 +28,23 @@ export default class Menu extends Component {
   }
 
   show = () => {
-    // Get all separators in menu, to determine
-    // new height of the menu with these separators.
-    let separators = this.menu.getElementsByClassName('separator')
-    let separatorsCount = 0
-
     // Remove height transition to quickly set height to 0.
     this.menu.style.transition = '0.27s margin-top, 0.2s opacity'
     this.setState({height: 0})
 
-    // Get amount of all visible separators.
-    for (var i = 0; i < separators.length; i++) {
-      if (separators[i].style.display === 'block') {
-        separatorsCount += 1
-      }
-    }
-
-    // Top padding is 16 and bottom too.
-    let topBottomPadding = 32
-    // Margin top and bottom of separators are 8.
-    let separatorsMargins = 16
-    // Initialize height by adding these values.
-    let height = separatorsCount + separatorsCount * separatorsMargins + topBottomPadding
-
-    // Get height of all visible items.
-    for (i = 0; i < this.items.length; i++) {
-      if (this.items[i].props.visible) {
-        // Each element has height of 32.
-        height += 32
-      }
-    }
-
     // Store the new calculated height,
     // to help with calculating new position of menu.
-    this.newHeight = height
+    this.newHeight = this.menu.scrollHeight
 
     // Need to use little timeout
     // for setting height and transitions.
     setTimeout(() => {
       // Revert height transition.
-      this.menu.style.transition = '0.3s height, 0.27s margin-top, 0.2s opacity'
+      this.menu.style.transition = '0.3s max-height, 0.27s margin-top, 0.2s opacity'
       this.setState({
         marginTop: 0,
         opacity: 1,
-        height: height,
+        height: this.newHeight,
         pointerEvents: true
       })
     }, 10)
@@ -97,7 +70,7 @@ export default class Menu extends Component {
     } = this.state
 
     const menuStyle = {
-      height: height,
+      maxHeight: height,
       marginTop: marginTop,
       opacity: opacity,
       left: left,
