8604868d9ee06b38d199bb273af586f32173fd55
Sential <sentialx@gmail.com>
2017-12-10 18:41:07 +0100

    Add navigation using arrow keys in suggestions

--- src/components/Suggestion/index.jsx
+++ src/components/Suggestion/index.jsx
@@ -3,6 +3,8 @@ import Component from 'inferno-component'
 import { observer } from 'inferno-mobx'
 import Store from '../../store'
 
+import { getSelectedPage } from '../../actions/pages'
+
 @observer
 export default class Suggestion extends Component {
   render () {
@@ -15,8 +17,19 @@ export default class Suggestion extends Component {
       dashDisplay = 'none'
     }
 
+    const onClick = (e) => {
+      const page = getSelectedPage()
+      if (this.props.title == null) {
+        page.page.webview.loadURL('https://www.google.com/search?q=' + this.props.url)
+      } else {
+        page.page.webview.loadURL(this.props.url)
+      }
+      this.props.hide()
+      Store.app.bar.addressBar.setInputToggled(false, true)
+    }
+
     return (
-      <div className='suggestion'>
+      <div onClick={onClick} className={'suggestion ' + ((this.props.selected) ? 'selected' : '')}>
         <div className='container'>
           <div className='favicon'>
 
