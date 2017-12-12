204cc5b2afe07c6f43b697629f5c0bada75822c0
xNerhu22 <xnerhu22@onet.pl>
2017-12-12 18:32:53 +0100

    Rewrite history layout to InfernoJS and SASS

---
+++ src/components/HistoryToolBar/index.jsx
@@ -0,0 +1,13 @@
+import Component from 'inferno-component'
+
+export default class HistoryToolBar extends Component {
+  render () {
+    return (
+      <div className='history-toolbar'>
+        <div class='title'>
+          History
+        </div>
+      </div>
+    )
+  }
+}
