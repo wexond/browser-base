449f1b3efea792efb6c89d9ec5fadc419d943216
xNerhu22 <xnerhu22@onet.pl>
2017-12-12 18:32:53 +0100

    Rewrite history layout to InfernoJS and SASS
    Former-commit-id: 204cc5b2afe07c6f43b697629f5c0bada75822c0

---
+++ src/components/HistoryCard/index.jsx
@@ -0,0 +1,30 @@
+import Component from 'inferno-component'
+
+export default class HistoryCard extends Component {
+  render () {
+    const screenshotStyle = {
+      backgroundImage: `url(${this.props.data.screenshot}`
+    }
+
+    const iconStyle = {
+      backgroundImage: `url(${this.props.data.icon}`
+    }
+
+    return (
+      <a href={this.props.data.url} class='history-card'>
+        <div class='screenshot' style={screenshotStyle} />
+        <div class='info-container'>
+          <div class='icon' style={iconStyle} />
+          <div class='title-container'>
+            <div class='title'>
+              {this.props.data.title}
+            </div>
+            <div class='description'>
+              {this.props.data.description}
+            </div>
+          </div>
+        </div>
+      </a>
+    )
+  }
+}
