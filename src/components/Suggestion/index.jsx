ff255a5f4bbb5768bb964518935872ad4da1d950
Sential <sentialx@gmail.com>
2017-12-12 16:08:19 +0100

    Improve suggestions
    Former-commit-id: e166cbdc2e32e989f4eb286538ab8d5692a4cf55

--- src/components/Suggestion/index.jsx
+++ src/components/Suggestion/index.jsx
@@ -8,24 +8,29 @@ import { getSelectedPage } from '../../actions/pages'
 @observer
 export default class Suggestion extends Component {
   render () {
-    let dashDisplay = 'block'
-    let titleContent = this.props.title
-    let urlContent = this.props.url
-    if (this.props.title == null) {
-      titleContent = this.props.url
-      urlContent = ''
-      dashDisplay = 'none'
-    }
+    let description = this.props.url
+    if (this.props.description != null) description = this.props.description
 
     const onClick = (e) => {
       const page = getSelectedPage()
-      if (this.props.title == null) {
-        page.page.webview.loadURL('https://www.google.com/search?q=' + this.props.url)
-      } else {
+      if (this.props.type === 'search') {
+        page.page.webview.loadURL('https://www.google.com/search?q=' + this.props.title)
+      } else if (this.props.type === 'history') {
         page.page.webview.loadURL(this.props.url)
+      } else if (this.props.type === 'first-url') {
+        page.page.webview.loadURL(this.props.title)
+      } else if (this.props.type === 'first-search') {
+        page.page.webview.loadURL('https://www.google.com/search?q=' + this.props.title)
       }
+
       this.props.hide()
       Store.app.bar.addressBar.setInputToggled(false, true)
+    }
+
+    let dashDisplay = 'block'
+    
+    if (this.props.url == null && this.props.description == null) {
+      dashDisplay = 'none'
     }
 
     return (
@@ -35,13 +40,13 @@ export default class Suggestion extends Component {
 
           </div>
           <div className='title'>
-            {titleContent}
+            {this.props.title}
           </div>
           <div className='dash' style={{display: dashDisplay}}>
             &mdash;
           </div>
           <div className='address'>
-            {urlContent}
+            {description}
           </div>
         </div>
       </div>
