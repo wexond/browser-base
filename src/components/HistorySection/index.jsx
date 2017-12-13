bc3fc572c56db4dd9188ed45e9d3c05e4ba22e6f
xNerhu22 <xnerhu22@onet.pl>
2017-12-13 09:45:17 +0100

    Move cards list to main history component

---
+++ src/components/HistorySection/index.jsx
@@ -0,0 +1,15 @@
+import Component from 'inferno-component'
+
+export default class HistorySection extends Component {
+  render () {
+    const cardsContainerStyle = {
+      maxWidth: this.state.cardsContainerWidth
+    }
+
+    return (
+      <div>
+        x
+      </div>
+    )
+  }
+}
