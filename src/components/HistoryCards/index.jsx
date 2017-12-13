c06cd168875341d500f6eb954f81271d3ea0e4b2
xNerhu22 <xnerhu22@onet.pl>
2017-12-13 09:45:17 +0100

    Move cards list to main history component
    Former-commit-id: aa5d8532d93b5243fefac2b1445022b84f41c3d8

--- src/components/HistoryCards/index.jsx
+++ src/components/HistoryCards/index.jsx
@@ -7,50 +7,6 @@ export default class HistoryCards extends Component {
     super()
 
     this.state = {
-      cards: [
-        {
-          url: 'https://www.github.com',
-          title: 'GitHub',
-          description: 'Build for developers',
-          screenshot: 'https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-9/24899862_2050574158507064_8530629417294747664_n.jpg?oh=eba9b82613dacc8a41e99d63ca32c845&oe=5ACD27C8',
-          icon: 'https://image.flaticon.com/icons/svg/25/25231.svg',
-        },
-        {
-          url: 'https://www.facebook.com',
-          title: 'Facebook',
-          description: '',
-          screenshot: 'http://cdn.ilovefreesoftware.com/wp-content/uploads/2013/06/facebookdesktopversion.png',
-          icon: 'https://image.flaticon.com/icons/png/512/124/124010.png',
-        },
-        {
-          url: 'https://www.youtube.com',
-          title: 'YouTube',
-          description: '',
-          screenshot: 'https://www.mhthemes.com/support/files/2015/10/YouTube.png',
-          icon: 'https://cdn1.iconfinder.com/data/icons/logotypes/32/youtube-256.png',
-        },
-        {
-          url: 'https://www.github.com',
-          title: 'GitHub',
-          description: 'Build for developers',
-          screenshot: 'https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-9/24899862_2050574158507064_8530629417294747664_n.jpg?oh=eba9b82613dacc8a41e99d63ca32c845&oe=5ACD27C8',
-          icon: 'https://image.flaticon.com/icons/svg/25/25231.svg',
-        },
-        {
-          url: 'https://www.facebook.com',
-          title: 'Facebook',
-          description: '',
-          screenshot: 'http://cdn.ilovefreesoftware.com/wp-content/uploads/2013/06/facebookdesktopversion.png',
-          icon: 'https://image.flaticon.com/icons/png/512/124/124010.png',
-        },
-        {
-          url: 'https://www.youtube.com',
-          title: 'YouTube',
-          description: '',
-          screenshot: 'https://www.mhthemes.com/support/files/2015/10/YouTube.png',
-          icon: 'https://cdn1.iconfinder.com/data/icons/logotypes/32/youtube-256.png',
-        }
-      ],
       cardsContainerWidth: 256
     }
   }
@@ -95,7 +51,7 @@ export default class HistoryCards extends Component {
     return (
         <div class='cards-container' style={cardsContainerStyle}>
           {
-            this.state.cards.map((data, key) => {
+            this.props.items.map((data, key) => {
               return <Card data={data} key={key} />
             })
           }
@@ -106,5 +62,6 @@ export default class HistoryCards extends Component {
 
 HistoryCards.defaultProps = {
   cardWidth: 256,
-  cardMargin: 32
+  cardMargin: 32,
+  items: []
 }
