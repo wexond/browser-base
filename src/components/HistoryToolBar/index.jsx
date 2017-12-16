3d7d0b2df987be1d028b6f9836eef29a3e8dce2a
xNerhu22 <xnerhu22@onet.pl>
2017-12-17 00:15:28 +0100

    Add items selecting in history (Part 1)
    Former-commit-id: 399adae9f4dc2b3f2fbc77cef6401ea425aca76f

--- src/components/HistoryToolBar/index.jsx
+++ src/components/HistoryToolBar/index.jsx
@@ -1,11 +1,65 @@
 import React from 'react'
 
 export default class HistoryToolBar extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      titleOpacity: 1,
+      containerDisplay: false,
+      containerOpacity: 0
+    }
+  }
+
+  toggle = (flag) => {
+    if (flag) {
+      this.setState({titleOpacity: 0, containerDisplay: true})
+
+      setTimeout(() => {
+        this.setState({containerOpacity: 1})
+      }, 10)
+    } else {
+      this.setState({titleOpacity: 1, containerOpacity: 0})
+
+      setTimeout(() => {
+        this.setState({containerDisplay: false})
+      }, 150)
+    }
+  }
+
+  componentWillReceiveProps () {
+    this.toggle(this.props.selectedUrls.length > 0)
+  }
+
   render () {
+    const titleStyle = {
+      opacity: this.state.titleOpacity
+    }
+
+    const containerStyle = {
+      display: !this.state.containerDisplay ? 'none' : 'block',
+      opacity: this.state.containerOpacity
+    }
+
     return (
       <div className='history-toolbar'>
-        <div className='title'>
+        <div className='title' style={titleStyle}>
           History
+        </div>
+        <div className='controls-container' style={containerStyle}>
+          <div className='exit-icon' onClick={this.props.onExitIconClick} />
+          <div className='selected-items'>
+            Selected items:
+          </div>
+          <div className='count'>
+            {this.props.selectedUrls.length}
+          </div>
+          <div className='delete-button'>
+            delete
+          </div>
+          <div className='cancel-button'>
+            cancel
+          </div>
         </div>
       </div>
     )
