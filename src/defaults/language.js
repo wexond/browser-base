a20ee3c44075b842d382e00ecaaa18fa649eed65
xNerhu22 <xnerhu22@onet.pl>
2017-12-16 14:26:52 +0100

    Connect history to preload API

---
+++ src/defaults/language.js
@@ -0,0 +1,27 @@
+module.exports = {
+  english: {
+    weekDays: [
+      'sunday',
+      'monday',
+      'tuesday',
+      'wednesday',
+      'thursday',
+      'friday',
+      'saturday'
+    ],
+    months: [
+      'january',
+      'february',
+      'march',
+      'april',
+      'may',
+      'june',
+      'july',
+      'august',
+      'september',
+      'october',
+      'november',
+      'december'
+    ]
+  }
+}
