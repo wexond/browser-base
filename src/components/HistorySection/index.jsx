86ec05f758caf2f1be1a885fc898530cd9ba5d53
Sential <sentialx@gmail.com>
2017-12-17 15:03:31 +0100

    Add checkboxes to history sections
    Former-commit-id: 73f8374abb2b4b832c29f4ea1a262466acf329ea

--- src/components/HistorySection/index.jsx
+++ src/components/HistorySection/index.jsx
@@ -1,17 +1,48 @@
 import React from 'react'
 
+import Checkbox from '../Checkbox'
 import Item from '../HistoryItem'
 
+import Store from '../../history-store'
+import { observer } from 'mobx-react'
+
+@observer
 export default class HistorySection extends React.Component {
+  constructor () {
+    super()
+
+    this.items = []
+  }
+
   render () {
+    const onCheck = (flag) => {
+      console.log(this.items)
+      for (var i = 0; i < this.items.length; i++) {
+        const data = this.items[i].props.data
+        data.checkbox = this.items[i].checkbox
+        if (flag) {
+          data.checkbox.setState({checked: true})
+          Store.selectedItems.push(data)
+        } else {
+          data.checkbox.setState({checked: false})
+          Store.selectedItems.splice(Store.selectedItems.indexOf(data), 1)
+        }
+      }
+    }
+
+    this.items = []
+
     return (
       <div className='history-section'>
         <div className='subheader'>
+          <div className='section-checkbox'>
+            <Checkbox ref={(r) => { this.checkbox = r }} onCheck={onCheck} />
+          </div>
           {this.props.data.title}
         </div>
         {
           this.props.data.items.map((data, key) => {
-            return <Item data={data} key={key} />
+            return <Item ref={(r) => { this.items.push(r) }} data={data} key={key} section={this} />
           })
         }
       </div>
