3068d70bbee3c393d19d6154ec9b9ef35f68a799
Sential <sentialx@gmail.com>
2017-12-17 14:23:04 +0100

    Code cleanup in history
    Former-commit-id: f12bfbc846b145f7d2cd76a35f3b3cafdb1d6eb0

--- src/components/History/index.jsx
+++ src/components/History/index.jsx
@@ -7,45 +7,37 @@ import HistoryCards from '../HistoryCards'
 import HistorySection from '../HistorySection'
 
 import Store from '../../history-store'
+import { observer } from 'mobx-react'
 
+@observer
 export default class History extends React.Component {
   constructor () {
     super()
-
-    this.state = {
-      cards: [],
-      sections: [],
-      selectedUrls: [],
-      selectingMode: false
-    }
-
-    this.selectedCheckboxes = []
   }
 
   componentDidMount () {
     Store.history = this
+
     this.loadData()
   }
 
   async loadData () {
     const history = await window.historyAPI.get()
 
-    this.setState({
-      cards: HistoryParser.getCards(history),
-      sections: HistoryParser.getSections(history)
-    })
+    Store.cards = HistoryParser.getCards(history)
+    Store.sections = HistoryParser.getSections(history)
   }
 
   render () {
     return (
       <div className='history'>
-        <ToolBar selectingMode={this.state.selectingMode} selectedUrls={this.state.selectedUrls} onExitIconClick={this.onToolbarExitIconClick} />
+        <ToolBar />
         <div className='content'>
-          <HistoryCards items={this.state.cards} />
+          <HistoryCards />
           {
-            this.state.sections.map((data, key) => {
+            Store.sections.map((data, key) => {
               if (data.items.length > 0) {
-                return <HistorySection data={data} key={key} onItemSelect={this.onItemSelect} />
+                return <HistorySection data={data} key={key} />
               }
             })
           }
