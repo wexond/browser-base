a8659782209b200db59f0fd4f90081f7f5c4a3e0
Sential <sentialx@gmail.com>
2017-12-17 13:54:48 +0100

    Add security for history API in webview's preload

--- src/preloads/history.js
+++ src/preloads/history.js
@@ -1,38 +1,40 @@
 const fs = require('fs')
 const paths = require('../defaults/files')
 
-window.historyAPI = {
-	get: () => {
-		return new Promise((resolve, reject) => {
-      fs.readFile(paths.files.history, (error, data) => {
-        if (error) {
-          reject(error)
-        } else {
-          resolve(JSON.parse(data))
-        }
+if (window.location.protocol === 'wexond:') {
+  window.historyAPI = {
+    get: () => {
+      return new Promise((resolve, reject) => {
+        fs.readFile(paths.files.history, (error, data) => {
+          if (error) {
+            reject(error)
+          } else {
+            resolve(JSON.parse(data))
+          }
+        })
       })
-    })
-  },
-  delete: async (item) => {
-    const data = await window.historyAPI.get()
-
-    for (var i = 0; i < data.length; i++) {
-      if (data[i].id === item.id) {
-        data.splice(i, 1)
-        await window.historyAPI.save(data)
-        break
+    },
+    delete: async (item) => {
+      const data = await window.historyAPI.get()
+  
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].id === item.id) {
+          data.splice(i, 1)
+          await window.historyAPI.save(data)
+          break
+        }
       }
-    }
-  },
-  save: (data) => {
-    return new Promise((resolve, reject) => {
-      fs.writeFile(paths.files.history, JSON.stringify(data), (error) => {
-        if (error) {
-          reject(error)
-        } else {
-          resolve()
-        }
+    },
+    save: (data) => {
+      return new Promise((resolve, reject) => {
+        fs.writeFile(paths.files.history, JSON.stringify(data), (error) => {
+          if (error) {
+            reject(error)
+          } else {
+            resolve()
+          }
+        })
       })
-    })
-  }
+    }
+  }  
 }
