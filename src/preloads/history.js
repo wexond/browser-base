6ed84f093708a4e461a81287147c1f45f5655b16
Sential <sentialx@gmail.com>
2017-12-17 14:10:42 +0100

    Fix history items deleting

--- src/preloads/history.js
+++ src/preloads/history.js
@@ -14,16 +14,18 @@ if (window.location.protocol === 'wexond:') {
         })
       })
     },
-    delete: async (item) => {
+    delete: async (items) => {
       const data = await window.historyAPI.get()
   
-      for (var i = 0; i < data.length; i++) {
-        if (data[i].id === item.id) {
-          data.splice(i, 1)
-          await window.historyAPI.save(data)
-          break
+      for (var x = data.length; x >= 0; x--) {
+        for (var y = 0; y < items.length; y++) {
+          if (data[x].id === items[y].id) {
+            data.splice(data.indexOf(data[x]), 1)
+          }
         }
       }
+
+      await window.historyAPI.save(data)
     },
     save: (data) => {
       return new Promise((resolve, reject) => {
