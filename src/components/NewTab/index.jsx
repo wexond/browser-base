5bfa20882c87f7f74897cdad94cbbe248efb38fc
xNerhu22 <xnerhu22@onet.pl>
2017-12-20 13:27:29 +0100

    Add cards to new tab

--- src/components/NewTab/index.jsx
+++ src/components/NewTab/index.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 
+import HistoryParser from '../../utils/history'
+import HistoryCards from '../HistoryCards'
+
+import Store from '../../history-store'
+import { observer } from 'mobx-react'
+
 export default class NewTab extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      cards: []
+    }
+  }
+
+  componentDidMount () {
+    this.loadData()
+  }
+
+  async loadData () {
+    const history = await window.historyAPI.get()
+
+    Store.cards = HistoryParser.getCards(history)
+  }
+
   render () {
     return (
       <div className='new-tab'>
-        New Tab
+        <HistoryCards />
       </div>
     )
   }
