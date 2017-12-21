41248c48d33c91393322ba6c6346552af6c623cf
Sential <sentialx@gmail.com>
2017-12-21 23:04:25 +0100

    Add extensions loading system

--- src/actions/webview.js
+++ src/actions/webview.js
@@ -1,5 +1,18 @@
 import Colors from '../utils/colors'
 
+export const getBarBorder = (webview) => {
+  return new Promise((resolve, reject) => {
+    if (webview != null && webview.getWebContents() != null) {
+      webview.executeJavaScript('(function () { return document.documentElement.innerHTML })()', false, async (result) => {
+        const regexp = /<meta name='?.bar-border'?.* content='?.false'?.*>/
+        resolve(!regexp.test(result))
+      })
+    } else {
+      reject(new Error('WebContents are not available'))
+    }
+  })
+}
+
 export const getColorFromTop = (webview) => {
   return new Promise((resolve, reject) => {
     if (webview != null && webview.getWebContents() != null) {
