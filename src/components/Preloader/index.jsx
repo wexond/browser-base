6c67856320a4db61b99d749fd912c67753db4394
xNerhu22 <xnerhu22@onet.pl>
2017-12-22 14:44:23 +0100

    Some fixes in history

--- src/components/Preloader/index.jsx
+++ src/components/Preloader/index.jsx
@@ -5,7 +5,7 @@ export default class Preloader extends React.Component {
     return (
       <div className='preloader' style={this.props.style}>
         <svg className='preloader-indeterminate' viewBox='25 25 50 50'>
-          <circle className='path' cx='50' cy='50' r='20' fill='none' stroke-miterlimit='10' />
+          <circle className='path' cx='50' cy='50' r='20' fill='none' strokeMiterlimit='10' />
         </svg>
       </div>
     )
