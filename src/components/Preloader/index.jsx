bcf9ecba6544a9f5813399ae63bb969714853dd2
xNerhu22 <xnerhu22@onet.pl>
2017-12-22 14:17:55 +0100

    Add preloader as new component, add preloader to history

---
+++ src/components/Preloader/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Preloader extends React.Component {
+  render () {
+    return (
+      <div className='preloader' style={this.props.style}>
+        <svg className='preloader-indeterminate' viewBox='25 25 50 50'>
+          <circle className='path' cx='50' cy='50' r='20' fill='none' stroke-miterlimit='10' />
+        </svg>
+      </div>
+    )
+  }
+}
