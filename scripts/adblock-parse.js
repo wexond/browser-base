7eb26aea85fed167312abbc7590c94e2171cbb94
Sential <sentialx@gmail.com>
2017-12-23 22:54:00 +0100

    Download filter lists, and parse them using npm run adblock:parse

--- scripts/adblock-parse.js
+++ scripts/adblock-parse.js
@@ -1,31 +1,96 @@
+const chalk = require('chalk')
 const { AdBlockClient, FilterOptions } = require('ad-block')
-const { app } = require('electron')
 const path = require('path')
 const fs = require('fs')
+const http = require('http')
+const https = require('https')
+const url = require('url')
 
-const lists = path.resolve(__dirname, './lists')
+const filterListsPath = path.resolve(__dirname, 'filter-lists.json')
 const output = path.resolve(__dirname, '../detector.buffer')
+
 const client = new AdBlockClient()
 
-fs.readdir(lists, (err, files) => {
-  if (err) return console.error(err)
+const filterLists = fs.readFileSync(filterListsPath, 'utf8')
+const filterListsJson = JSON.parse(filterLists)
 
-  for (var i in files) {
-    console.log('Parsing ' + files[i] + '...')
+let parsedLists = 0
+let listsToParse = 0
 
-    const data = fs.readFileSync(path.resolve(lists, files[i]), 'utf8')
-    data.split('\n').forEach(line => client.parse(line))
+const requestURL = (urlToList, callback) => {
+  let request
 
-    console.log('Parsed ' + files[i])
+  if (urlToList.startsWith('http://')) {
+    request = http.request(url.parse(urlToList), callback)
+  } else if (urlToList.startsWith('https://')) {
+    request = https.request(url.parse(urlToList), callback)
+  } else {
+    request = http.request(url.parse(urlToList), callback)
   }
 
-  const buffer = client.serialize(64)
-    
-  fs.writeFile(output, buffer, (err) => {
-    if (err) console.log(err)
-
-    console.log('Saved to detector.buffer.')
-
-    process.exit()
+  request.on('error', (e) => {
+    listsToParse--
   })
-})
+
+  request.end()
+}
+
+console.log(chalk.gray('[INFO] ') + 'Getting lists to download and parse...')
+
+for (let urlToList in filterListsJson) {
+  listsToParse++
+}
+
+console.log(listsToParse)
+
+for (let urlToList in filterListsJson) {
+  console.log(chalk.gray('[INFO] ') + 'Downloading ' + filterListsJson[urlToList].title + '...')
+
+  const onListReceive = (list) => {
+    console.log(parsedLists + '/' + listsToParse)
+
+    console.log(chalk.green('[SUCCESS] ') + 'Downloaded ' + filterListsJson[urlToList].title)
+    console.log(chalk.gray('[INFO] ') + 'Parsing ' + filterListsJson[urlToList].title + '...')
+
+    list.split('\n').forEach(line => client.parse(line))
+
+    console.log(chalk.green('[SUCCESS] ') + 'Parsed ' + filterListsJson[urlToList].title)
+    
+    parsedLists++
+
+    const buffer = client.serialize(64)
+    
+    console.log(chalk.gray('[INFO] ') + 'Saving to detector.buffer...')
+    console.log('')
+    fs.writeFile(output, buffer, (err) => {
+      if (err) return console.error(err)
+    })
+  }
+
+  const onListRequest = (res) => {
+    let data = ''
+    res.setEncoding('utf8')
+    res.on('data', (chunk) => {
+      data += chunk
+    })
+    res.on('end', () => {
+      onListReceive(data)
+    })
+  }
+
+  let request
+
+  if (urlToList.startsWith('http://')) {
+    request = http.request(url.parse(urlToList), onListRequest)
+  } else if (urlToList.startsWith('https://')) {
+    request = https.request(url.parse(urlToList), onListRequest)
+  } else {
+    request = http.request(url.parse(urlToList), onListRequest)
+  }
+
+  request.on('error', (e) => {
+    
+  })
+
+  request.end()
+}
