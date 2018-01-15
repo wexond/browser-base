4bcbd65af3d06c811d30724e13ccc3582db5a6e3
Mikołaj Palkiewicz <xnerhu22@onet.pl>
2018-01-15 18:49:40 +0100

    Some code cleanup in new tab
    Former-commit-id: 62d37deb111aaa2543353f2deb44028fcba8baa3 [formerly 62d37deb111aaa2543353f2deb44028fcba8baa3 [formerly 64681291f206d7cac648b5042620b05c9572a71a]]
    Former-commit-id: 92a41a94ed311f04329a78813a285b2a3236270c
    Former-commit-id: 93ad9cb75bd7e1730dc66e0a8732c83d26b6dced

--- src/components/NewTab/index.jsx
+++ src/components/NewTab/index.jsx
@@ -27,88 +27,10 @@ export default class NewTab extends React.Component {
     this.loadData()
   }
 
-  getCountryCode = async () => {
-    return new Promise(
-      (resolve, reject) => {
-        const req = https.get(url.parse('https://www.freegeoip.net/json/'), (res) => {
-          let data = ''
-
-          res.on('data', (d) => {
-            data += d
-          })
-
-          res.on('end', () => {
-            resolve(JSON.parse(data).country_code)
-          })
-        })
-
-        req.end()
-      }
-    )
-  }
-
-  getNews = (country = 'us') => {
-    const newsURL = `https://newsapi.org/v2/top-headlines?country=${country}`
-
-    return new Promise(
-      (resolve, reject) => {
-        let options = {
-          headers: {
-            'X-Api-Key': '113af42f31a5472187e0b85ce398994d'
-          }
-        }
-
-        Object.assign(options, url.parse(newsURL))
-
-        const req = https.get(options, (res) => {
-          let data = ''
-
-          res.on('data', (d) => {
-            data += d
-          })
-
-          res.on('end', () => {
-            resolve(JSON.parse(data))
-          })
-        })
-
-        req.end()
-      }
-    )
-  }
-  
-  loadPicture = (url) => {
-    return new Promise(
-      (resolve, reject) => {
-        const img = new Image()
-
-        img.onload = () => {
-          resolve()
-        }
-
-        img.onerror = (e) => {
-          reject(e)
-        }
-
-        img.src = url
-      }
-    )
-  }
-
-  loadPictures = async (news) => {
-    for (var i = 0; i < news.length; i++) {
-      await this.loadPicture(news[i].urlToImage)
-    }
-  }
-
   async loadData() {
     Store.loading = true
 
-    const countryCode = await this.getCountryCode()
-    const data = await this.getNews(countryCode)
-    const news = NewTabHelper.getNews(data)
-
-    await this.loadPictures(news)
+    const news = await NewTabHelper.getNews()
 
     Store.news = news
     Store.loading = false
