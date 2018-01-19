bd17c02e74fdb1e47a9174007ca0238b328516d3
Mikołaj Palkiewicz <xnerhu22@onet.pl>
2018-01-19 16:23:49 +0100

    Move bootstraps to diffrent directory

--- src/bootstraps/history.jsx
+++ src/bootstraps/history.jsx
@@ -1,9 +1,9 @@
-import './history.scss'
+import '../history.scss'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import History from './components/History'
+import History from '../components/History'
 
 // Wait for sass load.
 setTimeout(function () {
