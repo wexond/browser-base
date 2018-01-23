ddda4fd56d13efed1bda487e27b5d05447611c72
Sential <sentialx@gmail.com>
2018-01-23 22:21:16 +0100

    Add custom EventEmitter for extensions API, add onCommited event
    Former-commit-id: f01edf153a1bb06441e8715ccb96759b50cc6967 [formerly f1dad8cb67097ca06332e7075808f8cee8efd434]
    Former-commit-id: 1c2c511a699b9c654ff265e62519ef336188d2de

--- src/components/BackgroundExtension/index.jsx
+++ src/components/BackgroundExtension/index.jsx
@@ -3,10 +3,22 @@ import React from 'react'
 import { observer } from 'mobx-react'
 import Store from '../../stores/store'
 
+import ipcMessages from '../../defaults/ipc-messages'
+
 @observer
 export default class BackgroundExtensions extends React.Component {
   componentDidMount () {
+    this.props.data.backgroundExtension = this
 
+    this.webview.addEventListener('dom-ready', (e) => {
+      this.webview.openDevTools()
+    })
+
+    this.webview.addEventListener('ipc-message', (e) => {
+      if (e.channel === ipcMessages.EXTENSION_RELOAD) {
+        this.webview.reload()
+      }
+    })
   }
 
   render () {
