6baa30a646c49f3972aff1c0d5369d16a1a0708d
Sential <sentialx@gmail.com>
2018-01-24 22:55:08 +0100

    Fix production build errors again
    Former-commit-id: 705197efc68cd0724b49cfcbc6f98810f286854f [formerly 705197efc68cd0724b49cfcbc6f98810f286854f [formerly c88a3135aa1b7a2c8799db4ade0053d383bdf132]]
    Former-commit-id: 761fc5c266e72467b5764a6c6a9c76958aa2c1d0
    Former-commit-id: 1b98968dc20d0304ed6c5414e05b5bf7bbf61b7d

--- src/preloads/language.js
+++ src/preloads/language.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const { remote } = require('electron')
 
 if (window.location.protocol === 'wexond:') {
   window.env = process.env.NODE_ENV
@@ -7,7 +8,7 @@ if (window.location.protocol === 'wexond:') {
   window.dictionaryAPI = {
     get: (dname = 'english_US') => {
       try {
-        const json = JSON.parse(fs.readFileSync(path.resolve(process.env.dirname, `dictionaries/${dname}.json`)))
+        const json = JSON.parse(fs.readFileSync(path.resolve(remote.getGlobal('shared').dirname, `dictionaries/${dname}.json`)))
     
         return json
       } catch (e) {
