21ed7b8e6a1fcfdbdd042cfa1a4fa2b759413832
Sential <sentialx@gmail.com>
2018-01-24 22:41:15 +0100

    Fix production build issues
    Former-commit-id: 6173560d186d25900957b112c397dcd91abfa904 [formerly 6173560d186d25900957b112c397dcd91abfa904 [formerly 063cf7db443b2ae97ed47cf02f401784495d6737]]
    Former-commit-id: ec412cdfd56c1868f3c3a1e5f1951effa1deaf52
    Former-commit-id: 52da6effa5bf04fcd9613f85183c6a33173905de

--- src/actions/language.js
+++ src/actions/language.js
@@ -1,10 +1,11 @@
 import fs from 'fs'
+import path from 'path'
 
 import Store from '../stores/store'
 
 export const loadDictionary = (dname = 'english_US') => {
   try {
-    const json = JSON.parse(fs.readFileSync(`./dictionaries/${dname}.json`))
+    const json = JSON.parse(fs.readFileSync(path.resolve(process.env.dirname, `dictionaries/${dname}.json`)))
 
     Store.dictionary = json
   } catch (e) {
