d1d1f50fa7185359fd5078a47c458480518e2ea0
Sential <sentialx@gmail.com>
2018-02-03 14:35:09 +0100

    Change project structure
    Former-commit-id: d289c28db01b8451c3b39bc2a5a07bd7e3172336 [formerly d289c28db01b8451c3b39bc2a5a07bd7e3172336 [formerly 808098b5f9217fa909253663a22b0aeeb4f9e5f7]]
    Former-commit-id: a544c7aa3597e235bdcf25d61faf692cd653286e
    Former-commit-id: be16a6c3680b6fd61bf64155031c45f6a33cccf0

---
+++ src/components/App/FindMenu/index.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+import { observer } from 'mobx-react'
+import Store from '../../../stores/store'
+
+import Storage from '../../../utils/storage'
+import Colors from '../../../utils/colors'
+
+import * as filesActions from '../../../actions/files'
+import * as tabsActions from '../../../actions/tabs'
+import * as webviewActions from '../../../actions/webview'
+
+import Input from '../../Input'
+
+@observer
+export default class FindMenu extends React.Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  
+
+  last() {
+    this.props.webview.findInPage(input.value, {
+      forward: false,
+      findNext: true
+    })
+  }
+
+  next() {
+    this.props.webview.findInPage(input.value, {
+      forward: true,
+      findNext: true
+    })
+  }
+
+  toggle() {
+    const { webview } = this.props
+    const { isOpen } = this.state
+
+    if (isOpen) {
+      webview.stopFindInPage("clearSelection")
+      // hide with animation 
+    } else {
+      // show with animation
+      this.input.focus()
+    }
+
+    this.setState({
+      isOpen: !isOpen
+    })
+  }
+
+  render() {
+    const {
+      webview
+    } = this.props
+
+    const onInput = (e) => {
+      webview.findInPage(e.currentTarget.value)
+    }
+
+    const inputEvents = {
+      onInput: onInput
+    }
+
+    return (
+      <div>
+        <Input ref={ (r) => this.input = r } placeholder='Find in page' focused={true} {...inputEvents} />
+      </div>
+    )
+  }
+}
---
+++ src/components/App/SystemBar/index.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { platform } from 'os'
+
+import Controls from '../Controls'
+
+import Store from '../../../stores/store'
+import { observer } from 'mobx-react'
+
+import Colors from '../../../utils/colors'
+
+@observer
+export default class SystemBar extends React.Component {
+  constructor () {
+    super()
+  }
+
+  render () {
+    let backColor = Colors.shadeColor(Store.backgroundColor, -0.2)
+
+    if (Store.backgroundColor === '#fff') backColor = Colors.shadeColor(Store.backgroundColor, -0.1)
+
+    const onGroupsMouseDown = e => {
+      e.stopPropagation()
+
+      Store.app.tabMenu.hide()
+      Store.app.pageMenu.hide()
+      Store.app.menu.hide()
+    }
+
+    const onGroupsClick = (e) => {
+      e.stopPropagation()
+
+      if (Store.app.tabGroupsMenu.visible) {
+        Store.app.tabGroupsMenu.hide()
+      } else {
+        Store.app.tabGroupsMenu.show()
+      }
+    }
+
+    const systemBarStyle = {
+      backgroundColor: backColor,
+      display: (Store.isFullscreen) ? 'none' : 'flex',
+      height: platform() == 'darwin' ? 34 : 32
+    }
+
+    return (
+      <div className={'system-bar ' + Store.foreground} style={systemBarStyle}>
+        {this.props.children}
+        <div onMouseDown={onGroupsMouseDown} onClick={onGroupsClick} className='groups'></div>
+        {platform() !== 'darwin' && <div className='border-vertical2'></div> }
+        {platform() !== 'darwin' && <Controls /> }
+        <div className='border-bottom' />
+      </div>
+    )
+  }
+}
