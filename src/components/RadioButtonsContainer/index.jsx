8f7ae71ab2480a5baf648d7397593a3c42cca727
Mikołaj Palkiewicz <xnerhu22@onet.pl>
2018-02-07 09:09:19 +0100

    Rendering radio buttons by 'items' prop

--- src/components/RadioButtonsContainer/index.jsx
+++ src/components/RadioButtonsContainer/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import RadioButton from '../RadioButton'
+
 export default class RadioButtonsContainer extends React.Component {
   render () {
+    const {
+      items
+    } = this.props
+
     return (
       <div className='radio-buttons-container'>
-        {this.props.children}
+        {
+          items.map((data, key) => {
+            return <RadioButton data={data} key={key} />
+          })
+        }
       </div>
     )
   }
