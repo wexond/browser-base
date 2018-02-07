cf3a21e8e1923e51b503a839d8b8c883b631308c
Mikołaj Palkiewicz <xnerhu22@onet.pl>
2018-02-07 11:27:56 +0100

    Add text  next to radiobutton

--- src/components/RadioButtonsContainer/index.jsx
+++ src/components/RadioButtonsContainer/index.jsx
@@ -4,19 +4,63 @@ import ReactDOM from 'react-dom'
 import RadioButton from '../RadioButton'
 
 export default class RadioButtonsContainer extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      selectedItem: -1
+    }
+  }
+
+  componentWillMount () {
+    this.setState({
+      selectedItem: this.props.selected
+    })
+  }
+
+  onRadioButtonClick = (item) => {
+    const index = item.props.index
+
+    if (this.state.selectedItem !== index) {
+      this.setState({
+        selectedItem: index
+      })
+
+      this.triggerOnToggle()
+    }
+  }
+
+  triggerOnToggle () {
+    const onToggle = this.props.onToggle
+
+    if (typeof onToggle === 'function') onToggle(this.state.selectedItem)
+  }
+
   render () {
     const {
       items
     } = this.props
 
+    const selectedItem = this.state.selectedItem
+
+    let index = -1
+
     return (
       <div className='radio-buttons-container'>
         {
           items.map((data, key) => {
-            return <RadioButton data={data} key={key} />
+            index++
+
+            const toggled = selectedItem === index
+
+            return <RadioButton text={data} key={key} toggled={toggled} index={index} onClick={this.onRadioButtonClick} />
           })
         }
       </div>
     )
   }
+}
+
+RadioButtonsContainer.defaultProps = {
+  selected: 0
 }
