b1808a7a6b3f96ee98ebbae05054d9b71086f3f9
AhmedMostafa15 <amk67482@gmail.com>
2018-02-09 22:40:18 +0200

    New Components

---
+++ src/components/Dialog/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+
+/**
+ * Dialog with action buttons. The actions are passed in as an array of React objects,
+ * in this example [FlatButtons](/#/components/flat-button).
+ *
+ * You can also close this dialog by clicking outside the dialog, or with the 'Esc' key.
+ */
+
+export default class DialogExampleSimple extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
+  render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onClick={this.handleClose}
+      />,
+      <FlatButton
+        label="Submit"
+        backgroundColor="#1976D2"
+        hoverColor="#ffffff"
+        primary={true}
+        keyboardFocused={true}
+        onClick={this.handleClose}
+      />,
+    ];
+
+    return (
+      <div>
+        <RaisedButton label="Dialog" onClick={this.handleOpen} />
+        <Dialog
+          title="Dialog With Actions"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          Write any thing here.
+          By Ahmed Mostafa
+        </Dialog>
+      </div>
+    );
+  }
+}
---
+++ src/components/Slider/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Slider} from 'material-ui';
+import Slider from 'material-ui/Slider';
+/**
+ * The `defaultValue` property sets the initial position of the slider.
+ * The slider appearance changes when not at the starting position.
+ */
+const Slider = () => (
+  <div>
+    <Slider
+        defaultValue={0.0}
+        
+    />
+  </div>
+);
+
+export default SliderExampleSimple;
