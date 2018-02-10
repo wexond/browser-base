d206fc5cc82ae1b76b1b73c83ad902e8b943ccd9
Sential <sentialx@gmail.com>
2018-02-10 01:19:07 +0100

    New storage system (part 1)
    Former-commit-id: ba7259b05b9cec6692fc8376a5c1deedd2a02232 [formerly ba7259b05b9cec6692fc8376a5c1deedd2a02232 [formerly a3e48ee9d8a321733c0aa449e82ac39825bafca9]]
    Former-commit-id: 928c9f063b4ee7888bd6b18d9cfe51f4b25ea56f
    Former-commit-id: 4c2c524fa0006d626386eb1a7d5a00a4e51b5f3a

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,167 +1,26 @@
 import fs from 'fs'
 import paths from '../defaults/files'
 
-import base64Img from 'base64-img'
+export const history = new sqlite3.Database(paths.files['history'])
+export const sites = new sqlite3.Database(paths.files['sites'])
+export const favicons = new sqlite3.Database(paths.files['favicons'])
 
-export default class Storage {
-  static async addHistoryItem (title = '', url = '', favicon = '', ogData = {}) {
-    return new Promise(async (resolve, reject) => {
-      if (title != null && url != null) {
-        // Get today's date.
-        let today = new Date()
-        let dd = today.getDate()
-        let mm = today.getMonth() + 1
-        let yyyy = today.getFullYear()
-  
-        if (dd < 10) {
-          dd = '0' + dd
-        }
-  
-        if (mm < 10) {
-          mm = '0' + mm
-        }
-  
-        today = mm + '-' + dd + '-' + yyyy
-  
-        let data = await Storage.get('history')
-  
-        if (!url.startsWith('wexond://') && !url.startsWith('about:blank')) {
-          // Get current time.
-          let date = new Date()
-          let currentHour = ('0' + date.getHours()).slice(-2)
-          let currentMinute = ('0' + date.getMinutes()).slice(-2)
-          let time = `${currentHour}:${currentMinute}`
-  
-          // Configure newItem's data.
-          let newItem = {
-            'url': url,
-            'title': title,
-            'date': today,
-            'time': time,
-            'favicon': favicon,
-            'ogdata': ogData
-          }
-  
-          // Get newItem's new id.
-          if (data[data.length - 1] == null) {
-            newItem.id = 0
-          } else {
-            newItem.id = data[data.length - 1].id + 1
-          }
-  
-          // Push new history item.
-          data.push(newItem)
+history.run(`CREATE TABLE IF NOT EXISTS history(title TEXT, url TEXT, favicon TEXT, date TEXT, ogTitle TEXT, ogDescription TEXT, ogImage TEXT)`)
+sites.run(`CREATE TABLE IF NOT EXISTS sites(title TEXT, url TEXT, favicon TEXT)`)
+favicons.run(`CREATE TABLE IF NOT EXISTS favicons(url TEXT, favicon BLOB)`)
 
-          await Storage.save('history', data)
+export const addFavicon = (url) => {
+  fetch(url)
+    .then(res => res.blob())
+    .then(blob => {
+      const reader = new FileReader()
+      reader.onload = () => {
+        const generatedBuffer = reader.result
 
-          resolve(newItem.id)
-        }
+        console.log(generatedBuffer)
+
+        favicons.run('INSERT INTO favicons(url, favicon) VALUES(?, ?)', [url, new Buffer(generatedBuffer)])
       }
+      reader.readAsArrayBuffer(blob)
     })
-  }
-
-  static async addSite (title = '',  url = '', favicon = '') {
-    return new Promise(async (resolve, reject) => {
-      if (title != null && url != null) {
-        let data = await Storage.get('sites')
-  
-        if (!url.startsWith('wexond://') && !url.startsWith('about:blank')) {
-          // Configure newItem's data.
-          let newItem = {
-            url: url,
-            title: title,
-            favicon: favicon
-          }
-  
-          let canSave = true
-  
-          for (var i = 0; i < data.length; i++) {
-            if (data[i].url === newItem.url) {
-              canSave = false
-              break
-            }
-          }
-  
-          if (!canSave) return
-  
-          // Get newItem's new id.
-          if (data[data.length - 1] == null) {
-            newItem.id = 0
-          } else {
-            newItem.id = data[data.length - 1].id + 1
-          }
-  
-          // Push new history item.
-          data.push(newItem)
-  
-          await Storage.save('sites', data)
-
-          resolve(newItem.id)
-        }
-      }
-    })
-  }
-
-  static async addFavicon (url) {
-    if (url != null) {
-      let data = await Storage.get('favicons')
-
-      if (!url.startsWith('wexond://') && !url.startsWith('about:blank')) {
-        base64Img.requestBase64(url, async (err, res, body) => {
-          if (err) console.error(err)
-          // Configure newItem's data.
-          let newItem = {
-            url: url,
-            data: body
-          }
-
-          let canSave = true
-
-          for (var i = 0; i < data.length; i++) {
-            if (data[i].url === newItem.url) {
-              canSave = false
-              break
-            }
-          }
-
-          if (!canSave) return
-
-          // Get newItem's new id.
-          if (data[data.length - 1] == null) {
-            newItem.id = 0
-          } else {
-            newItem.id = data[data.length - 1].id + 1
-          }
-
-          // Push new history item.
-          data.push(newItem)
-
-          await Storage.save('favicons', data)
-        })
-      }
-    }
-  }
-
-  static save (file, jsonObject) {
-    return new Promise((resolve, reject) => {
-      if (jsonObject != null) {
-        fs.writeFileSync(paths.files[file], JSON.stringify(jsonObject), 'utf8')
-        resolve()
-      } else {
-        reject()
-      }
-    })
-  }
-
-  static get (file) {
-    return new Promise((resolve, reject) => {
-      fs.readFile(paths.files[file], (error, data) => {
-        if (error) {
-          reject(error)
-        } else {
-          resolve(JSON.parse(data))
-        }
-      })
-    })
-  }
 }
