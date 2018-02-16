42fd56142c1f6cbd5017d539bb3f076de5950c14
AhmedMostafa15 <amk67482@gmail.com>
2018-02-16 16:13:09 +0200

    Added "Snack Bar"

--- src/components/Material/Snackbar/index.jsx
+++ src/components/Material/Snackbar/index.jsx
@@ -1,38 +1,59 @@
 import React from 'react'
 
-import { Button, Snackbar } from 'react-toolbox';
-import theme from './style.css';
+import { getForeground } from '../utils/foreground'
 
-class Snackbar extends React.Component {
-    handleClick = () => {
-        this.refs.snackbar.show();
-    };
+import ClassManager from '../utils/class'
 
-    handleSnackbarClick = () => {
-        this.refs.snackbar.hide();
-    };
 
+export default class Snackbar extends React.Component {
     render() {
+        const {
+            background,
+            foreground,
+            className,
+            children,
+            disabled,
+            darkTheme,
+            onClick,
+            onMouseEnter,
+            onMouseLeave,
+            timeout
+        } = this.props
+
+        const foregroundColor = getForeground(foreground)
+
+        const rootClass = ClassManager.get('snack-bar raised', [
+            className,
+            foregroundColor,
+            disabled ? 'disabled' : '',
+            darkTheme ? 'dark-theme' : ''
+        ])
+
+        const style = {
+            backgroundColor: background,
+            color: foregroundColor
+        }
+
+        const events = {
+            onClick: onClick,
+            onMouseEnter: onMouseEnter,
+            onMouseLeave: onMouseLeave
+        }
+
         return (
-            <section>
-                <Button 
-                label='Clear History'
-                 raised primary onClick={this.handleClick}
-                 className='clearBtn'
-                 theme={clearBtn}
-                 />
-                <Snackbar
-                className='snackBar'
-                    action='Dismiss'
-                    active={this.state.active}
-                    label='History Cleared Successfuly'
-                    timeout={4000}
-                    onClick={this.handleSnackbarClick}
-                    onTimeout={this.handleSnackbarTimeout}
-                    type='cancel'
-                />
-            </section>
-        );
+            <div className={rootClass} style={style} {...events}>
+                {children}
+            </div>
+        )
     }
 }
-return <SnackbarTest />;
+
+
+
+Snackbar.defaultProps = {
+    background: '#323232',
+    foreground: '#efefef',
+    disabled: false,
+    darkTheme: false,
+    timeout: 4
+}
