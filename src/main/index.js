9f4868aa26fc4ce3461be06ed213155c25d3a049
Sential <sentialx@gmail.com>
2018-03-04 23:57:47 +0100

    Use prettier, configure webpack
    Former-commit-id: 9d9d77bfbf67188ee9ec39b5413d8ccab2eb1187 [formerly 9d9d77bfbf67188ee9ec39b5413d8ccab2eb1187 [formerly 1211b3e90dc179d46614be09a1af2c3f032dcfb5]]
    Former-commit-id: e142133747a26b6040ae0e7644de85b29e1d2c5c
    Former-commit-id: f06404a7e317d96fd756745d00236e31e96493db

---
+++ src/main/index.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', () => {
+  createWindow();
+});
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow(
+    {
+      frame: false,
+      minWidth: 300,
+      minHeight: 430,
+      width: 900,
+      height: 700,
+      show: false,
+      titleBarStyle: 'hidden-inset',
+      webPreferences: {
+        preload: path.resolve(__dirname, 'preloads/index.js')
+      }
+    }
+  );
+
+  if (process.env.NODE_ENV === 'dev') {
+    mainWindow.webContents.openDevTools({mode: 'detach'});
+  }
+
+  mainWindow.loadURL(path.join('file://', __dirname, '../../static/pages/app.html'));
+
+  mainWindow.setMenu(null);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+};
