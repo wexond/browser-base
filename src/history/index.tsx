ca646147dbbc9d75f92e70503c78436460950eac
Sential <sentialx@gmail.com>
2018-03-05 15:23:29 +0100

    Lint code with tslint and prettier
    Former-commit-id: 5e65be32eddba73dd943a2192aa07b56d5b71e37 [formerly 5e65be32eddba73dd943a2192aa07b56d5b71e37 [formerly 1ba69bc6c1cb02c472cb3f3704acb2a2dbb9114e]]
    Former-commit-id: 2d30047ea932de77dfdb9d0a31d9419893b0c4df
    Former-commit-id: 6e0668efaa44fa5714df3fd5d205664ed21bf935

--- src/history/index.tsx
+++ src/history/index.tsx
@@ -1,7 +1,7 @@
 import "../history.scss";
 
-import React from "react";
-import ReactDOM from "react-dom";
+import react from "react";
+import reactDom from "react-dom";
 
 import History from "../components/History";
 
