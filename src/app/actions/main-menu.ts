8f4ac2eea5b7302c58a7a3a724982fe2b475ac74
Jack ODonnell <jackappdevinc@gmail.com>
2018-03-05 13:22:05 -0500

    Convert more actoins to ts

--- src/app/actions/main-menu.ts
+++ src/app/actions/main-menu.ts
@@ -2,40 +2,40 @@ import * as tabsActions from './tabs'
 import * as pagesActions from './pages'
 import { ipcRenderer } from 'electron'
 
-export const newWindow = () => {
+export const newWindow = (): void => {
   ipcRenderer.send("newWindow")
 }
 
-export const newIncognitoWindow = () => {
+export const newIncognitoWindow = (): void => {
 
 }
 
-export const history = () => {
+export const history = (): void => {
   tabsActions.addTab({
     select: true,
     url: 'wexond://history'
   })
 }
 
-export const bookmarks = () => {
+export const bookmarks = (): void => {
 
 }
 
-export const downloads = () => {
+export const downloads = (): void => {
 
 }
 
-export const settings = () => {
+export const settings = (): void => {
   tabsActions.addTab({
     select: true,
     url: 'wexond://settings'
   })
 }
 
-export const extensions = () => {
+export const extensions = (): void => {
 
 }
 
-export const developerTools = () => {
+export const developerTools = (): void => {
   pagesActions.getSelectedPage().page.webview.openDevTools()
 }
