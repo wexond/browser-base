8a19839504eaa3a5db34bed43fd2aea7ee4c1f64
Sential <sentialx@gmail.com>
2018-03-06 21:07:05 +0100

    Add tabs
    Former-commit-id: f83072e69524406bbb12d4a02b377f1d0fbf9a37 [formerly 6536953da2f3cf4961ba38c04e7eee4eab0be28e]
    Former-commit-id: 3d8a62266e752bb047880601b10368ea08c6e873

--- src/app/components/App/index.tsx
+++ src/app/components/App/index.tsx
@@ -13,20 +13,46 @@ import {
 import List from "../List";
 import SystemBar from "../SystemBar";
 import SystemBarButton from "../SystemBarButton";
+import Tab from "../Tab";
 
+import { ITab, ITabGroup } from "../../interfaces";
+
+import Store from "../../store";
+
+import * as tabs from "../../actions/tabs";
+
+@observer
 export default class App extends React.Component {
   private tabBar: HTMLDivElement;
+
+  public onAddTabClick = () => {
+    tabs.addTab();
+  }
+
+  public renderTabGroups() {
+    return Store.tabGroups.map((tabGroup: ITabGroup) => {
+      return (
+        <List key={tabGroup.id} inline>
+          {tabGroup.tabs.map((tab: ITab) => {
+            return <Tab key={tab.id} tabGroupId={tabGroup.id} selected={tabGroup.selectedTab === tab.id} {...tab} />;
+          })}
+        </List>
+      );
+    });
+  }
 
   public render() {
     return (
       <List style={{ height: "100vh" }}>
         <SystemBar>
-          <List innerRef={(r: any) => (this.tabBar = r)} />
-  
-          <SystemBarButton icon={SystemBarIcons.Add} />
-  
+          <List innerRef={(r: any) => (this.tabBar = r)}>
+            {this.renderTabGroups()}
+          </List>
+
+          <SystemBarButton icon={SystemBarIcons.Add} onClick={this.onAddTabClick} />
+
           <SystemBarButton size={16} icon={SystemBarIcons.TabGroups} />
-  
+
           <SystemBarButton
             windows
             icon={SystemBarIcons.Minimize}
@@ -44,12 +70,12 @@ export default class App extends React.Component {
           />
           <Line />
         </SystemBar>
-  
+
         <List />
       </List>
     );
   }
-};
+}
 
 const Line = styled.div`
   background-color: rgba(0, 0, 0, 0.12);
