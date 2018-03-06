896bb8e3c4408fd48225a919f68571c3d595a1a3
Sential <sentialx@gmail.com>
2018-03-06 20:31:08 +0100

    Add tab-groups icon, import fonts
    Former-commit-id: 7e03bdf9d9cf852758f0aa521cf926278a24fb11 [formerly 1025e39aa022bde12d97791754f20394b591568f]
    Former-commit-id: c799906f7660723f190025858803f963ae9019d4

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { injectGlobal } from "styled-components";
+
+import typography from "../shared/mixins/typography";
 
 import App from "./components/App";
+
+injectGlobal`
+  body {
+    user-select: none;
+    cursor: default;
+    ${typography.body1()}
+    margin: 0;
+    padding: 0;
+  }
+
+  @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Roboto'), local('Roboto-Regular'), url(../shared/fonts/roboto-regular.woff2) format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 500;
+    src: local('Roboto Medium'), local('Roboto-Medium'), url(../shared/fonts/roboto-medium.woff2) format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 300;
+    src: local('Roboto Light'), local('Roboto-Light'), url(../shared/fonts/roboto-light.woff2) format('woff2');
+  }
+`;
 
 ReactDOM.render(<App />, document.getElementById("app"));
