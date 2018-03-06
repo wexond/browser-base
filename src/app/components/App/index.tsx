b8bb063f42658496073dd9fce85320aee8b2f086
Sential <sentialx@gmail.com>
2018-03-06 20:15:57 +0100

    Fix webpack-dev-server HMR
    Former-commit-id: 22d7a29366c573a28f5cf82d9dfeec03672383cc [formerly 6ddd70b94505f68cc8b836351e5db4d7a7015788]
    Former-commit-id: be84d1c67fe2e36f66ab5bb3e1deccfb469102ff

--- src/app/components/App/index.tsx
+++ src/app/components/App/index.tsx
@@ -14,37 +14,41 @@ import List from "../List";
 import SystemBar from "../SystemBar";
 import SystemBarButton from "../SystemBarButton";
 
-export default () => {
-  return (
-    <List style={{ height: "100vh" }}>
-      <SystemBar>
-        <List innerRef={(r: any) => (this.tabBar = r)} />
+export default class App extends React.Component {
+  private tabBar: HTMLDivElement;
 
-        <SystemBarButton icon={SystemBarIcons.Add} />
-
-        <SystemBarButton size={16} icon={SystemBarIcons.TabGroups} />
-
-        <SystemBarButton
-          windows
-          icon={SystemBarIcons.Minimize}
-          onClick={() => minimizeWindow()}
-        />
-        <SystemBarButton
-          windows
-          icon={SystemBarIcons.Maximize}
-          onClick={() => maximizeWindow()}
-        />
-        <SystemBarButton
-          windows
-          icon={SystemBarIcons.Close}
-          onClick={() => closeWindow()}
-        />
-        <Line />
-      </SystemBar>
-
-      <List />
-    </List>
-  );
+  public render() {
+    return (
+      <List style={{ height: "100vh" }}>
+        <SystemBar>
+          <List innerRef={(r: any) => (this.tabBar = r)} />
+  
+          <SystemBarButton icon={SystemBarIcons.Add} />
+  
+          <SystemBarButton size={16} icon={SystemBarIcons.TabGroups} />
+  
+          <SystemBarButton
+            windows
+            icon={SystemBarIcons.Minimize}
+            onClick={() => minimizeWindow()}
+          />
+          <SystemBarButton
+            windows
+            icon={SystemBarIcons.Maximize}
+            onClick={() => maximizeWindow()}
+          />
+          <SystemBarButton
+            windows
+            icon={SystemBarIcons.Close}
+            onClick={() => closeWindow()}
+          />
+          <Line />
+        </SystemBar>
+  
+        <List />
+      </List>
+    );
+  }
 };
 
 const Line = styled.div`
