03a52ceb7d9875d714fd1fd02ef855d7e45604a1
Sential <sentialx@gmail.com>
2018-03-09 19:15:26 +0100

    Add mobx devtools
    Former-commit-id: 94638359e10f28edc3ff479d8846703540822cfe [formerly 8ed9c8efe4cf2c2554c9c4d0d44b9009ae85097d]
    Former-commit-id: 5d1ca381f2f40d042b8d5bc31891ef2d1c4bd3fd

--- src/app/components/App/index.tsx
+++ src/app/components/App/index.tsx
@@ -1,3 +1,4 @@
+import DevTools from 'mobx-react-devtools'
 import React from "react";
 import styled from "styled-components";
 
@@ -13,12 +14,10 @@ import {
 
 // Components
 import List from "../List";
-import Page from "../Page";
 import Pages from "../Pages";
 import SystemBar from "../SystemBar";
 import SystemBarButton from "../SystemBarButton";
 import TabBar from "../TabBar";
-import TabGroup from "../TabGroup";
 
 // Interfaces
 import { IPage, ITab, ITabGroup } from "../../interfaces";
@@ -59,6 +58,8 @@ export default () => {
       </SystemBar>
 
       <Pages />
+
+      {process.env.NODE_ENV === 'development' && <DevTools position={{bottom: 0, right: 0}}/>}
     </List>
   );
 };
