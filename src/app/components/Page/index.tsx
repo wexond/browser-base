5dc3ba581cd8c1347438f99b363aa49a692b2308
Sential <sentialx@gmail.com>
2018-03-09 18:57:58 +0100

    Add tslint-react rules
    Former-commit-id: 74772af692b7caf12bc7b168706169e6612ccdb9 [formerly 74772af692b7caf12bc7b168706169e6612ccdb9 [formerly 211f6d49c4d6faad18f0dcb7a4288ae876f20f96]]
    Former-commit-id: 56ff207bcbe65f72e90ebe3f12d999ad0054cdf1
    Former-commit-id: a2d66b96c8d0e89d5b0128df14af0a48aeb02eed

--- src/app/components/Page/index.tsx
+++ src/app/components/Page/index.tsx
@@ -11,19 +11,19 @@ interface IProps {
 export default ({ page, selected }: IProps) => {
   const { url } = page;
 
+  const pageStyle: any = {
+    flex: selected ? 1 : "0 1",
+    height: !selected ? 0 : "auto",
+    width: !selected ? 0 : "auto",
+    pointerEvents: !selected ? "none" : "auto",
+    opacity: !selected ? 0 : 1,
+    position: !selected ? "absolute" : "initial",
+    top: !selected ? 0 : "auto",
+    left: !selected ? 0 : "auto"
+  };
+
   return (
-    <div
-      style={{
-        flex: selected ? 1 : "0 1",
-        height: !selected ? 0 : "auto",
-        width: !selected ? 0 : "auto",
-        pointerEvents: !selected ? "none" : "auto",
-        opacity: !selected ? 0 : 1,
-        position: !selected ? "absolute" : "initial",
-        top: !selected ? 0 : "auto",
-        left: !selected ? 0 : "auto"
-      }}
-    >
+    <div style={pageStyle}>
       <webview src={url} style={{ height: "100%" }} />
     </div>
   );
