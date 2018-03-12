04ecb0777f975da61741a21b37f2a5f1138fb86a
Sential <sentialx@gmail.com>
2018-03-12 16:35:05 +0100

    Update design
    Former-commit-id: 24cc15352eaf00418728d5cf7c8a3537b5b7f8ec [formerly 9f7034d071e904a157a4cf33cd08ca61a2cc8d94]
    Former-commit-id: ba62ddb5e96e8c60300958a31f5b70dafdb49a54

--- src/app/constants/design.ts
+++ src/app/constants/design.ts
@@ -2,7 +2,7 @@ import { platform } from "os";
 import { Platforms } from "../../shared/enums";
 
 // Heights
-export const SYSTEM_BAR_HEIGHT = 36;
+export const SYSTEM_BAR_HEIGHT = 48;
 export const TABBAR_HEIGHT = 48;
 
 // Widths
@@ -10,6 +10,7 @@ export const SYSTEM_BAR_WINDOWS_BUTTON_WIDTH = 45;
 export const TAB_MAX_WIDTH = 200;
 export const TAB_MIN_WIDTH = 60;
 export const TAB_PINNED_WIDTH = 32;
+export const TABBAR_BUTTON_WIDTH = 40;
 
 // Animations
 export const HOVER_DURATION = 0.2;
