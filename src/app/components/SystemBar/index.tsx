0202961a0fbff50b37aeaca2a5db471d54c46880
Sential <sentialx@gmail.com>
2018-03-12 16:54:59 +0100

    Change design for system bar

--- src/app/components/SystemBar/index.tsx
+++ src/app/components/SystemBar/index.tsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
+import { shadows } from "nersent-ui";
 import { SYSTEM_BAR_HEIGHT } from "../../constants/design";
+
+import Store from "../../store";
+
+import { Platforms } from "../../../shared/enums";
 
 export default styled.div`
   position: relative;
@@ -9,4 +14,5 @@ export default styled.div`
   display: flex;
   -webkit-app-region: drag;
   background-color: #fff;
+  padding-left: ${Store.platform === Platforms.MacOS ? 78 : 0}px;
 `;
