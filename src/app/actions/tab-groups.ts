09735dbe2943db79f52f96b8d8c4a4450f139faf
Sential <sentialx@gmail.com>
2018-03-18 17:20:54 +0100

    :art: Refactor code
    Former-commit-id: c55d1fd57aa10d476365b61f819d3c57d08cc69e [formerly c00c19b1fea3c4e4f4f2561590381feeeceaca83]
    Former-commit-id: a079ce75a80d1576b345b5ef1eb241b93313d59e

--- src/app/actions/tab-groups.ts
+++
@@ -1,5 +0,0 @@
-import Store from "../store";
-
-export const getTabGroupById = (id: number) => {
-    return Store.tabGroups.filter(tabGroup => tabGroup.id === id)[0];
-}
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,71 +1,65 @@
+import { TweenLite } from "gsap";
 import { computed, observable } from "mobx";
 import os from "os";
-
-// Interfaces
-import {
-  IAddressBar,
-  IAddTabButton,
-  IPage,
-  ITab,
-  ITabGroup
-} from "./interfaces";
 
 // Enums
 import { Platforms } from "../shared/enums";
 
-// Actions
-import * as tabGroups from "./actions/tab-groups";
-import * as tabs from "./actions/tabs";
+// Constants and defaults
+import { TOOLBAR_BUTTON_WIDTH } from "./constants/design";
+import { tabAnimations } from "./defaults/tabs";
+
+// Models
+import AddTabButton from "./models/add-tab-button";
+import Page from "./models/page";
+import TabGroup from "./models/tab-group";
 
 class Store {
-  @computed
-  public get currentTabGroup(): ITabGroup {
-    return tabGroups.getTabGroupById(this.selectedTabGroup);
-  }
-
-  @computed
-  public get currentTab(): ITab {
-    return this.currentTabGroup.getSelectedTab();
-  }
-
-  @observable public selectedTabGroup: number = 0;
-
-  @observable
-  public tabGroups: ITabGroup[] = [
-    {
-      selectedTab: -1,
-      tabs: [] as ITab[],
-      id: 0,
-      lineLeft: 0,
-      lineWidth: 0,
-      getSelectedTab() {
-        return tabs.getTabById(this.selectedTab);
-      }
-    }
-  ];
-
-  @observable public pages: IPage[] = [];
-
-  @observable
-  public addTabButton: IAddTabButton = {
-    left: 0
-  };
-
-  @observable
-  public addressBar: IAddressBar = {
+  // Observables
+  @observable public selectedTabGroup = 0;
+  @observable public tabGroups = [ new TabGroup() ];
+  @observable public pages: Page[] = [];
+  @observable public addTabButton = new AddTabButton();
+  @observable public addressBar = {
     toggled: false,
     canToggle: false
   };
 
-  public getTabBarWidth: () => number;
-  public addTab: () => void;
-
-  public platform: Platforms = os.platform() as Platforms;
-
+  public platform = os.platform() as Platforms;
   public mouse = {
     x: 0,
     y: 0
   };
+  public getTabBarWidth: () => number;
+
+  public getCurrentTabGroup() {
+    return this.getTabGroupById(this.selectedTabGroup);
+  }
+
+  public getTabGroupById(id: number) {
+    return this.tabGroups.filter(tabGroup => tabGroup.id === id)[0];
+  }
+
+  public getTabById(id: number) {
+    const { tabGroups } = this;
+  
+    const tabs = tabGroups.map(tabGroup => {
+      return tabGroup.getTabById(id);
+    });
+  
+    return tabs[0];
+  }
+
+  public getPageById(id: number) {
+    return this.pages.filter(page => page.id === id)[0];
+  }
+
+  public addPage(tabId: number) {
+    const page = new Page(tabId);
+    const index = this.pages.push(page) - 1;
+  
+    return this.pages[index];
+  }
 }
 
 export default new Store();
