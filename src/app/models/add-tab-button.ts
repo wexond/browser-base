2047ee24a6aeec0667fe6142177bc9edea46eab6
Sential <sentialx@gmail.com>
2018-03-23 23:03:13 +0100

    :lipstick: Improve tabs closing animations

--- src/app/models/add-tab-button.ts
+++ src/app/models/add-tab-button.ts
@@ -1,37 +1,11 @@
 import { TweenLite } from 'gsap';
 
 // Constants and defaults
-import { TOOLBAR_BUTTON_WIDTH } from '../constants/design';
 import tabAnimations from '../defaults/tab-animations';
-
-import Store from '../store';
 
 export default class AddTabButton {
   public left: number | 'auto' = 0;
   public ref: HTMLDivElement;
-
-  public updateLeft(left: number, animation = true) {
-    const containerWidth = Store.getTabBarWidth();
-
-    if (left >= containerWidth - TOOLBAR_BUTTON_WIDTH) {
-      if (this.left !== 'auto') {
-        if (animation) {
-          this.setLeft(containerWidth - TOOLBAR_BUTTON_WIDTH, true);
-          setTimeout(() => {
-            this.setLeft('auto');
-          }, tabAnimations.left.duration * 1000);
-        } else {
-          this.setLeft('auto');
-        }
-      }
-    } else {
-      if (this.left === 'auto') {
-        this.setLeft(containerWidth - TOOLBAR_BUTTON_WIDTH, animation);
-      }
-
-      this.setLeft(left, animation);
-    }
-  }
 
   public setLeft(left: 'auto' | number, animation = false) {
     if (!animation) {
