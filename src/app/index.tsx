dc675068d56814d8a484ed8b37eab57de26e816f
Sential <sentialx@gmail.com>
2018-03-27 23:33:13 +0200

    :boom: New API for themes
    Former-commit-id: 535a4a5310ecd36b74e528d86eed0fd0963912f0 [formerly 535a4a5310ecd36b74e528d86eed0fd0963912f0 [formerly 5c1af9f96cd4e6412d96cf3457370b46bc7a1df3]]
    Former-commit-id: c224abf48ebaa8c321debc8e1eaffd1c045ec553
    Former-commit-id: cbb2af79a1a70953c2451794b69b33fa46fa186e

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { injectGlobal } from 'styled-components';
-
-// Mixins
+import wpm from 'wexond-package-manager';
 import { typography } from 'nersent-ui';
 
 // Components
 import App from './components/App';
+
+// Models
+import PluginAPI from './models/plugin-api';
+
+import Store from './store';
 
 injectGlobal`
   body {
@@ -17,5 +21,28 @@ injectGlobal`
     padding: 0;
   }
 `;
+
+wpm.list().then((plugins) => {
+  for (const plugin of plugins) {
+    wpm.run(plugin.namespace).then((pkg) => {
+      const api = pkg as PluginAPI;
+
+      if (api.toolbar != null) {
+        Store.theme.toolbar = {
+          ...Store.theme.toolbar,
+          ...api.toolbar,
+        };
+      }
+
+      if (api.toolbarDividersColor != null) {
+        Store.theme.toolbarDividersColor = api.toolbarDividersColor;
+      }
+
+      if (api.accentColor != null) {
+        Store.theme.accentColor = api.accentColor;
+      }
+    });
+  }
+});
 
 ReactDOM.render(<App />, document.getElementById('app'));
