1a9622ae04b6e6c7d1e2eb115690cdf1aa9b5104
Sential <sentialx@gmail.com>
2018-03-28 22:12:35 +0200

    :lipstick: Change separators color based on toolbar foreground

--- src/app/components/ToolBarSeparator/index.tsx
+++ src/app/components/ToolBarSeparator/index.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
+import { transparency } from 'nersent-ui';
 
 export default styled.div`
-  background-color: rgba(0, 0, 0, 0.12);
   height: calc(100% - 24px);
   min-width: 1px;
   top: 50%;
@@ -9,4 +9,9 @@ export default styled.div`
   transform: translateY(-50%);
   margin-left: 8px;
   margin-right: 8px;
+
+  background-color: ${props =>
+    (props.theme.toolbar.foreground === 'white'
+      ? `rgba(255, 255, 255, ${transparency.dark.dividers})`
+      : `rgba(0, 0, 0, ${transparency.light.dividers})`)};
 `;
