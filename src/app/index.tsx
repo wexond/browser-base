8901d518bb3084e0556b027013d0aaa2cf1016f0
Sential <sentialx@gmail.com>
2018-03-29 18:12:44 +0200

    :ambulance: Fix errors with plugins API
    Former-commit-id: bf2aa76af7e17cdab58954d5429ea92b519140cb [formerly bf2aa76af7e17cdab58954d5429ea92b519140cb [formerly 1c8262a4758183e6fe4a41a2ba2273d735c2fa5f]]
    Former-commit-id: df973d4dbd55e4aa2651c0981346a429a2e7b5be
    Former-commit-id: 863c55b5d87a9562a7a8702f90c1744309cb42af

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -27,50 +27,52 @@ wpm.list().then((plugins) => {
     wpm.run(plugin.namespace).then((pkg) => {
       const api = pkg as PluginAPI;
 
-      if (api.theme.toolbar != null) {
-        Store.theme.toolbar = {
-          ...Store.theme.toolbar,
-          ...api.theme.toolbar,
-        };
-      }
+      if (api.theme != null) {
+        if (api.theme.toolbar != null) {
+          Store.theme.toolbar = {
+            ...Store.theme.toolbar,
+            ...api.theme.toolbar,
+          };
+        }
 
-      if (api.theme.searchBar != null) {
-        Store.theme.searchBar = {
-          ...Store.theme.searchBar,
-          ...api.theme.searchBar,
-        };
-      }
+        if (api.theme.searchBar != null) {
+          Store.theme.searchBar = {
+            ...Store.theme.searchBar,
+            ...api.theme.searchBar,
+          };
+        }
 
-      if (api.theme.tabs != null) {
-        Store.theme.tabs = {
-          ...Store.theme.tabs,
-          ...api.theme.tabs,
-        };
-      }
+        if (api.theme.tabs != null) {
+          Store.theme.tabs = {
+            ...Store.theme.tabs,
+            ...api.theme.tabs,
+          };
+        }
 
-      if (api.theme.tabbar != null) {
-        Store.theme.tabbar = {
-          ...Store.theme.tabbar,
-          ...api.theme.tabbar,
-        };
-      }
+        if (api.theme.tabbar != null) {
+          Store.theme.tabbar = {
+            ...Store.theme.tabbar,
+            ...api.theme.tabbar,
+          };
+        }
 
-      if (api.theme.toolbarButtons != null) {
-        Store.theme.toolbarButtons = {
-          ...Store.theme.toolbarButtons,
-          ...api.theme.toolbarButtons,
-        };
-      }
+        if (api.theme.toolbarButtons != null) {
+          Store.theme.toolbarButtons = {
+            ...Store.theme.toolbarButtons,
+            ...api.theme.toolbarButtons,
+          };
+        }
 
-      if (api.theme.addTabButton != null) {
-        Store.theme.addTabButton = {
-          ...Store.theme.addTabButton,
-          ...api.theme.addTabButton,
-        };
-      }
+        if (api.theme.addTabButton != null) {
+          Store.theme.addTabButton = {
+            ...Store.theme.addTabButton,
+            ...api.theme.addTabButton,
+          };
+        }
 
-      if (api.theme.accentColor != null) {
-        Store.theme.accentColor = api.theme.accentColor;
+        if (api.theme.accentColor != null) {
+          Store.theme.accentColor = api.theme.accentColor;
+        }
       }
     });
   }
