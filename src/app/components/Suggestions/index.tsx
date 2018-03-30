6737a152da52d8591a42eb2b775e14f5d78e8dbc
Sential <sentialx@gmail.com>
2018-03-30 16:59:17 +0200

    :sparkles: Integrate suggestions with store

--- src/app/components/Suggestions/index.tsx
+++ src/app/components/Suggestions/index.tsx
@@ -6,22 +6,28 @@ import { StyledSuggestions, Caption } from './styles';
 
 import Suggestion from '../Suggestion';
 
+import Store from '../../store';
+
 export default observer(() => (
-  <StyledSuggestions>
-    <Caption>Most visited</Caption>
-    <Suggestion>www.nersent.tk &mdash; Nersent</Suggestion>
-    <Suggestion>www.nersent.tk &mdash; Nersent</Suggestion>
+  <StyledSuggestions onMouseDown={e => e.stopPropagation()}>
+    {Store.suggestions.mostVisited.length > 0 && <Caption>Most visited</Caption>}
+    {Store.suggestions.mostVisited.map(suggestion => (
+      <Suggestion suggestion={suggestion} key={suggestion.id} />
+    ))}
 
-    <Caption>Bookmarks</Caption>
-    <Suggestion>www.nersent.tk &mdash; Nersent</Suggestion>
-    <Suggestion>www.nersent.tk &mdash; Nersent</Suggestion>
-    <Suggestion>www.nersent.tk &mdash; Nersent</Suggestion>
-    <Suggestion>www.nersent.tk &mdash; Nersent</Suggestion>
+    {Store.suggestions.bookmarks.length > 0 && <Caption>Bookmarks</Caption>}
+    {Store.suggestions.bookmarks.map(suggestion => (
+      <Suggestion suggestion={suggestion} key={suggestion.id} />
+    ))}
 
-    <Caption>Google suggestions</Caption>
-    <Suggestion>nersent</Suggestion>
-    <Suggestion>nersent</Suggestion>
-    <Suggestion>nersent</Suggestion>
-    <Suggestion>nersent</Suggestion>
+    {Store.suggestions.history.length > 0 && <Caption>History</Caption>}
+    {Store.suggestions.history.map(suggestion => (
+      <Suggestion suggestion={suggestion} key={suggestion.id} />
+    ))}
+
+    {Store.suggestions.search.length > 0 && <Caption>Google suggestions</Caption>}
+    {Store.suggestions.search.map(suggestion => (
+      <Suggestion suggestion={suggestion} key={suggestion.id} />
+    ))}
   </StyledSuggestions>
 ));
