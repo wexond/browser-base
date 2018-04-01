20a066458b235df18378b83a8b66b68f24e40603
Sential <sentialx@gmail.com>
2018-04-01 23:25:50 +0200

    :art: Simplify themes loading, inherit foregrounds from toolbar
    Former-commit-id: 6b81191eb307db3ebb58274a752ea1d584a10fa4 [formerly 9d4e2079c351768760359b1be82afae51d9303c3]
    Former-commit-id: 61b6e43a37624d4032f2e17d31c9b8b9421f5d25

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,6 +6,7 @@ import wpm from 'wexond-package-manager';
 import App from './components/App';
 import PluginAPI from './models/plugin-api';
 import Store from './store';
+import Theme from './models/theme';
 
 injectGlobal`
   body {
@@ -17,6 +18,42 @@ injectGlobal`
   }
 `;
 
+const setTheme = (object1: any, object2: any, themeBase: Theme = null, objectName = '') => {
+  if (!themeBase) {
+    themeBase = object1;
+  }
+
+  for (const key in object1) {
+    const newName = `${objectName}.${key}`;
+
+    if (object2[key] != null) {
+      if (typeof object2[key] !== 'object') {
+        object1[key] = object2[key];
+      } else {
+        setTheme(object1[key], object2[key], themeBase, newName);
+      }
+    } else if (
+      newName === '.tabs.hovered.foreground' ||
+      newName === '.tabs.normal.foregrond' ||
+      newName === '.tabs.selected.foreground' ||
+      newName === '.searchBar.foreground'
+    ) {
+      // Inherit foregrounds from toolbar foreground
+      // if the custom theme hasn't foregrounds set.
+      object1[key] = themeBase.toolbar.foreground;
+    } else if (
+      newName === '.tabs.hovered' ||
+      newName === '.tabs.normal' ||
+      newName === '.tabs.selected' ||
+      newName === '.searchBar'
+    ) {
+      object1[key] = {
+        foreground: themeBase.toolbar.foreground,
+      };
+    }
+  }
+};
+
 wpm.list().then((plugins) => {
   for (const plugin of plugins) {
     wpm.run(plugin.namespace).then((pkg) => {
@@ -24,58 +61,7 @@ wpm.list().then((plugins) => {
 
       if (typeof api.setTheme === 'function') {
         const theme = api.setTheme();
-        if (theme.toolbar) {
-          Store.theme.toolbar = {
-            ...Store.theme.toolbar,
-            ...theme.toolbar,
-          };
-        }
-
-        if (theme.searchBar != null) {
-          Store.theme.searchBar = {
-            ...Store.theme.searchBar,
-            ...theme.searchBar,
-          };
-        }
-
-        if (theme.tabs != null) {
-          Store.theme.tabs = {
-            ...Store.theme.tabs,
-            ...theme.tabs,
-          };
-        }
-
-        if (theme.tabbar != null) {
-          Store.theme.tabbar = {
-            ...Store.theme.tabbar,
-            ...theme.tabbar,
-          };
-        }
-
-        if (theme.toolbarButtons != null) {
-          Store.theme.toolbarButtons = {
-            ...Store.theme.toolbarButtons,
-            ...theme.toolbarButtons,
-          };
-        }
-
-        if (theme.addTabButton != null) {
-          Store.theme.addTabButton = {
-            ...Store.theme.addTabButton,
-            ...theme.addTabButton,
-          };
-        }
-
-        if (theme.accentColor != null) {
-          Store.theme.accentColor = theme.accentColor;
-        }
-
-        if (theme.suggestions != null) {
-          Store.theme.suggestions = {
-            ...Store.theme.suggestions,
-            ...theme.suggestions,
-          };
-        }
+        setTheme(Store.theme, theme);
       }
     });
   }
