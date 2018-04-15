d0dc9e9bb483526baa43c60de791c7c0e7d0ef90
Sential <sentialx@gmail.com>
2018-04-15 23:02:47 +0200

    :sparkles: Add menu, add history page

--- src/app/models/page.ts
+++ src/app/models/page.ts
@@ -2,9 +2,9 @@ import { observable } from 'mobx';
 
 export default class Page {
   @observable public id: number = -1;
-  @observable public url: string = 'https://nersent.tk/Projects/Material-React';
+  @observable public url: string = 'wexond://newtab';
 
-  public webview: any;
+  public webview: Electron.WebviewTag;
 
   constructor(id: number) {
     this.id = id;
