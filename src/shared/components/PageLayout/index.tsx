597f1ad11ad75215f4cf61c66c3e1f43949aa680
xnerhu <xnerhu@gmail.com>
2018-04-26 19:03:25 +0200

    Add search input in history
    Former-commit-id: 12d814fa12dcdc1791b0ae82b955cea4e78cb8c9 [formerly 7fb75b4e2349dfcfca18b8c5aa50d1585367f724]
    Former-commit-id: 76c539a7c0559564385ed7e30bb12dd9218b62f9

--- src/shared/components/PageLayout/index.tsx
+++ src/shared/components/PageLayout/index.tsx
@@ -4,7 +4,7 @@ import { observer } from 'mobx-react';
 import Toolbar from '../Toolbar';
 import NavigationDrawer from '../NavigationDrawer';
 
-import { StyledPageLayout, Container } from './styles';
+import { StyledPageLayout, Container, Content } from './styles';
 
 export interface IProps {
   title?: string;
@@ -20,6 +20,14 @@ export default class PageLayout extends React.Component<IProps, {}> {
         <NavigationDrawer />
         <Container>
           <Toolbar title={title} />
+          <Content>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus voluptatibus,
+            tempore facilis accusamus, eaque officiis repudiandae saepe deserunt error temporibus
+            ducimus repellat natus ad vel vero dolorum possimus omnis nulla?Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Ab eos aliquid nesciunt tempore magnam, voluptas illo
+            quod odit quia impedit dignissimos aut repellendus inventore est reprehenderit amet
+            minima sed atque!Lorem
+          </Content>
         </Container>
       </StyledPageLayout>
     );
