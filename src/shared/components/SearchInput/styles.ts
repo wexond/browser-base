9303d7b34c10f9ac431c3651b574c47ea059c33e
Sential <sentialx@gmail.com>
2018-05-04 12:43:00 +0200

    :lipstick: Improve Wexond pages layout

--- src/shared/components/SearchInput/styles.ts
+++ src/shared/components/SearchInput/styles.ts
@@ -11,12 +11,14 @@ const searchIcon = require('../../icons/actions/search.svg');
 export const StyledSearchInput = styled.div`
   width: 100%;
   max-width: 640px;
-  height: 48px;
+  height: 40px;
   background-color: rgba(0, 0, 0, 0.22);
   position: absolute;
   left: 50%;
   border-radius: 2px;
   transform: translateX(-50%);
+  display: flex;
+  align-items: center;
 `;
 
 export const Input = styled.input`
@@ -28,7 +30,7 @@ export const Input = styled.input`
   -webkit-text-fill-color: transparent;
   text-shadow: 0px 0px 0px #fff;
   color: #fff;
-  font-size: 15px;
+  font-size: 13px;
   padding-left: 16px;
   ${typography.robotoRegular()};
 
@@ -38,23 +40,14 @@ export const Input = styled.input`
 
   &::-webkit-input-placeholder {
     text-shadow: 0px 0px 0px rgba(255, 255, 255, ${transparency.light.text.secondary});
-    padding-left: 32px;
   }
 `;
-
-export interface ISearchIcon {
-  visible: boolean;
-}
 
 export const SearchIcon = styled.div`
   width: 22px;
   height: 22px;
-  position: absolute;
-  top: 50%;
-  left: 16px;
-  transform: translateY(-50%);
+  margin-left: 16px;
   background-image: url(${searchIcon});
   ${center('100%', 'auto')};
   ${invertColors()};
-  display: ${(props: ISearchIcon) => (props.visible ? 'block' : 'none')};
 `;
