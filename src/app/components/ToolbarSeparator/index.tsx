3b90d46e107120f245048d405f0d64e782a37f4f
Sential <sentialx@gmail.com>
2018-05-06 23:47:01 +0200

    :wrench: Add missing files
    Former-commit-id: e49499d02fadcf2497fda54256eaf25180b32e95 [formerly 9b1b6d2589194bd19aceaa17714e51f3fe40782e]
    Former-commit-id: c7fb6003840fd22e55d12f3660ccc81a4547155f

---
+++ src/app/components/ToolbarSeparator/index.tsx
@@ -0,0 +1,14 @@
+import { transparency } from 'nersent-ui';
+import styled from 'styled-components';
+
+export default styled.div`
+  height: calc(100% - 24px);
+  min-width: 1px;
+  top: 50%;
+  position: relative;
+  transform: translateY(-50%);
+  margin-left: 8px;
+  margin-right: 8px;
+
+  background-color: rgba(0, 0, 0, ${transparency.light.dividers});
+`;
