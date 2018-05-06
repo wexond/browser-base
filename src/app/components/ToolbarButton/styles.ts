2daff669a3bc6b9e38961fd9395413cac9ef3bbb
Sential <sentialx@gmail.com>
2018-05-06 23:47:01 +0200

    :wrench: Add missing files

---
+++ src/app/components/ToolbarButton/styles.ts
@@ -0,0 +1,47 @@
+import { transparency } from 'nersent-ui';
+import styled from 'styled-components';
+import images from '../../../shared/mixins/images';
+import { HOVER_DURATION, TOOLBAR_BUTTON_WIDTH } from '../../constants/design';
+import { Theme } from '../../models/theme';
+
+interface IconProps {
+  size: number;
+  icon: string;
+  disabled: boolean;
+  theme?: Theme;
+}
+
+export const Icon = styled.div`
+  width: 100%;
+  height: 100%;
+
+  ${(props: IconProps) => images.center(`${props.size}px`, `${props.size}px`)}
+  opacity: ${(props: IconProps) => {
+    if (props.disabled) {
+      return props.theme.toolbarButtons.color === 'light'
+        ? transparency.dark.button.disabled
+        : transparency.light.button.disabled;
+    }
+    return props.theme.toolbarButtons.color === 'light'
+      ? transparency.dark.icons.inactive
+      : transparency.light.icons.inactive;
+  }};
+  background-image: ${props => `url(${props.icon})`};
+`;
+
+interface ButtonProps {
+  theme?: Theme;
+  disabled: boolean;
+}
+
+export const Button = styled.div`
+  height: 100%;
+  -webkit-app-region: no-drag;
+  position: relative;
+
+  transition: ${HOVER_DURATION}s background-color;
+  filter: ${(props: ButtonProps) =>
+    (props.theme.toolbarButtons.color === 'light' ? 'invert(100%)' : '')};
+  width: ${TOOLBAR_BUTTON_WIDTH}px;
+  pointer-events: ${props => (props.disabled ? 'none' : 'auto')};
+`;
