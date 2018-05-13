edc9bd14afb788ad5b7520925f0647f6a51a25ce
Sential <sentialx@gmail.com>
2018-05-13 17:08:32 +0200

    :art: Separate configs
    Former-commit-id: 04f991b7f9dd64c2178e124dc793fc84ded34793 [formerly 491d68f854e904a78097a1e4c6afbefa607d554b]
    Former-commit-id: 297a5784e8a001c118c160b629c3d62ffcd7ed85

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,65 +1,28 @@
 const webpack = require('webpack');
-const { resolve } = require('path');
-const { spawn } = require('child_process');
+const baseConfig = require('./webpack.config.base');
 
-const INCLUDE = resolve(__dirname, 'src');
-const EXCLUDE = /node_modules/;
+const PORT = 8080;
 
-const config = {
-  devtool: 'source-map',
+const config = Object.assign(
+  {
+    devtool: 'source-map',
 
-  output: {
-    path: resolve(__dirname, 'build'),
-    filename: '[name].bundle.js',
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.(png|gif|jpg|woff2|tff|svg)$/,
-        include: INCLUDE,
-        exclude: EXCLUDE,
-        use: [
-          {
-            loader: 'url-loader',
-          },
-        ],
-      },
-      {
-        test: /\.(tsx|ts|jsx|js)$/,
-        include: INCLUDE,
-        exclude: EXCLUDE,
-        use: [
-          {
-            loader: 'ts-loader',
-          },
-        ],
-      },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      }),
     ],
   },
-
-  node: {
-    __dirname: false,
-    __filename: false,
-  },
-
-  resolve: {
-    modules: ['node_modules'],
-    extensions: ['.js', '.tsx', '.ts', '.json'],
-  },
-
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-  ],
-};
+  baseConfig,
+);
 
 const appConfig = {
   target: 'electron-renderer',
+
   entry: {
     app: ['./src/app'],
   },
+
   externals: {
     sqlite3: 'commonjs sqlite3',
     npm: 'require("npm")',
