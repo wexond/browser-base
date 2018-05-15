1ca4705d2d7386adc520c29aa55c7b4244b957ab
xnerhu <xnerhu@gmail.com>
2018-05-15 16:24:33 +0200

    :sparkles: Separate navigation drawer as a component from pages menu

--- src/app/components/NavigationDrawer/index.tsx
+++ src/app/components/NavigationDrawer/index.tsx
@@ -1,28 +1,19 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import { Styled, Title, Header, Dark, NavContent, Content } from './styles';
-import Item from './Item';
+import { Styled, Dark } from './styles';
+
 import Store from '../../store';
-import Search from './Search';
-
-import About from '../../../menu/about/components/About';
-import History from '../../../menu/history/components/History';
-
-const clearIcon = require('../../../shared/icons/clear.svg');
-const historyIcon = require('../../../shared/icons/history.svg');
-const bookmarksIcon = require('../../../shared/icons/bookmarks.svg');
-const settingsIcon = require('../../../shared/icons/settings.svg');
-const extensionsIcon = require('../../../shared/icons/extensions.svg');
-const aboutIcon = require('../../../shared/icons/info.svg');
 
 interface Props {
   children?: any;
-  title?: string;
+  contentVisible?: boolean;
 }
 
 @observer
 export default class extends React.Component<Props, {}> {
-  public static Item = Item;
+  public static defaultProps = {
+    contentVisible: false,
+  };
 
   public onDarkClick = () => {
     requestAnimationFrame(() => {
@@ -31,80 +22,13 @@ export default class extends React.Component<Props, {}> {
     });
   };
 
-  public onItemClick = (e: React.MouseEvent<HTMLDivElement>, item: Item) => {
-    if (item != null && item.props.pageName != null) {
-      Store.navigationDrawer.selectedItem = item.props.pageName;
-    }
-  };
-
   public render() {
-    const { children, title } = this.props;
-
-    const selected = Store.navigationDrawer.selectedItem;
-
-    const contentVisible =
-      selected === 'history' ||
-      selected === 'extensions' ||
-      selected === 'bookmarks' ||
-      selected === 'settings' ||
-      selected === 'about';
-
-    const searchVisible =
-      selected === 'history' ||
-      selected === 'bookmarks' ||
-      selected === 'settings' ||
-      selected === 'extensions';
+    const { children, contentVisible } = this.props;
 
     return (
       <React.Fragment>
         <Styled visible={Store.navigationDrawer.visible} contentVisible={contentVisible}>
-          <Content visible={contentVisible}>
-            {(selected === 'history' && <History />) || (selected === 'about' && <About />)}
-          </Content>
-          <NavContent>
-            <Header>{(searchVisible && <Search />) || <Title>{title}</Title>}</Header>
-            <Item
-              onClick={this.onItemClick}
-              icon={historyIcon}
-              selected={selected === 'history'}
-              pageName="history"
-            >
-              History
-              <Item icon={clearIcon}>Clear browsing history</Item>
-            </Item>
-            <Item
-              onClick={this.onItemClick}
-              icon={bookmarksIcon}
-              selected={selected === 'bookmarks'}
-              pageName="bookmarks"
-            >
-              Bookmarks
-            </Item>
-            <Item
-              onClick={this.onItemClick}
-              icon={settingsIcon}
-              selected={selected === 'settings'}
-              pageName="settings"
-            >
-              Settings
-            </Item>
-            <Item
-              onClick={this.onItemClick}
-              icon={extensionsIcon}
-              selected={selected === 'extensions'}
-              pageName="extensions"
-            >
-              Extensions
-            </Item>
-            <Item
-              onClick={this.onItemClick}
-              icon={aboutIcon}
-              selected={selected === 'about'}
-              pageName="about"
-            >
-              About
-            </Item>
-          </NavContent>
+          {children}
         </Styled>
         <Dark onClick={this.onDarkClick} visible={Store.navigationDrawer.visible} />
       </React.Fragment>
