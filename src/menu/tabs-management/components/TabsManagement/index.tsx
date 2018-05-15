02672364d764ff8bad2d502889550d6c5b95d9ec
xnerhu <xnerhu@gmail.com>
2018-05-15 18:08:52 +0200

    :sparkles: Add tab groups menu
    Former-commit-id: ca9f41d7d50a2d2c78e76281e4b59e28744e7c16 [formerly ca9f41d7d50a2d2c78e76281e4b59e28744e7c16 [formerly 6a9ab28feacafb6bf8560ae1df85f42a5cfda017]]
    Former-commit-id: 4ce6221da38f73ef688fc00da46912291f7caf5f
    Former-commit-id: ad2c57f3ca3a70c0899bd96bb1dbdf020773c4a7

---
+++ src/menu/tabs-management/components/TabsManagement/index.tsx
@@ -0,0 +1,17 @@
+import { observer } from 'mobx-react';
+import { hot } from 'react-hot-loader';
+import React from 'react';
+import { Content } from './styles';
+
+@observer
+class TabsManagment extends React.Component {
+  public render() {
+    return (
+      <React.Fragment>
+        <Content>Foo</Content>
+      </React.Fragment>
+    );
+  }
+}
+
+export default hot(module)(TabsManagment);
