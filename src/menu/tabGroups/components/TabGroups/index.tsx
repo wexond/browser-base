8b655cc77fe31fb1d98b329e322419f8c74089b7
xnerhu <xnerhu@gmail.com>
2018-05-19 19:25:07 +0200

    :sparkles: Update tab groups

--- src/menu/tabGroups/components/TabGroups/index.tsx
+++ src/menu/tabGroups/components/TabGroups/index.tsx
@@ -3,12 +3,16 @@ import { hot } from 'react-hot-loader';
 import React from 'react';
 import { Content } from './styles';
 
+import Goup from '../Group';
+
 @observer
 class TabGroups extends React.Component {
   public render() {
     return (
       <React.Fragment>
-        <Content>Foo</Content>
+        <Content>
+          <Goup title="First group" />
+        </Content>
       </React.Fragment>
     );
   }
