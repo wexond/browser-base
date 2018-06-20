29d432962a7c96fe31e67c0390c6b43776ced12b
xnerhu <xnerhu@gmail.com>
2018-06-20 16:29:02 +0200

    :sparkles: Add card
    Former-commit-id: dbd9daaefe8f649d73c02151bb1cde2f8decf545 [formerly dbd9daaefe8f649d73c02151bb1cde2f8decf545 [formerly 96bfa48760bd1c5377ca9828a0ecb85ecb0f326e]]
    Former-commit-id: cbd2be6cc4ab2c8c5a46bb9330512783fed67599
    Former-commit-id: d035b9b2d3a290db9c17aba0ce3ab7f2a338981e

--- src/newtab/components/App/index.tsx
+++ src/newtab/components/App/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { hot } from 'react-hot-loader';
 
-class App extends React.Component {
+import Card from '../Card';
+
+import { StyledApp, Container, Temperature, InfoContainer, TemperatureIcon } from './styles';
+
+export default class App extends React.Component {
   public render() {
-    return <div>New tab2</div>;
+    return (
+      <StyledApp>
+        <Container>
+          <Card title="Hong kong" secondaryText="Mon, 26:72 AM, Mostly cloudy" largeTitle>
+            <InfoContainer>
+              <Temperature>21&#x2103;</Temperature>
+              <TemperatureIcon src="https://img7.downloadapk.net/7/b7/9e012d_0.png" />
+            </InfoContainer>
+          </Card>
+        </Container>
+      </StyledApp>
+    );
   }
 }
-
-export default hot(module)(App);
