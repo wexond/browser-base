5b981fb5ab64c3343d25aa102d2452e33642a550
xnerhu <xnerhu@gmail.com>
2018-06-22 21:00:45 +0200

    :sparkles: Weather API

--- src/newtab/components/App/index.tsx
+++ src/newtab/components/App/index.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 
 import WeatherCard from '../WeatherCard';
 
+import { requestWeather } from '../../../shared/utils/weather';
+import { Languages } from '../../../shared/enums';
+
 import { StyledApp, Container } from './styles';
 
 export default class App extends React.Component {
+  async componentDidMount() {
+    const weatherData = await requestWeather('opole', Languages.pl);
+
+    console.log(weatherData);
+  }
+
   public render() {
     return (
       <StyledApp>
