be7596839c6c02f280118aa6d22389311919a7a9
Sential <sentialx@gmail.com>
2018-06-22 23:32:06 +0200

    :sparkles: Add deleting history
    Former-commit-id: 528e4a9b4639acd1e8531c4521b83f8573c94701 [formerly 75f0624f2646cc9d23561cc07d745866e1e6f46d]
    Former-commit-id: af70ad644285ba5f8a121b534d1ba25751b68662

--- src/app/defaults/menu-items.tsx
+++ src/app/defaults/menu-items.tsx
@@ -6,6 +6,10 @@ import TabGroups from '../../menu/tabGroups/components/TabGroups';
 import History from '../../menu/history/components/History';
 import About from '../../menu/about/components/About';
 
+import HistoryStore from '../../menu/history/store';
+
+import db from '../../shared/models/app-database';
+
 const tabGroupsIcon = require('../../shared/icons/tab-groups.svg');
 const tabGroupsAddIcon = require('../../shared/icons/add.svg');
 const tabGroupsLoadIcon = require('../../shared/icons/load.svg');
@@ -17,6 +21,50 @@ const settingsIcon = require('../../shared/icons/settings.svg');
 const extensionsIcon = require('../../shared/icons/extensions.svg');
 const aboutIcon = require('../../shared/icons/info.svg');
 const selectAllIcon = require('../../shared/icons/select-all.svg');
+const closeIcon = require('../../shared/icons/close.svg');
+const trashIcon = require('../../shared/icons/delete.svg');
+
+const history = {
+  onSelectAllClick: () => {
+    const { selectedItems, sections } = HistoryStore;
+    for (const section of sections) {
+      for (const item of section.items) {
+        item.selected = true;
+        selectedItems.push(item);
+      }
+    }
+  },
+  onDeselectAllClick: () => {
+    const { selectedItems } = HistoryStore;
+    for (let i = selectedItems.length - 1; i >= 0; i--) {
+      selectedItems[i].selected = false;
+      selectedItems.splice(i, 1);
+    }
+  },
+  onDeleteSelectedClick: () => {
+    const { selectedItems, sections } = HistoryStore;
+    for (let i = selectedItems.length - 1; i >= 0; i--) {
+      const selectedItem = selectedItems[i];
+      for (let j = sections.length - 1; j >= 0; j--) {
+        const section = sections[j];
+        const item = section.items.find(x => x.id === selectedItem.id);
+
+        if (item) {
+          section.items.splice(section.items.indexOf(item), 1);
+
+          if (section.items.length === 0) {
+            sections.splice(j, 1);
+          }
+
+          db.history.delete(item.id);
+
+          break;
+        }
+      }
+      selectedItems.splice(i, 1);
+    }
+  },
+};
 
 export default [
   {
@@ -54,6 +102,20 @@ export default [
       {
         label: 'Select all',
         icon: selectAllIcon,
+        onClick: history.onSelectAllClick,
+        visible: true,
+      },
+      {
+        label: 'Deselect all',
+        icon: closeIcon,
+        onClick: history.onDeselectAllClick,
+        visible: false,
+      },
+      {
+        label: 'Remove selected items',
+        icon: trashIcon,
+        onClick: history.onDeleteSelectedClick,
+        visible: false,
       },
     ],
   },
