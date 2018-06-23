86e30e5c1febfdb571ceb78d9a06c39ed3c73b41
xnerhu <xnerhu@gmail.com>
2018-06-23 20:39:52 +0200

    :art: Add weather icons

---
+++ src/shared/defaults/weatherIcons.ts
@@ -0,0 +1,94 @@
+export const weatherIcons = [
+  /** Day */
+  {
+    // clear sky
+    code: '01d',
+    name: 'weather-clear-day.png',
+  },
+  {
+    // few clouds
+    code: '02d',
+    name: 'weather-few-clouds-day.png',
+  },
+  {
+    // scattered clouds
+    code: '03d',
+    name: 'weather-few-clouds-day.png',
+  },
+  {
+    // broken clouds
+    code: '04d',
+    name: 'weather-few-clouds-day.png',
+  },
+  {
+    // shower rain
+    code: '09d',
+    name: 'weather-showers-day.png',
+  },
+  {
+    // rain
+    code: '10d',
+    name: 'weather-rain-day.png',
+  },
+  {
+    // thunderstorm
+    code: '11d',
+    name: 'weather-storm-day.png',
+  },
+  {
+    // snow
+    code: '13d',
+    name: 'weather-snow-day.png',
+  },
+  {
+    // mist
+    code: '50d',
+    name: 'weather-mist.png',
+  },
+  /** Night */
+  {
+    // clear sky
+    code: '01n',
+    name: 'weather-clear-night.png',
+  },
+  {
+    // few clouds
+    code: '02n',
+    name: 'weather-few-clouds-night.png',
+  },
+  {
+    // scattered clouds
+    code: '03n',
+    name: 'weather-few-clouds-night.png',
+  },
+  {
+    // broken clouds
+    code: '04n',
+    name: 'weather-few-clouds-night.png',
+  },
+  {
+    // shower rain
+    code: '09n',
+    name: 'weather-showers-night.png',
+  },
+  {
+    // rain
+    code: '10n',
+    name: 'weather-rain-night.png',
+  },
+  {
+    // thunderstorm
+    code: '11n',
+    name: 'weather-storm-night.png',
+  },
+  {
+    // snow
+    code: '13n',
+    name: 'weather-snow-night.png',
+  },
+  {
+    // mist
+    code: '50n',
+    name: 'weather-mist.png',
+  },
+];
