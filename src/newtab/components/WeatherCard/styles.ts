0de2fff12be42277e543a8422c5da2d451b56c8e
xnerhu <xnerhu@gmail.com>
2018-06-23 17:20:35 +0200

    :sparkles: New weather options

--- src/newtab/components/WeatherCard/styles.ts
+++ src/newtab/components/WeatherCard/styles.ts
@@ -2,17 +2,26 @@ import styled from 'styled-components';
 
 import images from '../../../shared/mixins/images';
 import typography from '../../../shared/mixins/typography';
+import opacity from '../../../shared/defaults/opacity';
 
 export const InfoContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  padding-bottom: 16px;
 `;
 
 export const Temperature = styled.div`
   font-size: 96px;
+  display: flex;
 
   ${typography.robotoLight()};
+`;
+
+export const TemperatureDeg = styled.div`
+  margin-top: 12px;
+  margin-left: 4px;
+  font-size: 48px;
+
+  ${typography.robotoRegular()};
 `;
 
 export interface ITemperatureIconProps {
@@ -26,4 +35,8 @@ export const TemperatureIcon = styled.div`
 
   ${images.center('100%', 'auto')};
   background-image: url(${(props: ITemperatureIconProps) => props.src});
+`;
+
+export const ErrorContainer = styled.div`
+  color: rgba(0, 0, 0, ${opacity.light.secondaryText});
 `;
