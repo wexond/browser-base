d219bfe344172539e0cf970f99c047034b8b1127
xnerhu <xnerhu@gmail.com>
2018-06-23 23:20:57 +0200

    :bug: Fix some bugs in weather card

--- src/shared/utils/objects.ts
+++ src/shared/utils/objects.ts
@@ -21,3 +21,16 @@ export const merge = (target: any, source: any, onlyNotExisting = false): any =>
 
   return target;
 };
+
+export const capitalizeFirstLetter = (str: string) =>
+  str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+
+export const capitalizeFirstLetterInEachWord = (str: string) => {
+  const splited = str.split(' ');
+
+  for (let i = 0; i < splited.length; i++) {
+    splited[i] = splited[i][0].toUpperCase() + splited[i].substring(1);
+  }
+
+  return splited.join(' ');
+};
