eb0406c1a2b2ea0f11633920ed39c53aabda9bee
Sential <sentialx@gmail.com>
2018-06-25 00:22:48 +0200

    :art: Cleanup styled components

--- src/app/components/Menu/Item/styles.ts
+++ src/app/components/Menu/Item/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import images from '../../../../shared/mixins/images';
 import colors from '../../../../shared/defaults/colors';
 import typography from '../../../../shared/mixins/typography';
@@ -18,11 +18,13 @@ export const StyledItem = styled.div`
   padding-right: 16px;
   display: flex;
 
-  pointer-events: ${({ selected }: ItemProps) => (selected ? 'none' : 'auto')};
-
   &:hover {
     background-color: rgba(0, 0, 0, 0.03);
   }
+
+  ${({ selected }: ItemProps) => css`
+    pointer-events: ${selected ? 'none' : 'auto'};
+  `};
 `;
 
 interface IconProps {
@@ -35,10 +37,12 @@ export const Icon = styled.div`
   height: 24px;
   width: 24px;
 
-  margin-left: ${({ subItem }: IconProps) => (subItem ? '40px' : '16px')};
-  background-color: ${({ selected }) => (selected ? colors.blue['500'] : '#000')};
-  mask-image: ${({ image }) => `url(${image})`};
-  opacity: ${({ selected }) => (selected ? 1 : 0.5)};
+  ${({ subItem, selected, image }: IconProps) => css`
+    margin-left: ${subItem ? '40px' : '16px'};
+    background-color: ${selected ? colors.blue['500'] : '#000'};
+    mask-image: url(${image});
+    opacity: ${selected ? 1 : 0.5};
+  `};
 `;
 
 interface TitleProps {
@@ -49,10 +53,13 @@ export const Title = styled.div`
   font-size: 14px;
   margin-left: 32px;
 
-  opacity: ${({ selected }: TitleProps) => (selected ? 1 : opacity.light.primaryText)};
   ${typography.robotoMedium()};
   display: 'flex';
-  color: ${({ selected }) => (selected ? colors.blue['500'] : '#000')};
+
+  ${({ selected }: TitleProps) => css`
+    color: ${selected ? colors.blue['500'] : '#000'};
+    opacity: ${selected ? 1 : opacity.light.primaryText};
+  `};
 `;
 
 interface BackgroundProps {
@@ -60,7 +67,6 @@ interface BackgroundProps {
 }
 
 export const Background = styled.div`
-  background: ${({ selected }: BackgroundProps) => (selected ? colors.blue['500'] : 'none')};
   opacity: 0.15;
   position: absolute;
   width: calc(100% - 16px);
@@ -69,4 +75,8 @@ export const Background = styled.div`
   top: 50%;
   height: calc(100% - 8px);
   transform: translate(-50%, -50%);
+
+  ${({ selected }: BackgroundProps) => css`
+    background: ${selected ? colors.blue['500'] : 'none'};
+  `};
 `;
