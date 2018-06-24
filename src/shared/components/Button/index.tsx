680d89b2cfe268094f5e80d852a640d731f313b3
xnerhu <xnerhu@gmail.com>
2018-06-24 22:43:43 +0200

    :sparkles: Add button
    Former-commit-id: eb35a7d8d9685c7c0c667b880c01a72958d70b49 [formerly 8a563a8d22eb3ccb4453eca089c8b482d0a1a18f]
    Former-commit-id: f8df5f9e05cbb416b54fcec3852b01a306633a64

---
+++ src/shared/components/Button/index.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+
+import colors from '../../defaults/colors';
+import { UITheme, ButtonType } from '../../enums';
+
+import { StyledButton, Icon, Text } from './styles';
+
+export type ButtonEvent = (e?: React.SyntheticEvent<HTMLDivElement>) => void;
+
+export interface IProps {
+  background?: string;
+  foreground?: string;
+  whiteIcon?: boolean;
+  icon?: string;
+  type?: ButtonType;
+  inline?: boolean;
+  style?: any;
+}
+
+export default class Button extends React.Component<IProps, {}> {
+  public static defaultProps = {
+    background: colors.deepPurple['500'],
+    foreground: '#fff',
+    whiteIcon: true,
+    type: ButtonType.Contained,
+    inline: false,
+  };
+
+  public render() {
+    const {
+      background, foreground, whiteIcon, icon, children, style, inline, type,
+    } = this.props;
+
+    return (
+      <React.Fragment>
+        <StyledButton color={background} icon={icon != null} style={style} type={type}>
+          {icon != null && <Icon src={icon} white={whiteIcon} />}
+          <Text color={foreground}>{children}</Text>
+        </StyledButton>
+        {!inline && <div style={{ clear: 'both' }} />}
+      </React.Fragment>
+    );
+  }
+}
--- src/test-field/components/App/index.tsx
+++ src/test-field/components/App/index.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
 
+import colors from '../../../shared/defaults/colors';
+
+import Button from '../../../shared/components/Button';
+import { ButtonType } from '../../../shared/enums';
+
 import { StyledApp } from './styles';
+
+const addIcon = require('../../../shared/icons/add.svg');
 
 export default class App extends React.Component {
   public render() {
-    return <StyledApp>Test field</StyledApp>;
+    const buttonStyle = {
+      marginTop: '24px',
+    };
+
+    const background = colors.deepPurple['500'];
+    const foreground = '#fff';
+
+    return (
+      <StyledApp>
+        <Button background={background} foreground={foreground}>
+          Button
+        </Button>
+        <Button background={background} foreground={foreground} icon={addIcon} style={buttonStyle}>
+          Button
+        </Button>
+        <Button foreground={background} style={buttonStyle} type={ButtonType.Text}>
+          Button
+        </Button>
+        <Button foreground={background} style={buttonStyle} type={ButtonType.Outlined}>
+          Button
+        </Button>
+      </StyledApp>
+    );
   }
 }
