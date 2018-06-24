f5937b885c4b97ea059cd5d7df6514e9e0028aa1
xnerhu <xnerhu@gmail.com>
2018-06-24 22:43:43 +0200

    :sparkles: Add button
    Former-commit-id: 043a526225a3ec8bc50890235bfa11dc46daf753 [formerly 043a526225a3ec8bc50890235bfa11dc46daf753 [formerly 4c48c1c2d333ec8a141662ac75ab9423739cb360]]
    Former-commit-id: d9548f480eafb7e92d9733b7f7ab899b8fa280e7
    Former-commit-id: 766a0808980219c3936ec2eb9d3472222747d76c

--- src/app/preloads/index.js
+++ src/app/preloads/index.js
@@ -11,3 +11,4 @@ if (
 ) {
   loadContent('testField');
 }
+r;
--- src/test-field/components/App/styles.ts
+++ src/test-field/components/App/styles.ts
@@ -4,7 +4,6 @@ export const StyledApp = styled.div`
   height: 100vh;
   overflow-x: hidden;
   overflow-y: auto;
-  padding-left: 32px;
-  padding-right: 32px;
   background-color: #f5f5f5;
+  padding: 32px;
 `;
