49297435f8b47e56d050f44071ef0b62e24fbe8a
xnerhu <xnerhu@gmail.com>
2018-06-26 00:10:52 +0200

    :sparkles: Theme support in button

--- src/shared/models/transparency.ts
+++ src/shared/models/transparency.ts
@@ -1,29 +1,27 @@
 import opacity from '../defaults/opacity';
 
-export class TransparencyComponent {
-  public light = {
+export const TransparencyComponent = {
+  light: {
     disabled: opacity.light.disabledControl,
     inactive: opacity.light.inactiveControl,
     active: 1,
-  };
-
-  public dark = {
+  },
+  dark: {
     disabled: opacity.dark.disabledControl,
     inactive: opacity.dark.inactiveControl,
     active: 1,
-  };
-}
+  },
+};
 
-export class TransparencyText {
-  public light = {
+export const TransparencyText = {
+  light: {
     disabled: opacity.light.disabledText,
     inactive: opacity.light.secondaryText,
     active: opacity.light.primaryText,
-  };
-
-  public dark = {
+  },
+  dark: {
     disabled: opacity.dark.disabledText,
     inactive: opacity.dark.secondaryText,
     active: opacity.dark.primaryText,
-  };
-}
+  },
+};
