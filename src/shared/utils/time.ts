0b2796e99a9c8eb5791412676c297d6fd6131f40
xnerhu <xnerhu@gmail.com>
2018-06-27 18:31:16 +0200

    :recycle: Code clean up
    Former-commit-id: da51cafa172c9f27e5e256a93eb7f013e92b9739 [formerly da51cafa172c9f27e5e256a93eb7f013e92b9739 [formerly 9a9da26cf31d5de9f9eb1851fddea9ee77d09817]]
    Former-commit-id: 60b04e92c354b5ed5cdbb33de2157a8284ef0648
    Former-commit-id: 18f5f1ac466562bd781e9a07800a0416eb0ac3c8

--- src/shared/utils/time.ts
+++ src/shared/utils/time.ts
@@ -2,10 +2,10 @@ import { TimeUnit } from '../enums';
 
 export const getTimeWithZero = (time: number) => (time < 10 ? `0${time}` : time);
 
-export const getActualTime = (timeUnit: TimeUnit = TimeUnit.am) => {
-  const date = new Date();
-  let hours = date.getHours();
+export const getTime = (date?: Date, timeUnit: TimeUnit = TimeUnit.am) => {
+  if (date == null) date = new Date();
 
+  let hours = date.getHours();
   if (timeUnit === TimeUnit.pm) hours -= 12;
 
   return `${getTimeWithZero(hours)}:${getTimeWithZero(date.getMinutes())}`;
