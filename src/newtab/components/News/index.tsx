cf9053a2cd57dc606f74c59a7df9a895e2fbdacb
xnerhu <xnerhu@gmail.com>
2018-06-27 23:59:25 +0200

    :sparkles: Add news card to new tab
    Former-commit-id: 41f8a3ebafe48996f17291090523b7272418eed4 [formerly 4b177620f02b39f8fd83e7af2fb8fa9708eaf83e]
    Former-commit-id: 1da32643ec114c71b657b317db0407f7d89983c3

--- src/newtab/components/News/index.tsx
+++ src/newtab/components/News/index.tsx
@@ -12,14 +12,14 @@ export default class News extends React.Component<Props, {}> {
   public render() {
     const { data } = this.props;
 
-    return <StyledNews>{data != null && <NewsCard data={data[1]} />}</StyledNews>;
+    return (
+      <StyledNews>
+        {data != null
+          && data.map((news: any, key: any) => {
+            const x = 5;
+            return <NewsCard data={news} key={key} />;
+          })}
+      </StyledNews>
+    );
   }
 }
-
-/*
-{data != null
-          && data.articles.map((news: any, key: any) => {
-            const x = 5;
-            return <div>xd</div>;
-          })}
-          */
--- src/shared/components/Card/index.ts
+++ src/shared/components/Card/index.ts
@@ -13,10 +13,14 @@ export const Card = styled.div`
   background-color: #fff;
   border-radius: 4px;
   overflow: hidden;
-  margin-top: 32px;
+  margin-top: 16px;
+
+  &:first-child {
+    margin-top: 0px;
+  }
 
   &:last-child {
-    margin-bottom: 32px;
+    margin-bottom: 24px;
   }
 
   box-shadow: 0 1px 1px 0 rgba(60, 64, 67, 0.08), 0 1px 3px 1px rgba(60, 64, 67, 0.16);
