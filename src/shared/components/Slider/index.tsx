26b8fe9226c6617b4325b8fd28f90a12f0118a8b
xnerhu <xnerhu@gmail.com>
2018-06-27 15:57:52 +0200

    :sparkles: Add ticks to slider

--- src/shared/components/Slider/index.tsx
+++ src/shared/components/Slider/index.tsx
@@ -2,65 +2,121 @@ import * as React from 'react';
 
 import { getEvents } from '../../utils/events';
 import { getRippleEvents } from '../../utils/ripple';
+import { hexToRgb } from '../../utils/colors';
 
 import Ripples from '../Ripples';
 
+import opacity from '../../defaults/opacity';
 import colors from '../../defaults/colors';
 import { SliderType, ButtonType } from '../../enums';
 
 import {
-  StyledSlider, UnderTrack, Track, ThumbContainer, ThumbHover, Thumb,
+  StyledSlider,
+  InactiveTrack,
+  ActiveTrack,
+  ThumbContainer,
+  ThumbHover,
+  Thumb,
+  TicksContainer,
+  Tick,
 } from './styles';
 
 export interface Props {
   color?: string;
   type?: SliderType;
-  minValue: number;
-  maxValue: number;
+  minValue?: number;
+  maxValue?: number;
   ticks?: any;
+  style?: any;
+  lineTicks?: boolean;
+  selectedTickColor?: string;
 }
 
 export interface State {
   trackWidth: string;
   thumbAnimation: boolean;
+  unselectedTickColor: string;
+  selectedTickIndex: number;
 }
 
 export default class Slider extends React.Component<Props, State> {
   public static defaultProps = {
     color: colors.deepPurple['500'],
     type: SliderType.Continuous,
+    lineTicks: false,
+    selectedTickColor: 'rgba(255, 255, 255, 0.38)',
   };
 
   public state: State = {
-    trackWidth: '50%',
+    trackWidth: '0%',
     thumbAnimation: false,
+    unselectedTickColor: colors.deepPurple['500'],
+    selectedTickIndex: 0,
   };
 
-  private thumbContainer: HTMLDivElement;
+  private inactiveTrack: HTMLDivElement;
 
-  private underTrack: HTMLDivElement;
+  private ticksList: any = [];
 
   private ripples: Ripples;
 
   private isMouseDown: boolean = false;
 
   public componentDidMount() {
+    const { color } = this.props;
+
+    const rgb = hexToRgb(color);
+    const rgba = `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, ${opacity.light.disabledIcon})`;
+
+    this.setState({
+      unselectedTickColor: rgba,
+    });
+
     window.addEventListener('mouseup', this.onWindowMouseUp);
     window.addEventListener('mousemove', this.onWindowMouseMove);
   }
 
   public onMouseDown = (e: React.MouseEvent<HTMLElement>) => {
+    const { type } = this.props;
+    const clientX = e.clientX;
+
     if (!this.isMouseDown) {
-      const clientX = e.clientX;
+      if (type !== SliderType.Discrete) {
+        this.setState({ thumbAnimation: true });
 
-      this.setState({ thumbAnimation: true });
+        setTimeout(() => {
+          this.setState({
+            thumbAnimation: false,
+            trackWidth: `${this.getPercent(clientX)}%`,
+          });
+        }, 150);
+      } else {
+        const gap = this.getGap();
 
-      setTimeout(() => {
-        this.setState({
-          thumbAnimation: false,
-          trackWidth: `${this.getPercent(clientX)}%`,
-        });
-      }, 150);
+        for (let i = 0; i < this.ticksList.length; i++) {
+          const rect = this.ticksList[i].getBoundingClientRect();
+
+          if (rect.left >= clientX + gap / 2) {
+            const tickRect = this.ticksList[i - 1].getBoundingClientRect();
+
+            this.setState({
+              selectedTickIndex: i - 1,
+              trackWidth: `${this.getPercent(tickRect.left)}%`,
+            });
+
+            break;
+          }
+
+          if (i === this.ticksList.length - 1) {
+            const tickRect = this.ticksList[i].getBoundingClientRect();
+
+            this.setState({
+              selectedTickIndex: i,
+              trackWidth: `${this.getPercent(tickRect.left)}%`,
+            });
+          }
+        }
+      }
     }
   };
 
@@ -73,17 +129,41 @@ export default class Slider extends React.Component<Props, State> {
   };
 
   public onWindowMouseMove = (e: MouseEvent) => {
+    const { type } = this.props;
+
     if (this.isMouseDown) {
-      this.setState({
-        trackWidth: `${this.getPercent(e.clientX)}%`,
-      });
+      if (type !== SliderType.Discrete) {
+        this.setState({
+          trackWidth: `${this.getPercent(e.clientX)}%`,
+        });
+      } else {
+        const gap = this.getGap();
+
+        const { selectedTickIndex } = this.state;
+        const selectedTick = this.ticksList[selectedTickIndex];
+        const selectedTickRect = selectedTick.getBoundingClientRect();
+        const right = selectedTickRect.left - gap / 2 <= e.clientX;
+
+        const tickIndex = right ? selectedTickIndex + 1 : selectedTickIndex - 1;
+
+        if (tickIndex >= 0 && tickIndex < this.ticksList.length) {
+          const tickRect = this.ticksList[tickIndex].getBoundingClientRect();
+
+          if (tickRect.left - gap / 2 <= e.clientX) {
+            this.setState({
+              selectedTickIndex: tickIndex,
+              trackWidth: `${this.getPercent(tickRect.left)}%`,
+            });
+          }
+        }
+      }
     }
   };
 
   public getPercent = (clientX: number) => {
-    const underTrackRect = this.underTrack.getBoundingClientRect();
-    const posX = clientX - underTrackRect.left;
-    const percent = (posX * 100) / this.underTrack.clientWidth;
+    const inactiveTrackRect = this.inactiveTrack.getBoundingClientRect();
+    const posX = clientX - inactiveTrackRect.left;
+    const percent = (posX * 100) / this.inactiveTrack.clientWidth;
 
     if (percent < 0) return 0;
     if (percent > 100) return 100;
@@ -91,9 +171,15 @@ export default class Slider extends React.Component<Props, State> {
     return percent;
   };
 
+  public getGap = () => this.inactiveTrack.clientWidth / this.ticksList.length;
+
   public render() {
-    const { color } = this.props;
-    const { trackWidth, thumbAnimation } = this.state;
+    const {
+      color, ticks, style, type, selectedTickColor,
+    } = this.props;
+    const {
+      trackWidth, thumbAnimation, unselectedTickColor, selectedTickIndex,
+    } = this.state;
 
     const trackStyle = {
       width: trackWidth,
@@ -103,19 +189,40 @@ export default class Slider extends React.Component<Props, State> {
       left: trackWidth,
     };
 
+    let tickIndex = 0;
+
+    this.ticksList = [];
+
     return (
-      <StyledSlider onMouseDown={this.onMouseDown}>
-        <UnderTrack innerRef={r => (this.underTrack = r)} color={color} />
-        <Track color={color} thumbAnimation={thumbAnimation} style={trackStyle} />
-        <ThumbContainer
-          innerRef={r => (this.thumbContainer = r)}
-          style={thumbStyle}
-          onMouseDown={this.onThumbMouseDown}
-        >
-          <ThumbHover className="thumb-hover" color={color} />
-          <Thumb thumbAnimation={thumbAnimation} color={color} />
-        </ThumbContainer>
-      </StyledSlider>
+      <React.Fragment>
+        <StyledSlider onMouseDown={this.onMouseDown} style={style}>
+          <InactiveTrack innerRef={r => (this.inactiveTrack = r)} color={color} />
+          <ActiveTrack
+            type={type}
+            color={color}
+            thumbAnimation={thumbAnimation}
+            style={trackStyle}
+          />
+          <TicksContainer>
+            {type === SliderType.Discrete
+              && Object.prototype.toString.call(ticks) === '[object Array]'
+              && ticks.map((data: any, key: any) => {
+                tickIndex++;
+                return (
+                  <Tick
+                    innerRef={r => r != null && this.ticksList.push(r)}
+                    key={key}
+                    color={tickIndex <= selectedTickIndex ? selectedTickColor : unselectedTickColor}
+                  />
+                );
+              })}
+          </TicksContainer>
+          <ThumbContainer type={type} style={thumbStyle} onMouseDown={this.onThumbMouseDown}>
+            <ThumbHover className="thumb-hover" color={color} />
+            <Thumb thumbAnimation={thumbAnimation} color={color} />
+          </ThumbContainer>
+        </StyledSlider>
+      </React.Fragment>
     );
   }
 }
