bd346b18ead683bfaee33d9239d6dde1dfeb54ea
xnerhu <xnerhu@gmail.com>
2018-06-28 00:24:53 +0200

    :sparkles: Icon, thumbnail load animation in news card

--- src/newtab/components/News/NewsCard/index.tsx
+++ src/newtab/components/News/NewsCard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { loadImage } from '../../../../shared/utils/network';
 import { getTimeOffset } from '../../../../shared/utils/time';
 
 import { Card, CardHeader, CardImage } from '../../../../shared/components/Card';
@@ -12,25 +13,45 @@ export interface Props {
   data: any;
 }
 
-export default class NewsCard extends React.Component<Props, {}> {
-  public render() {
+export interface State {
+  loaded: boolean;
+}
+
+export default class NewsCard extends React.Component<Props, State> {
+  public state: State = {
+    loaded: false,
+  };
+
+  public async componentDidMount() {
     const { data } = this.props;
 
+    await loadImage(data.urlToImage);
+    await loadImage(data.icon);
+
+    this.setState({ loaded: true });
+  }
+
+  public render() {
+    const { data } = this.props;
+    const { loaded } = this.state;
+
     return (
-      <Card>
-        <CardHeader logo>
-          <CardHeaderText>
-            <Title>{data.title}</Title>
-            <Info>
-              <Icon source={data.icon} />
-              <Source>
-                {data.source.name} - {getTimeOffset(new Date(data.publishedAt))}
-              </Source>
-            </Info>
-          </CardHeaderText>
-          <CardImage src={data.urlToImage} />
-        </CardHeader>
-      </Card>
+      <a href={data.url}>
+        <Card>
+          <CardHeader logo>
+            <CardHeaderText>
+              <Title>{data.title}</Title>
+              <Info>
+                <Icon visible={loaded} source={data.icon} />
+                <Source>
+                  {data.source.name} - {getTimeOffset(new Date(data.publishedAt))}
+                </Source>
+              </Info>
+            </CardHeaderText>
+            <CardImage visible={loaded} src={data.urlToImage} />
+          </CardHeader>
+        </Card>
+      </a>
     );
   }
 }
--- src/newtab/components/News/styles.ts
+++ src/newtab/components/News/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
 export const StyledNews = styled.div`
-  width: 300px;
+  width: 256px;
 `;
