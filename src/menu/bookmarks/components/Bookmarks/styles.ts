c1fc754949bf693f1be293353bfadd901e091416
xnerhu <xnerhu@gmail.com>
2018-07-02 14:35:20 +0200

    :lipstick: Change folders style
    Former-commit-id: 8e3b566ac8c400e04e0d191288610b1b8f16ab00 [formerly eb4411f8325501d7504f1911de2ddb09024cb248]
    Former-commit-id: a878152620b98108a07e2480b19e136c66ffabc0

--- src/menu/bookmarks/components/Bookmarks/styles.ts
+++ src/menu/bookmarks/components/Bookmarks/styles.ts
@@ -7,17 +7,15 @@ export const Content = styled.div`
 `;
 
 export const Folders = styled.div`
-  padding: 16px;
-  margin-top: 24px;
+  padding: 8px;
+  margin-top: 16px;
   display: flex;
   overflow: hidden;
   border-radius: 4px;
-  background-color: #fff;
-  box-shadow: 0 1px 1px 0 rgba(60, 64, 67, 0.08), 0 1px 3px 1px rgba(60, 64, 67, 0.16);
 `;
 
 export const Items = styled.div`
-  margin-top: 32px;
+  margin-top: 16px;
   display: flex;
   flex-flow: column;
   overflow: hidden;
