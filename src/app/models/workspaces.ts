7f92686e43e161c5bee3f5995c6663dd6a2887e8
xnerhu <xnerhu@gmail.com>
2018-07-02 10:01:58 +0200

    :sparkles: Improve animations
    Former-commit-id: 83ec44a12fd4924f840f2a617b725371c0b1186e [formerly 83ec44a12fd4924f840f2a617b725371c0b1186e [formerly f44b6e59f06f8a9ba238dc4134a976d76a795320]]
    Former-commit-id: c476ba01ef998765d412a801dca787af3333d77b
    Former-commit-id: 0e0f692052c3e7ef659119891706b376cfcad6fb

--- src/app/models/workspaces.ts
+++ src/app/models/workspaces.ts
@@ -1,11 +1,23 @@
-import { observable, observe } from 'mobx';
+import { observable } from 'mobx';
 import Workspace from './workspace';
-import Store from '../store';
 
 export default class Workspaces {
   @observable public visible = false;
 
   @observable public selected = 0;
 
-  @observable public list = [new Workspace()];
+  @observable
+  public list = [
+    new Workspace(),
+    new Workspace(),
+    new Workspace(),
+    new Workspace(),
+    new Workspace(),
+  ];
+
+  public timer: any;
+
+  public hide = () => {
+    this.visible = false;
+  };
 }
