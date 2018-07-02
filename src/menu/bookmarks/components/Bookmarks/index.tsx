1c66fe736d085181d53454497e23db6c6ac7908b
xnerhu <xnerhu@gmail.com>
2018-07-02 13:43:25 +0200

    :sparkles: Add bookmark items

--- src/menu/bookmarks/components/Bookmarks/index.tsx
+++ src/menu/bookmarks/components/Bookmarks/index.tsx
@@ -3,17 +3,30 @@ import { hot } from 'react-hot-loader';
 import React from 'react';
 
 import Folder from '../Folder';
-import { Content, Folders } from './styles';
+import Item from '../Item';
+import { Content, Folders, Items } from './styles';
 
 @observer
 class Bookmarks extends React.Component {
   public render() {
+    const testData = {
+      title: 'wexond/wexond: An extensible web browser with Material UI and built-in ad blocker.',
+      url: 'https://www.github.com/Wexond/Wexond',
+      selected: false,
+    };
+
     return (
       <Content>
         <Folders>
           <Folder />
           <Folder />
         </Folders>
+        <Items>
+          <Item data={testData} />
+          <Item data={testData} />
+          <Item data={testData} />
+          <Item data={testData} />
+        </Items>
       </Content>
     );
   }
