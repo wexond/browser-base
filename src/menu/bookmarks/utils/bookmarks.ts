1f29d2af07f432b6bb6fc77307e66f59ce78da40
xnerhu <xnerhu@gmail.com>
2018-07-18 21:38:37 +0200

    :sparkles: Folder path in bookmarks
    Former-commit-id: 7502de0889f501d93d81f261fd3548c7e6509a73 [formerly 7502de0889f501d93d81f261fd3548c7e6509a73 [formerly af8a234ebb09449659ed0a3fa66ecad1002f7d12]]
    Former-commit-id: d88947e8f88490514bb62e81adbd81dc500e2e70
    Former-commit-id: 63cee9ceca252b14576068d724109e9612e5c151

---
+++ src/menu/bookmarks/utils/bookmarks.ts
@@ -0,0 +1,35 @@
+import Store from '../store';
+
+export const getParentFolder = (folder: any, folders: any = Store.data.folders, parent?: any) => {
+  for (let i = 0; i < folders.length; i++) {
+    if (folders[i] === folder) {
+      return parent;
+    }
+
+    const p: any = getParentFolder(folder, folders[i].folders, folders[i]);
+    if (p != null) return p;
+  }
+
+  return null;
+};
+
+export const getFolderPath = (folder: any) => {
+  const titles = [];
+  let lastChild = folder;
+
+  while (true) {
+    const parent = getParentFolder(lastChild);
+
+    if (parent != null) {
+      titles.push(parent.title);
+      lastChild = parent;
+    } else {
+      break;
+    }
+  }
+
+  titles.reverse();
+  titles.push(folder.title);
+
+  return titles;
+};
