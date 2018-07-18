bfd75fda403bc6d352ba7b4eb4518a32d14b6708
xnerhu <xnerhu@gmail.com>
2018-07-18 18:40:02 +0200

    :lipstick: Less title margin in item in bookmarks
    Former-commit-id: c4cf41aa791f7222dec490d9bedf838db1765f2f [formerly c37911a94ac56b284ef4dde08c0afb1cb8d5984b]
    Former-commit-id: 4f54fe279773ba42ad0684a61ad046036a57b851

---
+++ src/menu/bookmarks/components/Item/styles.ts
@@ -0,0 +1,7 @@
+import styled, { css } from 'styled-components';
+
+import { PageItemTitle } from '../../../../shared/components/PageItem/styles';
+
+export const Title = styled(PageItemTitle)`
+  margin-left: 24px;
+`;
---
+++ src/menu/bookmarks/components/TreeBar/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Root } from './styles';
+
+export default class TreeBar extends React.Component {
+  public render() {
+    return <Root />;
+  }
+}
