af3145137a8bcae54b069c153564da062093a24f
xnerhu <xnerhu@gmail.com>
2018-07-21 22:15:56 +0200

    Add bookmarks dialog animation

--- src/app/components/BookmarksDialog/styles.ts
+++ src/app/components/BookmarksDialog/styles.ts
@@ -4,6 +4,12 @@ import shadows from '../../../shared/mixins/shadows';
 import typography from '../../../shared/mixins/typography';
 import opacity from '../../../shared/defaults/opacity';
 
+import { EASE_FUNCTION } from '../../../shared/constants';
+
+export interface RootProps {
+  visible: boolean;
+}
+
 export const Root = styled.div`
   width: 364px;
   background-color: #fff;
@@ -13,8 +19,14 @@ export const Root = styled.div`
   z-index: 100;
   border-radius: 4px;
   padding: 16px;
+  transform-origin: top right;
+  will-change: transform, opacity;
+
   box-shadow: ${shadows(6)};
-  display: none;
+  transform: ${({ visible }: RootProps) => (visible ? 'scale(1)' : 'scale(0)')};
+  transition: ${({ visible }) =>
+    `0.5s ${EASE_FUNCTION} transform, ${visible ? 0.1 : 0.2}s opacity`};
+  opacity: ${({ visible }) => (visible ? 1 : 0)};
 `;
 
 export const Title = styled.div`
