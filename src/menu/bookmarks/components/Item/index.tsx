438bd7d8fc67dda483c6fc8045ded025faad138b
Sential <sentialx@gmail.com>
2018-07-21 22:01:24 +0200

    Add ability to select items in bookmarks
    Former-commit-id: 76f1b833dff165bafad5f892d9aef92e8daea0a9 [formerly 8941212430fc60191a58e6d60cc1c2b01ca42a45]
    Former-commit-id: 8158c0152ec0d7189d280ea038901aa5c48531d4

--- src/menu/bookmarks/components/Item/index.tsx
+++ src/menu/bookmarks/components/Item/index.tsx
@@ -5,6 +5,7 @@ import { PageItem, PageItemIcon, PageItemRemoveIcon } from '../../../../shared/c
 import { Title } from './styles';
 import Store from '../../store';
 import BookmarkItem from '../../../../shared/models/bookmark-item';
+import AppStore from '../../../../app/store';
 
 const pageIcon = require('../../../../shared/icons/page.svg');
 const folderIcon = require('../../../../shared/icons/folder.svg');
@@ -29,10 +30,20 @@ export default class Item extends React.Component<{ data: BookmarkItem }, { hove
     });
   }
 
-  public onClick = () => {
+  public onClick = (e: React.MouseEvent<HTMLDivElement>) => {
     const { data } = this.props;
-    if (data.type === 'folder') {
+
+    if (this.cmdPressed || e.ctrlKey) {
+      if (Store.selectedItems.indexOf(data.id) === -1) {
+        Store.selectedItems.push(data.id);
+      } else {
+        Store.selectedItems.splice(Store.selectedItems.indexOf(data.id), 1);
+      }
+    } else if (data.type === 'folder') {
       Store.goTo(data.id);
+    } else {
+      AppStore.getCurrentWorkspace().addTab(data.url);
+      AppStore.menu.hide();
     }
   };
 
@@ -60,7 +71,7 @@ export default class Item extends React.Component<{ data: BookmarkItem }, { hove
         onMouseOver={this.onMouseEnter}
         onMouseLeave={this.onMouseLeave}
         onClick={this.onClick}
-        selected={false}
+        selected={Store.selectedItems.indexOf(data.id) !== -1}
       >
         <PageItemRemoveIcon visible={hovered} />
         <PageItemIcon style={{ opacity: hovered ? 0 : opacity }} icon={favicon} />
