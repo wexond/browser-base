c699b9604e0e6896f6de94fc2511ed181f585df3
xnerhu <xnerhu@gmail.com>
2018-07-22 16:34:23 +0200

    Adding page to bookmarks

--- src/app/components/BookmarksDialog/index.tsx
+++ src/app/components/BookmarksDialog/index.tsx
@@ -5,14 +5,11 @@ import { ButtonType } from '../../../shared/enums';
 import Textfield from '../../../shared/components/Textfield';
 import Button from '../../../shared/components/Button';
 import colors from '../../../shared/defaults/colors';
+import db from '../../../shared/models/app-database';
 import { Root, Title, ButtonsContainer } from './styles';
 
-export interface IProps {
-  visible: boolean;
-}
-
 @observer
-export default class BookmarksDialog extends Component<IProps, {}> {
+export default class BookmarksDialog extends Component {
   private textField: Textfield;
 
   public onMouseDown = (e?: SyntheticEvent<any>) => {
@@ -24,15 +21,33 @@ export default class BookmarksDialog extends Component<IProps, {}> {
     Store.bookmarksDialogVisible = false;
   };
 
+  public onRemoveClick = () => {
+    const selectedTab = Store.getSelectedTab();
+
+    if (selectedTab.bookmarkId !== -1) {
+      db.bookmarks.delete(selectedTab.bookmarkId);
+      selectedTab.isSavedAsBookmark = false;
+      selectedTab.bookmarkId = -1;
+
+      Store.bookmarksDialogVisible = false;
+    } else {
+      console.error(selectedTab); // eslint-disable-line
+    }
+  };
+
   public render() {
-    const { visible } = this.props;
+    const visible = Store.bookmarksDialogVisible;
 
     return (
       <Root visible={visible} onMouseDown={this.onMouseDown}>
         <Title>New bookmark</Title>
         <Textfield ref={r => (this.textField = r)} label="Name" style={{ marginTop: 16 }} />
         <ButtonsContainer>
-          <Button foreground={colors.blue['500']} type={ButtonType.Outlined}>
+          <Button
+            foreground={colors.blue['500']}
+            type={ButtonType.Outlined}
+            onClick={this.onRemoveClick}
+          >
             REMOVE
           </Button>
           <Button onClick={this.onDoneClick}>DONE</Button>
