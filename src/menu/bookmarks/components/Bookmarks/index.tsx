c7dd6e7da164f4392a0d310f67736fcb1be9ab71
xnerhu <xnerhu@gmail.com>
2018-07-22 16:41:56 +0200

    Loading bookmarks from database
    Former-commit-id: 02225ae6074ffe21567516ce1d03000a7481435d [formerly f4c397d44be37c5f5bbe2bf38eb8a000a1b57f3c]
    Former-commit-id: 6c0e59e741242a3d16d0bbcc527358cd63539169

--- src/menu/bookmarks/components/Bookmarks/index.tsx
+++ src/menu/bookmarks/components/Bookmarks/index.tsx
@@ -2,7 +2,7 @@ import { observer } from 'mobx-react';
 import { hot } from 'react-hot-loader';
 import React from 'react';
 import Store from '../../store';
-
+import db from '../../../../shared/models/app-database';
 import Item from '../Item';
 import TreeBar from '../TreeBar';
 import { Content, Items } from './styles';
@@ -10,38 +10,15 @@ import { Content, Items } from './styles';
 @observer
 class Bookmarks extends React.Component {
   componentDidMount() {
-    // Temporary, for testing
-    Store.bookmarks = [
-      {
-        title: 'Folder 1',
-        type: 'folder',
-        id: 0,
-        parent: -1,
-      },
-      {
-        title: 'Subfolder 1',
-        type: 'folder',
-        id: 1,
-        parent: 0,
-      },
-      {
-        title: 'Wexond',
-        type: 'item',
-        url: 'https://github.com/wexond/wexond',
-        id: 2,
-        parent: -1,
-      },
-      {
-        title: 'Nersent',
-        type: 'item',
-        url: 'https://github.com/nersent',
-        id: 3,
-        parent: 1,
-      },
-    ];
-
-    Store.goTo(-1);
+    this.loadBookmarks();
   }
+
+  loadBookmarks = async () => {
+    const bookmarks = await db.bookmarks.toArray();
+
+    Store.bookmarks = bookmarks;
+    Store.goTo(-1);
+  };
 
   public render() {
     return (
