a874a97701244d12522433c0bab4eb766242ed5a
xnerhu <xnerhu@gmail.com>
2018-07-24 17:23:40 +0200

    Add custom scroll mixin

---
+++ src/shared/mixins/scroll.ts
@@ -0,0 +1,32 @@
+import colors from '../defaults/colors';
+
+export const noButtons = (
+  width: string = '6px',
+  color: string = 'rgba(0, 0, 0, 0.38)',
+  hoverColor: string = 'rgba(0, 0, 0, 0.54)',
+) => `
+    &::-webkit-scrollbar {
+      width: ${width};
+    }
+
+    &::-webkit-scrollbar-button {
+      width: 0px;
+      height: 0px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background: ${color};
+      border: 0px none #ffffff;
+      border-radius: 0px;
+    }
+
+    &::-webkit-scrollbar-thumb:hover {
+      background: ${hoverColor};
+    }
+
+    &::-webkit-scrollbar-corner {
+      background: transparent;
+    }
+  `;
+
+export default { noButtons };
