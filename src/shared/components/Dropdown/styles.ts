9dc2d9f60ec0621c63ea8f7dda99fee70b48b0e6
xnerhu <xnerhu@gmail.com>
2018-07-24 16:03:56 +0200

    Add list in drop-down menu
    Former-commit-id: af61624c275dc6c4b7504afd1339aa254872f760 [formerly af61624c275dc6c4b7504afd1339aa254872f760 [formerly e185232d98199246dbf9985c7e8e3e6d04ceefd1]]
    Former-commit-id: ae1a2a6b478307d063801c7f4bdc525039509089
    Former-commit-id: 16ba58ff4dfaa2834538fd6b9efe82d401871175

--- src/shared/components/Dropdown/styles.ts
+++ src/shared/components/Dropdown/styles.ts
@@ -4,6 +4,7 @@ import styled, { StyledComponentClass } from 'styled-components';
 import opacity from '../../defaults/opacity';
 import typography from '../../mixins/typography';
 import images from '../../mixins/images';
+import shadows from '../../mixins/shadows';
 
 const dropDownIcon = require('../../icons/drop-down.svg');
 
@@ -17,19 +18,42 @@ export const Root = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  cursor: pointer;
+  position: relative;
 `;
 
-export const Selected = styled.div`
+export const Name = styled.div`
   font-size: 14px;
+  color: rgba(0, 0, 0, ${opacity.light.primaryText});
 
   ${typography.robotoRegular()};
 `;
 
+export interface IconProps {
+  activated: boolean;
+}
+
 export const Icon = styled.div`
   width: 24px;
   height: 24px;
+  will-change: transform;
+  transition: 0.2s ease-out transform;
   opacity: ${opacity.light.inactiveIcon};
   background-image: url(${dropDownIcon});
 
   ${images.center('24px', 'auto')};
+  transform: rotate(${({ activated }: IconProps) => (activated ? 180 : 0)});
+`;
+
+export const List = styled.div`
+  width: 100%;
+  max-height: 144px;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  background-color: #fff;
+  border-radius: 4px;
+  box-shadow: ${shadows(7)};
 `;
