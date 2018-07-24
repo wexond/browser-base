5b3c9e892f4ac496fa3516c4f86b3e2dd858aee4
xnerhu <xnerhu@gmail.com>
2018-07-24 16:03:56 +0200

    Add list in drop-down menu
    Former-commit-id: 3205397cb19724be8d833f22f07e878ebb6f5be9 [formerly e8b34a4e613fd122d21af1ddbfc346978be71464]
    Former-commit-id: 2ff85145e7a50340a69e3aff1a72a94a94465136

--- src/shared/components/Dropdown/index.tsx
+++ src/shared/components/Dropdown/index.tsx
@@ -1,17 +1,37 @@
 import * as React from 'react';
-
-import { Root, Selected, Icon } from './styles';
+import Item from './Item';
+import {
+  Root, Name, Icon, List,
+} from './styles';
 
 export interface IProps {}
 
-export default class Button extends React.Component<IProps, {}> {
+export interface IState {
+  activated: boolean;
+}
+
+export default class Button extends React.Component<IProps, IState> {
   public static defaultProps = {};
 
+  public static Item = Item;
+
+  public state: IState = {
+    activated: false,
+  };
+
   public render() {
+    const { activated } = this.state;
+
     return (
       <Root>
-        <Selected>Item 2</Selected>
-        <Icon />
+        <Name>Item 2</Name>
+        <Icon activated={activated} />
+        <List>
+          <Item>Item 1</Item>
+          <Item>Item 2</Item>
+          <Item>Item 3</Item>
+          <Item>Item 4</Item>
+        </List>
       </Root>
     );
   }
