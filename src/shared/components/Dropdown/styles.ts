4713563faba1c6ee6a4e820ab4336147a4fb20ae
xnerhu <xnerhu@gmail.com>
2018-07-24 15:45:23 +0200

    Add icon in drop-down menu

--- src/shared/components/Dropdown/styles.ts
+++ src/shared/components/Dropdown/styles.ts
@@ -3,6 +3,9 @@ import styled, { StyledComponentClass } from 'styled-components';
 
 import opacity from '../../defaults/opacity';
 import typography from '../../mixins/typography';
+import images from '../../mixins/images';
+
+const dropDownIcon = require('../../icons/drop-down.svg');
 
 export const Root = styled.div`
   width: 128px;
@@ -10,8 +13,9 @@ export const Root = styled.div`
   background-color: rgba(0, 0, 0, 0.06);
   border-radius: 4px;
   padding-left: 16px;
-  padding-right: 16px;
+  padding-right: 8px;
   display: flex;
+  justify-content: space-between;
   align-items: center;
 `;
 
@@ -19,4 +23,13 @@ export const Selected = styled.div`
   font-size: 14px;
 
   ${typography.robotoRegular()};
+`;
+
+export const Icon = styled.div`
+  width: 24px;
+  height: 24px;
+  opacity: ${opacity.light.inactiveIcon};
+  background-image: url(${dropDownIcon});
+
+  ${images.center('24px', 'auto')};
 `;
