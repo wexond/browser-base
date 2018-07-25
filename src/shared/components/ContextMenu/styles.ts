4d5d6d59f9155886298e67c8bcc604290c5ecd28
Sential <sentialx@gmail.com>
2018-07-26 00:01:53 +0200

    Fix context menu hiding
    Former-commit-id: 6b4a479258070a51594a6c5636939a638a94c5e1 [formerly 6b4a479258070a51594a6c5636939a638a94c5e1 [formerly bd503d045919fb1dff974362372966117da1cf34]]
    Former-commit-id: 2e56cc7e53aeb89c65c2139b64555d95cb3b227c
    Former-commit-id: 334fd12a8f4b9cc906e8d191e63542c2cb3ba775

--- src/shared/components/ContextMenu/styles.ts
+++ src/shared/components/ContextMenu/styles.ts
@@ -8,18 +8,18 @@ export interface StyledMenuProps {
 }
 
 export const StyledMenu = styled.div`
-  border-radius: 2px;
+  border-radius: 4px;
   height: 0;
   background-color: white;
   overflow: hidden;
-  display: block;
-  padding-top: ${({ dense }) => (dense ? 4 : 8)}px;
-  padding-bottom: ${({ dense }) => (dense ? 4 : 8)}px;
+  display: inline-block;
 
-  box-shadow: ${shadows(5)};
+  box-shadow: ${shadows(8)};
 
   opacity: ${({ visible }: StyledMenuProps) => (visible ? 1 : 0)};
   margin-top: ${({ visible }) => (visible ? 0 : -20)}px;
   width: ${({ width }) => width}px;
   pointer-events: ${({ visible }) => (visible ? 'auto' : 'none')};
+  padding-top: ${({ dense }) => (dense ? 4 : 8)}px;
+  padding-bottom: ${({ dense }) => (dense ? 4 : 8)}px;
 `;
