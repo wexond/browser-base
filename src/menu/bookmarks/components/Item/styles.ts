7c39c83d726ffa38b48f33c5fa8d62e622f13ab7
xnerhu <xnerhu@gmail.com>
2018-07-25 20:47:52 +0200

    Add title highlighting on folder hover in bookmark

--- src/menu/bookmarks/components/Item/styles.ts
+++ src/menu/bookmarks/components/Item/styles.ts
@@ -1,6 +1,22 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import * as PageItem from '../../../../shared/components/PageItem';
 
 export const Title = styled(PageItem.Title)`
-  margin-left: 24px;
+  margin-left: 12px;
+  margin-right: 12px;
+  padding: 8px;
+  border-radius: 4px;
+  will-change: background-color;
+  transition: 0.2s background-color;
+
+  ${({ isFolder }: { isFolder: boolean }) => css`
+    ${isFolder
+      && `
+      cursor: text;
+
+      &:hover {
+        background-color: rgba(0, 0, 0, 0.06);
+      }
+    `};
+  `};
 `;
