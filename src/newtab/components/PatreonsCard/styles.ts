388254b5fdad0038603e460a1971483145dada70
xnerhu <xnerhu@gmail.com>
2018-07-26 00:07:22 +0200

    Add patreons card

---
+++ src/newtab/components/PatreonsCard/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import * as Card from '../../../shared/components/Card';
+
+export const Content = styled(Card.Content)`
+  padding-top: 8px;
+  padding-bottom: 8px;
+`;
+
+export const Header = styled(Card.Header)`
+  padding-bottom: 0px;
+`;
