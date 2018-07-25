b829a3c3f97d63c943d9278b787787e1e1241837
AhmedMostafa16 <amk67482@gmail.com>
2018-07-25 05:02:19 +0200

    Add CountryCode Requester

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -5,3 +5,5 @@ export const NEWS_API_KEY = '246a3ebb9cb34dc68c9c9f6856c14ed5';
 export const TIME_ZONE_API_KEY = 'AIzaSyCdSPuvolIItonMERz3EpFAeB1oH7ItfYc';
 
 export const EASE_FUNCTION = 'cubic-bezier(0.19, 1, 0.22, 1)';
+
+export const IPGEO_API_KEY = '8a6cb23234fd4a84a1e3d0d119bbaba0';
