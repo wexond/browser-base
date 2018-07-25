bf20de33a630b950df26b517baa689f4cdd8cbcb
xnerhu <xnerhu@gmail.com>
2018-07-25 20:11:15 +0200

    Code clean up in new tab, rewrite weather API

---
+++ src/newtab/models/weather-daily-item.ts
@@ -0,0 +1,8 @@
+export default interface WeatherDailyItem {
+  temp: number;
+  description: string;
+  precipitation: number;
+  winds: number;
+  icon: string;
+  date: Date;
+} // eslint-disable-line
