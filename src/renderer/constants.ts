969d1c16eb32fc8d646e60d1d40a9a45d1c07c21
Sential <sentialx@gmail.com>
2018-07-31 19:38:36 +0200

    New project structure (part 1)
    Former-commit-id: b8249220b1db0f10b82f5afc5092694c6a32605d [formerly b8249220b1db0f10b82f5afc5092694c6a32605d [formerly 9d419ce93388782c205f575e517ce0fe31ddf45c]]
    Former-commit-id: 79e81b2266820c706a4ae299b3459d85de4bfe11
    Former-commit-id: aea3275dcd9e592f87a5bd794d7971b627dd0cff

--- src/renderer/constants.ts
+++ src/renderer/constants.ts
@@ -21,3 +21,11 @@ export const MENU_CONTENT_MAX_WIDTH = 640;
 export const WORKSPACE_FOLDER_SIZE = 96;
 export const WORKSPACE_ICON_SIZE = 16;
 export const WORKSPACE_MAX_ICONS_COUNT = 9;
+
+export const WEATHER_API_KEY = '979e1265d1057789445790a1cda05186';
+
+export const NEWS_API_KEY = '246a3ebb9cb34dc68c9c9f6856c14ed5';
+
+export const TIME_ZONE_API_KEY = 'AIzaSyCdSPuvolIItonMERz3EpFAeB1oH7ItfYc';
+
+export const EASE_FUNCTION = 'cubic-bezier(0.19, 1, 0.22, 1)';
