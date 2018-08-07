d1d1ee679caa083174b4d67b82e588beda95e669
xnerhu <xnerhu@gmail.com>
2018-08-07 22:59:47 +0200

    Improve binding keys

--- src/interfaces/key-binding.ts
+++ src/interfaces/key-binding.ts
@@ -1,10 +1,4 @@
 export interface KeyBinding {
   key?: string;
-  keyMinRange?: number;
-  keyMaxRange?: number;
-  altKey?: boolean;
-  ctrlKey?: boolean;
-  metaKey?: boolean;
   command?: string;
-  isRange?: boolean;
 }
