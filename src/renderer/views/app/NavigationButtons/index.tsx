5cedf5ce0325ca339b76bdde5fa6272169df4151
unknown <xnerhu@gmail.com>
2018-08-10 18:03:30 +0200

    Fix navigation buttons margin on Windows
    Former-commit-id: 50c782d637b5d74f7a3da2cb2843c5064efed138 [formerly 50c782d637b5d74f7a3da2cb2843c5064efed138 [formerly f226a3f09322b31e020a208457198177b234869b]]
    Former-commit-id: 3d24dc6d0e323fb3dd6b64f15a25922b590e4281
    Former-commit-id: 77d5a4208717b73e4732246c83a8e4366e220629

--- src/renderer/views/app/NavigationButtons/index.tsx
+++ src/renderer/views/app/NavigationButtons/index.tsx
@@ -21,7 +21,7 @@ export default class NavigationButtons extends React.Component {
 
   public render() {
     return (
-      <StyledContainer isFullscreen={store.isFullscreen}>
+      <StyledContainer>
         <Toolbar.Button
           disabled={!store.navigationState.canGoBack}
           size={24}
@@ -35,7 +35,11 @@ export default class NavigationButtons extends React.Component {
           icon={icons.forward}
           onClick={this.onForwardClick}
         />
-        <Toolbar.Button size={20} icon={icons.refresh} onClick={this.onRefreshClick} />
+        <Toolbar.Button
+          size={20}
+          icon={icons.refresh}
+          onClick={this.onRefreshClick}
+        />
       </StyledContainer>
     );
   }
