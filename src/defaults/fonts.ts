b417997331fb685fef05929625f2829fbcd3cfd4
unknown <xnerhu@gmail.com>
2018-08-17 13:26:04 +0200

    Add objects in utils
    Former-commit-id: c091fc99107a96ff7d3988bced54603fe865abc1 [formerly 8ee84c10766a403f9fcc83af9005b543fce2a2ed]
    Former-commit-id: eba798fc93fbea61b81a781b5eb3485fc23b546e

---
+++ src/defaults/fonts.ts
@@ -0,0 +1,6 @@
+/* eslint global-require: 0 */
+export const fonts = {
+  robotoRegular: require('../renderer/resources/fonts/Roboto-Regular.ttf'),
+  robotoMedium: require('../renderer/resources/fonts/Roboto-Medium.ttf'),
+  robotoLight: require('../renderer/resources/fonts/Roboto-Light.ttf'),
+};
---
+++ src/utils/objects.ts
@@ -0,0 +1,30 @@
+export const isObject = (item: any) => typeof item === 'object';
+
+export const merge = (
+  target: any,
+  source: any,
+  onlyNotExisting = false,
+): any => {
+  target = { ...target };
+
+  if (source == null) {
+    return target;
+  }
+
+  source = { ...source };
+
+  if (isObject(target) && isObject(source)) {
+    Object.keys(source).forEach((key: string) => {
+      if (isObject(source[key])) {
+        target[key] = merge(target[key], source[key], onlyNotExisting);
+      } else if (
+        !onlyNotExisting ||
+        (target[key] == null || target[key] === '')
+      ) {
+        target[key] = source[key];
+      }
+    });
+  }
+
+  return target;
+};
