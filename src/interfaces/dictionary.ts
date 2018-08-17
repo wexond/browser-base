bce8b0dc3e9036e4f825151a5a69d62c3ddf5604
unknown <xnerhu@gmail.com>
2018-08-17 19:13:22 +0200

    Update dictionary interface

--- src/interfaces/dictionary.ts
+++ src/interfaces/dictionary.ts
@@ -1,1 +1,64 @@
-export interface Dictionary {}
+export interface Dictionary {
+  locale: string;
+  languageCode: string;
+  languageName: string;
+  languageNativeName: string;
+  countryCode: string;
+  countryName: string;
+  addressBar: {
+    search: string;
+  };
+  suggestions: {
+    mostVisited: string;
+    bookmarks: string;
+    history: string;
+    googleSuggestions: string;
+    searchInGoogle: string;
+    openWebsite: string;
+  };
+  newTab: {
+    precipitation: string;
+    winds: string;
+  };
+  history: {
+    title: string;
+    clearHistory: string;
+  };
+  bookmarks: {
+    title: string;
+  };
+  settings: {
+    title: string;
+  };
+  extensions: {
+    title: string;
+  };
+  about: {
+    title: string;
+    wexondVersion: string;
+    reactVersion: string;
+    electronVersion: string;
+    nodejsVersion: string;
+  };
+  selecting: {
+    selectAll: string;
+    deselectAll: string;
+    deleteSelected: string;
+  };
+  dateAndTime: {
+    months: string[];
+    days: string[];
+    daysShort: string[];
+    today: string;
+    yesterday: string;
+    ago: string;
+    oneMinute: string;
+    oneHour: string;
+    minutes: string;
+    hours: string;
+  };
+  general: {
+    expand: string;
+    collapse: string;
+  };
+}
