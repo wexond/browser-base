b023e18994e62a27ed77e0a9ac8413f374c4456f
unknown <xnerhu@gmail.com>
2018-08-17 13:26:04 +0200

    Add objects in utils
    Former-commit-id: c264b53c11912e2b50c9a2839a0ea26149e096be [formerly c264b53c11912e2b50c9a2839a0ea26149e096be [formerly 87781b1d5e21830315930e24fb984a8eef186ca8]]
    Former-commit-id: 7c257081f8032e356631d92009e44730f9d3eece
    Former-commit-id: f9fda348c824fa845ffd0d809a3c4dc334a6ce52

---
+++ src/defaults/transparency.ts
@@ -0,0 +1,26 @@
+export const opacity = {
+  light: {
+    primaryText: 0.87,
+    secondaryText: 0.54,
+    disabledText: 0.38,
+    activeIcon: 1,
+    inactiveIcon: 0.54,
+    disabledIcon: 0.38,
+    dividers: 0.12,
+    activeControl: 1,
+    inactiveControl: 0.54,
+    disabledControl: 0.26,
+  },
+  dark: {
+    primaryText: 1,
+    secondaryText: 0.7,
+    disabledText: 0.3,
+    activeIcon: 1,
+    inactiveIcon: 0.54,
+    disabledIcon: 0.38,
+    dividers: 0.2,
+    activeControl: 1,
+    inactiveControl: 0.7,
+    disabledControl: 0.3,
+  },
+};
