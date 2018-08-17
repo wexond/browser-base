190c8805b20b9a944d866ca260984cacdefbd1ee
unknown <xnerhu@gmail.com>
2018-08-17 15:46:22 +0200

    Update tab-group model

--- src/renderer/app/models/tab-group.ts
+++ src/renderer/app/models/tab-group.ts
@@ -1,1 +1,12 @@
-export class TabGroup {}
+import { observable } from 'mobx';
+
+export class TabGroup {
+  @observable
+  public id: number;
+
+  @observable
+  public name: string;
+
+  @observable
+  public selectedTab: number;
+}
