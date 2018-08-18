24f5715680e19ffa8cb8c3d5119c008db8cabdb4
Mikołaj Palkiewicz <xnerhu@gmail.com>
2018-08-18 16:57:12 +0200

    Update button styles
    Former-commit-id: 1e2c29eb18c622042ddbb62b154e05e7c8f7b642 [formerly 431218d2ce82acd6dafedb712b136ac791f4769b]
    Former-commit-id: d397ba4d2a8772eaaca14554fc3a453bdd9e3762

--- src/renderer/components/Button/styles.ts
+++ src/renderer/components/Button/styles.ts
@@ -1,146 +1,82 @@
 import styled, { css } from 'styled-components';
-import { transparency } from '../../../defaults/transparency';
-import { ButtonType } from '../../../enums';
-import {
-  getComponentColor,
-  getComponentOpacity,
-} from '../../../utils/component-color';
-import { button, centerImage, shadows } from '../../mixins';
+
+import { opacity } from '../../../defaults/opacity';
+import { button, shadows } from '../../mixins';
+import { getComponentColor } from '../../../utils/component-color';
 
 type UITheme = 'light' | 'dark';
-
-const isTransparent = (type: ButtonType) =>
-  type === ButtonType.Outlined || type === ButtonType.Text;
-
-const getPadding = (icon: boolean) => (icon ? 8 : 16);
-
-const getBackground = (
-  background: string,
-  disabled: boolean,
-  type: ButtonType,
-  theme: UITheme,
-) => {
-  if (isTransparent(type)) {
-    return 'transparent';
-  }
-  return getComponentColor(background, true, disabled, theme);
-};
-
-const getBorder = (type: ButtonType, theme: UITheme) => {
-  if (type === ButtonType.Outlined) {
-    const rgb = theme === 'light' ? 0 : 255;
-    const alpha =
-      theme === 'light' ? transparency.light.dividers : transparency.dark.dividers;
-
-    return `1px solid rgba(${rgb}, ${rgb}, ${rgb}, ${alpha})`;
-  }
-
-  return 'unset';
-};
-
-const getBoxShadow = (type: ButtonType, disabled: boolean, z: number = 2) => {
-  if (!disabled && !isTransparent(type)) {
-    return shadows(z);
-  }
-  return 'unset';
-};
-
-const iconInvertColors = (
-  white: boolean,
-  theme: UITheme,
-  disabled: boolean,
-) => {
-  if (disabled) {
-    if (theme === 'dark') {
-      return 'filter: invert(100%);';
-    }
-  } else if (white) {
-    return 'filter: invert(100%);';
-  }
-
-  return null;
-};
 
 export interface StyledButtonProps {
   background: string;
   foreground: string;
-  icon: boolean;
   disabled: boolean;
-  type: ButtonType;
+  isContained: boolean;
+  isOutlined: boolean;
+  isText: boolean;
   theme: UITheme;
 }
+
+export const getBorder = (isOutlined: boolean, theme: UITheme) => {
+  if (isOutlined) {
+    const rgb = theme === 'light' ? 0 : 255;
+    const alpha =
+      theme === 'light' ? opacity.light.dividers : opacity.dark.dividers;
+    return `1px solid rgba(${rgb}, ${rgb}, ${rgb}, ${alpha})`;
+  }
+  return 'unset';
+};
 
 export const StyledButton = styled.div`
   display: inline-flex;
   min-width: 64px;
   height: 36px;
+  padding-left: 16px;
   padding-right: 16px;
   border-radius: 4px;
   align-items: center;
   justify-content: center;
   position: relative;
   overflow: hidden;
-  transition: 0.2s box-shadow;
   white-space: nowrap;
   overflow: hidden;
+  transition: 0.2s box-shadow;
 
   ${button()};
 
   ${({
     background,
     foreground,
-    icon,
     disabled,
-    type,
+    isContained,
+    isOutlined,
+    isText,
     theme,
   }: StyledButtonProps) => css`
-    background-color: ${getBackground(background, disabled, type, theme)};
+    background-color: ${isOutlined || isText
+      ? 'transparent'
+      : getComponentColor(background, true, disabled, theme)};
+
+    box-shadow: ${!disabled && !isOutlined && !isText ? shadows(2) : 'unset'};
     color: ${getComponentColor(foreground, true, disabled, theme, false)}};
-    border: ${getBorder(type, theme)};
-    box-shadow: ${getBoxShadow(type, disabled)};
-    padding-left: ${getPadding(icon)}px;
+    border: ${getBorder(isOutlined, theme)};
     cursor: ${disabled ? 'unset' : 'pointer'};
+    pointer-events: ${disabled ? 'none' : 'all'};
 
-    &:hover .overlay {
-      opacity: ${disabled ? 0 : 0.12};
-      box-shadow: ${getBoxShadow(type, disabled, 3)};
+    &::before {
+      content: '';
+      display: block;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      position: absolute;
+      background-color: ${foreground};
+      opacity: 0;
+      transition: 0.2s opacity;
     }
-  `};
-`;
 
-export interface IconProps {
-  src: string;
-  white: boolean;
-  disabled: boolean;
-  theme: UITheme;
-}
-
-export const Icon = styled.div`
-  width: 18px;
-  height: 18px;
-  margin-left: 4px;
-  margin-right: 8px;
-
-  ${centerImage('18px', 'auto')};
-
-  ${({ src, white, disabled, theme }: IconProps) => css`
-    background-image: url(${src});
-    opacity: ${getComponentOpacity(true, disabled, theme, false)};
-
-    ${iconInvertColors(white, theme, disabled)};
-  `};
-`;
-
-export const Overlay = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  opacity: 0;
-  transition: 0.2s opacity;
-
-  ${({ color }: { color: string }) => css`
-    background-color: ${color};
+    &:hover::before {
+      opacity: 0.12;
+    }
   `};
 `;
