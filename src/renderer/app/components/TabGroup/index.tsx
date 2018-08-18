da3a5d82c206719f9a5cb788b06b8033c9c18cc2
Sential <sentialx@gmail.com>
2018-08-19 01:22:58 +0200

    Fix scrollbar
    Former-commit-id: 88aec6ddd69b06952e3bb8192b703b580e3f4cfb [formerly 622952e76cdd522fc96f3a3cd4a641f085b80afc]
    Former-commit-id: 872f32932ea16be28986fdbd03700af63d2d2db4

--- src/renderer/app/components/TabGroup/index.tsx
+++ src/renderer/app/components/TabGroup/index.tsx
@@ -5,9 +5,24 @@ import { TabGroup } from '../../models';
 import store from '@app/store';
 import { StyledTabGroup } from './styles';
 import Tab from '../Tab';
+import { observe } from 'mobx';
+import { TAB_ANIMATION_DURATION } from '~/constants';
 
 @observer
 export default class extends React.Component<{ tabGroup: TabGroup }> {
+  public componentDidMount() {
+    observe(this.props.tabGroup.tabs, (change: any) => {
+      console.log(change);
+      if (change.addedCount > 0 && change.removedCount === 0) {
+        if (store.tabbarStore.scrollbarRef) {
+          store.tabbarStore.scrollbarRef.scrollToEnd(
+            TAB_ANIMATION_DURATION * 1000,
+          );
+        }
+      }
+    });
+  }
+
   public render() {
     const { tabGroup } = this.props;
 
