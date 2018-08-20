13f90a158db88094c6ea9259805914f65b974a7f
unknown <xnerhu@gmail.com>
2018-08-20 17:17:49 +0200

    Rewrite keyboard-shortcuts

--- src/utils/keyboard-shortcuts.ts
+++ src/utils/keyboard-shortcuts.ts
@@ -7,58 +7,36 @@ import { getPath } from './paths';
 
 const defaultKeyBindings = require('../../static/defaults/key-bindings.json');
 
-export const bindKeys = (bindings: KeyBinding[], reset = true) => {
-  Mousetrap.reset();
-
-  for (let i = 0; i < bindings.length; i++) {
-    const binding = bindings[i];
-    const digitIndex = binding.key.indexOf('digit');
-
-    if (digitIndex === -1) {
+export const bindKeys = (bindings: KeyBinding[]) => {
+  for (const binding of bindings) {
+    if (!binding.key.includes('digit')) {
       Mousetrap.bind(binding.key, commands[binding.command]);
     } else {
-      const firstPart = binding.key.substring(0, digitIndex);
-
-      for (let x = 0; x <= 9; x++) {
-        Mousetrap.bind(firstPart + x, commands[binding.command]);
+      for (let i = 0; i <= 9; i++) {
+        Mousetrap.bind(
+          binding.key.replace('digit', i),
+          commands[binding.command],
+        );
       }
     }
   }
 };
 
-export const isKeyBindingChanged = (command: string, key: string) => {
-  for (let i = 0; i < defaultKeyBindings.length; i++) {
-    const binding = defaultKeyBindings[i];
-
-    if (binding.command === command && typeof binding.key === 'string') {
-      return binding.key !== key;
-    }
-  }
-
-  return null;
-};
-
 export const getKeyBindings = async () => {
   const list: KeyBinding[] = [];
   const path = getPath('key-bindings.json');
-
   const data = fs.readFileSync(path, 'utf8');
-  const json = JSON.parse(data);
+  const userBindings = JSON.parse(data);
 
-  for (let i = 0; i < json.length; i++) {
-    if (!json[i].command || !json[i].key) {
-      continue;
-    }
+  for (const binding of defaultKeyBindings as KeyBinding[]) {
+    const userBinding: KeyBinding = userBindings.filter(
+      (r: any) => r.command === binding.command,
+    )[0];
 
-    const isChanged = isKeyBindingChanged(json[i].command, json[i].key);
+    if (userBinding != null) binding.key = userBinding.key;
+    binding.isChanged = userBinding != null;
 
-    const binding: KeyBinding = {
-      command: json[i].command,
-      key: json[i].key,
-      isChanged,
-    };
-
-    list.push(binding);
+    list.push(binding as KeyBinding);
   }
 
   return list;
