dfed235b7e3d43679c16edd38d09668e227e5cc3
unknown <xnerhu@gmail.com>
2018-08-20 19:15:29 +0200

    Move favicons functions in utils to store

--- src/renderer/app/store/favicons.ts
+++ src/renderer/app/store/favicons.ts
@@ -4,6 +4,36 @@ import { Favicon } from '~/interfaces';
 export class FaviconsStore {
   public favicons: { [key: string]: string } = {};
 
+  public getFavicons = (query: Favicon = {}) => {
+    return new Promise((resolve: (favicons: Favicon[]) => void, reject) => {
+      databases.favicons.find(query, (err: any, docs: Favicon[]) => {
+        if (err) return reject(err);
+        resolve(docs);
+      });
+    });
+  };
+
+  public addFavicon = async (url: string) => {
+    const favicons = await this.getFavicons({ url });
+
+    if (favicons.length === 0) {
+      fetch(url)
+        .then(res => res.blob())
+        .then(blob => {
+          const reader = new FileReader();
+          reader.onload = () => {
+            const generatedBuffer: any = reader.result;
+
+            databases.favicons.insert({
+              url,
+              favicon: Buffer.from(generatedBuffer),
+            });
+          };
+          reader.readAsArrayBuffer(blob);
+        });
+    }
+  };
+
   public load() {
     return new Promise(async resolve => {
       databases.favicons.find({}, (err: any, docs: Favicon[]) => {
