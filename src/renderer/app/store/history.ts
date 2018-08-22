117fc2b1ddd1fef1aaddaf6506cf3cc55d88ad94
unknown <xnerhu@gmail.com>
2018-08-22 18:41:57 +0200

    Add loading history
    Former-commit-id: acf7fa82696ae898f96d8e447a1373e4ed246420 [formerly c10440c4e09b519e9ec807568c6909b5f739acad]
    Former-commit-id: a30a953b58978910eeddf5ad7b2614569e54dae6

--- src/renderer/app/store/history.ts
+++ src/renderer/app/store/history.ts
@@ -2,6 +2,7 @@ import { observable } from 'mobx';
 import store from '.';
 import { HistoryItem, HistorySection } from '~/interfaces';
 import { formatDate } from '~/utils';
+import { databases } from '~/defaults/databases';
 
 export class HistoryStore {
   @observable
@@ -9,6 +10,16 @@ export class HistoryStore {
 
   @observable
   public selectedItems: string[] = [];
+
+  public load() {
+    return new Promise(async resolve => {
+      databases.history.find({}, (err: any, items: HistoryItem[]) => {
+        if (err) return console.warn(err);
+        this.historyItems = items;
+        resolve();
+      });
+    });
+  }
 
   public getItems(filter = '') {
     return this.historyItems
