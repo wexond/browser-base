0c6f14a275974467a39fee71cb59ef84add43bb7
unknown <xnerhu@gmail.com>
2018-08-25 23:24:19 +0200

    Add key-bindings menu

--- src/utils/key-bindings.ts
+++ src/utils/key-bindings.ts
@@ -1,10 +1,11 @@
-import fs from 'fs';
+import { writeFileSync, readFileSync } from 'fs';
 import Mousetrap from 'mousetrap';
 
 import { commands } from '~/defaults/commands';
 import { defaultPaths } from '~/defaults';
 import { KeyBinding } from '../interfaces';
 import { getPath } from './paths';
+import store from '@app/store';
 
 const defaultKeyBindings = require('../../static/defaults/key-bindings.json');
 
@@ -25,7 +26,7 @@ export const bindKeys = (bindings: KeyBinding[]) => {
 
 export const getKeyBindings = async () => {
   const list: KeyBinding[] = [];
-  const data = fs.readFileSync(getPath(defaultPaths.keyBindings), 'utf8');
+  const data = readFileSync(getPath(defaultPaths.keyBindings), 'utf8');
   const userBindings = JSON.parse(data);
 
   for (const binding of defaultKeyBindings as KeyBinding[]) {
@@ -40,4 +41,11 @@ export const getKeyBindings = async () => {
   }
 
   return list;
+};
+
+export const saveKeyBindings = () => {
+  const path = getPath(defaultPaths.keyBindings);
+  const changed = store.keyBindingsStore.keyBindings.filter(e => e.isChanged);
+
+  writeFileSync(path, JSON.stringify(changed, null, 2), 'utf8');
 };
