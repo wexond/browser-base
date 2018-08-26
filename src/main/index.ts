f2c634147b1e4109d0f7118e2f42450bbfba3269
Sential <sentialx@gmail.com>
2018-08-27 00:33:39 +0200

    Some fixes

--- src/main/index.ts
+++ src/main/index.ts
@@ -8,6 +8,7 @@ import { Global } from './interfaces';
 import { createWindow, getPath, registerProtocols } from './utils';
 import { defaultPaths, filesContent } from '~/defaults';
 import { matchesPattern } from '~/utils/url';
+import { runWebRequestService } from '~/main/services/web-request';
 
 app.setPath('userData', resolve(homedir(), '.wexond'));
 
@@ -19,38 +20,6 @@ global.extensions = {};
 global.backgroundPages = {};
 global.locale = 'en-US';
 global.databases = {};
-
-const eventListeners: any = {};
-
-const getRequestType = (type: string): any => {
-  if (type === 'mainFrame') return 'main_frame';
-  if (type === 'subFrame') return 'sub_frame';
-  if (type === 'cspReport') return 'csp_report';
-  return type;
-};
-
-const getDetails = (details: any) => {};
-
-const matchesFilter = (filter: any, url: string) => {
-  if (filter && Array.isArray(filter.urls)) {
-    for (const item of filter.urls) {
-      if (matchesPattern(item, url)) {
-        return true;
-      }
-    }
-  }
-  return false;
-};
-
-const getTabIdByWebContentsId = async (webContentsId: number) => {
-  return new Promise((resolve: (result: number) => void) => {
-    mainWindow.webContents.send('get-tab-id', { webContentsId });
-
-    ipcMain.once('get-tab-id', (e: any, tabId: number) => {
-      resolve(tabId);
-    });
-  });
-};
 
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
@@ -75,95 +44,19 @@ app.on('ready', () => {
 
   mainWindow = createWindow();
 
-  session
-    .fromPartition('persist:webviewsession')
-    .webRequest.onBeforeSendHeaders(
-      { urls: ['http://*/*', 'https://*/*'] },
-      (details: any, callback: any) => {
-        details.requestHeaders['DNT'] = '1';
-        callback({ cancel: false, requestHeaders: details.requestHeaders });
-      },
-    );
+  // session
+  //   .fromPartition('persist:webviewsession')
+  //   .webRequest.onBeforeSendHeaders(
+  //     { urls: ['http://*/*', 'https:// */*'] },
+  //     (details: any, callback: any) => {
+  //       details.requestHeaders['DNT'] = '1';
+  //       callback({ cancel: false, requestHeaders: details.requestHeaders });
+  //     },
+  //   );
 
   runAutoUpdaterService(mainWindow);
   runExtensionsService(mainWindow);
-
-  session
-    .fromPartition('persist:webviewsession')
-    .webRequest.onBeforeRequest((details, callback) => {
-      let callbackCalled = false;
-
-      const newDetails: chrome.webRequest.WebRequestDetails = {
-        requestId: details.id.toString(),
-        url: details.url,
-        method: details.method,
-        frameId: 0,
-        parentFrameId: -1,
-        tabId: 0,
-        type: getRequestType(details.resourceType),
-        timeStamp: Date.now(),
-      };
-
-      let isIntercepted = false;
-
-      if (Array.isArray(eventListeners.onBeforeRequest)) {
-        for (const event of eventListeners.onBeforeRequest) {
-          if (matchesFilter(event.filters, details.url)) {
-            const contents = webContents.fromId(event.webContentsId);
-            contents.send(
-              `api-webRequest-intercepted-onBeforeRequest-${event.id}`,
-              newDetails,
-            );
-
-            ipcMain.once(
-              `api-webRequest-response-onBeforeRequest-${event.id}`,
-              (event: any, res: any) => {
-                if (!callbackCalled) {
-                  if (res) {
-                    callback(res);
-                  } else {
-                    callback({ cancel: false });
-                  }
-
-                  callbackCalled = true;
-                }
-              },
-            );
-
-            isIntercepted = true;
-          }
-        }
-      }
-
-      if (!isIntercepted) {
-        callback({ cancel: false });
-      }
-    });
-
-  ipcMain.on('api-add-webRequest-listener', (e: any, data: any) => {
-    const { id, name, filters } = data;
-
-    const item: any = {
-      id,
-      filters,
-      webContentsId: e.sender.id,
-    };
-
-    if (eventListeners[name]) {
-      eventListeners[name].push(item);
-    } else {
-      eventListeners[name] = [item];
-    }
-  });
-
-  ipcMain.on('api-remove-webRequest-listener', (e: any, data: any) => {
-    const { id, name } = data;
-    if (eventListeners[name]) {
-      eventListeners[name] = eventListeners[name].filter(
-        (x: any) => x.id !== id && x.webContentsId !== e.sender.id,
-      );
-    }
-  });
+  runWebRequestService(mainWindow);
 
   mainWindow.on('closed', () => {
     mainWindow = null;
