c5b7d6191429d842bcef2b0201ed7d25ce5555e8
unknown <xnerhu@gmail.com>
2018-08-27 20:58:27 +0200

    Add experimental feature

--- src/preloads/dark-theme/index.ts
+++ src/preloads/dark-theme/index.ts
@@ -1,7 +1,9 @@
-import { rgbToHex } from '../../utils/colors';
+import { colorBrightness } from '../../utils/colors';
+
+const backgroundBrightness = 189;
 
 export const applyDarkTheme = () => {
-  const elements = document.querySelectorAll('body'); // body *
+  const elements = document.querySelectorAll('body, body *');
 
   for (let i = 0; i < elements.length; i++) {
     colorize(elements[i]);
@@ -11,10 +13,13 @@ export const applyDarkTheme = () => {
 export const colorize = (element: any) => {
   const style = window.getComputedStyle(element);
   const backgroundColor = style.getPropertyValue('background-color');
-  const hasColor = backgroundColor !== 'rgba(0, 0, 0, 0)';
 
-  if (hasColor) {
-    console.log(element, backgroundColor);
-    console.log(rgbToHex(backgroundColor));
+  if (
+    backgroundColor !== 'rgba(0, 0, 0, 0)' &&
+    colorBrightness(backgroundColor) >= backgroundBrightness
+  ) {
+    element.style.backgroundColor = '#212121';
   }
+
+  element.style.color = '#fff';
 };
