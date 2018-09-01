fe298108bc6fb18651512637f4f53be5f3b6f39c
unknown <xnerhu@gmail.com>
2018-09-01 18:15:40 +0200

    Fix toggling menu
    Former-commit-id: 0d3efb1f33b681d98250dcf732aa55b616cef815 [formerly 0d3efb1f33b681d98250dcf732aa55b616cef815 [formerly c9c5a1051d777c46e4ae30043da623797d93d0c3]]
    Former-commit-id: 9e0ac087d81218ca155b6075b40fcb705d5650b1
    Former-commit-id: 780257a0e2bc27c69109dba23088bb33e343eb75

--- src/renderer/app/components/Toolbar/index.tsx
+++ src/renderer/app/components/Toolbar/index.tsx
@@ -63,8 +63,13 @@ export default class Toolbar extends React.Component {
     store.tabsStore.menuVisible = true;
   };
 
-  public toggleMenu = () => {
+  public onMenuIconClick = () => {
     store.menuStore.visible = !store.menuStore.visible;
+  };
+
+  public onMenuIconMouseDown = (e: React.MouseEvent<any>) => {
+    e.preventDefault();
+    e.stopPropagation();
   };
 
   public render() {
@@ -90,7 +95,8 @@ export default class Toolbar extends React.Component {
           <BrowserAction browserAction={item} key={key} />
         ))}
         <ToolbarButton
-          onClick={this.toggleMenu}
+          onClick={this.onMenuIconClick}
+          onMouseDown={this.onMenuIconMouseDown}
           size={20}
           icon={icons.menu}
           style={{ marginRight: 4 }}
