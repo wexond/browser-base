1470fd91062649d88a682f3d612c80a4b7fbf358
unknown <xnerhu@gmail.com>
2018-09-01 16:26:49 +0200

    Improve menu design

--- src/renderer/app/store/menu.ts
+++ src/renderer/app/store/menu.ts
@@ -3,19 +3,4 @@ import { observable } from 'mobx';
 export class MenuStore {
   @observable
   public visible = false;
-
-  @observable
-  public selectedItem: number;
-
-  @observable
-  public hideContent = false;
-
-  @observable
-  public searchText = '';
-
-  public hide() {
-    this.visible = false;
-    this.selectedItem = null;
-    this.searchText = '';
-  }
 }
