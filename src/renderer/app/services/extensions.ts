b49865cad3b69345ff9551878bf09a4a42bdf871
Sential <sentialx@gmail.com>
2018-09-01 18:36:02 +0200

    Improve browser actions

--- src/renderer/app/services/extensions.ts
+++ src/renderer/app/services/extensions.ts
@@ -160,10 +160,23 @@ export const runExtensionsService = () => {
       extensionId: string,
       details: chrome.browserAction.BadgeTextDetails,
     ) => {
-      const browserAction = store.extensionsStore.getBrowserActionById(
-        extensionId,
-      );
-      browserAction.badgeText = details.text;
+      if (details.tabId) {
+        const browserAction = store.extensionsStore.queryBrowserAction({
+          extensionId,
+          tabId: details.tabId,
+        })[0];
+        if (browserAction) {
+          browserAction.badgeText = details.text;
+        }
+      } else {
+        store.extensionsStore
+          .queryBrowserAction({
+            extensionId,
+          })
+          .forEach(item => {
+            item.badgeText = details.text;
+          });
+      }
       const contents = remote.webContents.fromId(senderId);
       contents.send(API_BROWSER_ACTION_SET_BADGE_TEXT);
     },
