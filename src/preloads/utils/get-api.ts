12e2cc92142fb545d9273b3c905a82df33aabe2d
Sential <sentialx@gmail.com>
2018-09-02 19:58:46 +0200

    Refactor webRequest API
    Former-commit-id: 07918286d922f867fde1bad793fee0bcc78e6993 [formerly aee7df424e4afbecac38b3c04a866969ae99fb25]
    Former-commit-id: 0e2a3a2165df9efd18a6268c9f8ce92a9e49bfbc

--- src/preloads/utils/get-api.ts
+++ src/preloads/utils/get-api.ts
@@ -22,31 +22,7 @@ export const getAPI = (manifest: Manifest) => {
     runtime: runtime(manifest),
 
     // https://developer.chrome.com/extensions/webRequest
-    webRequest: {
-      ResourceType: {
-        CSP_REPORT: 'csp_report',
-        FONT: 'font',
-        IMAGE: 'image',
-        MAIN_FRAME: 'main_frame',
-        MEDIA: 'media',
-        OBJECT: 'object',
-        OTHER: 'other',
-        PING: 'ping',
-        SCRIPT: 'script',
-        STYLESHEET: 'stylesheet',
-        SUB_FRAME: 'sub_frame',
-        WEBSOCKET: 'websocket',
-        XMLHTTPREQUEST: 'xmlhttprequest',
-      },
-      onBeforeRequest: new WebRequestEvent('onBeforeRequest'),
-      onBeforeSendHeaders: new WebRequestEvent('onBeforeSendHeaders'),
-      onHeadersReceived: new WebRequestEvent('onHeadersReceived'),
-      onSendHeaders: new WebRequestEvent('onSendHeaders'),
-      onResponseStarted: new WebRequestEvent('onResponseStarted'),
-      onBeforeRedirect: new WebRequestEvent('onBeforeRedirect'),
-      onCompleted: new WebRequestEvent('onCompleted'),
-      onErrorOccurred: new WebRequestEvent('onErrorOccurred'),
-    },
+    webRequest: {},
 
     // https://developer.chrome.com/extensions/i18n
     i18n: {
