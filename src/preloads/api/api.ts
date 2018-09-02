c74197378ae08931d98dd2b2d960095000acf168
Sential <sentialx@gmail.com>
2018-09-02 19:49:08 +0200

    Refactor tabs API

--- src/preloads/api/api.ts
+++ src/preloads/api/api.ts
@@ -1,13 +1,17 @@
 import { Manifest } from '@/interfaces/extensions';
-import { Runtime, WebNavigation, Alarms, Storage } from '.';
+import { Runtime, WebNavigation, Alarms, Storage, Extension, Tabs } from '.';
 
 let manifest: Manifest;
+
+// https://developer.chrome.com/extensions/api_index
 
 export class API {
   public runtime = new Runtime(manifest);
   public webNavigation = new WebNavigation();
   public alarms = new Alarms(this);
   public storage = new Storage(this);
+  public extension = new Extension();
+  public tabs = new Tabs(this);
 
   // tslint:disable-next-line
   constructor(_manifest: Manifest) {
