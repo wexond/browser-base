6adaf54cbad03662d2dbfd4eecd6e7040c9cb0ae
Sential <sentialx@gmail.com>
2018-09-02 21:43:29 +0200

    Fix starting app errors
    Former-commit-id: 7831c89496b93832740c5dda11a57f7ef2877a7e [formerly 7831c89496b93832740c5dda11a57f7ef2877a7e [formerly 3ffeb36043bc5c1c0f91578330d21f2e36c0c54e]]
    Former-commit-id: bd6d00d6eb4ab95fc97d7760cb6cb0068d694292
    Former-commit-id: 92dbba918a8f57243f9d1df725acff1b420f5f06

--- src/shared/constants/dictionaries.ts
+++ src/shared/constants/dictionaries.ts
@@ -1,12 +1,12 @@
 export const locales = ['en-US', 'pl-PL'];
 export const localeBaseName = 'en-US';
 
-export const localeBase = require(`../../static/dictionaries/${localeBaseName}.json`);
+export const localeBase = require(`../../../static/dictionaries/${localeBaseName}.json`);
 
 export const getDictionary = (name: string) => {
   if (name === localeBaseName) return localeBase;
 
-  const locale = require(`../../static/dictionaries/${name}.json`);
+  const locale = require(`../../../static/dictionaries/${name}.json`);
   return {
     ...localeBase,
     ...locale,
