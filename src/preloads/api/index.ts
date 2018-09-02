c19a809dda1e116b84378da22b4544181c187e1e
Sential <sentialx@gmail.com>
2018-09-02 19:49:08 +0200

    Refactor tabs API

--- src/preloads/api/index.ts
+++ src/preloads/api/index.ts
@@ -3,5 +3,9 @@ export * from './runtime';
 export * from './extension';
 export * from './alarms';
 export * from './storage';
+export * from './tabs';
+export * from './web-request';
+export * from './i18n';
+export * from './browser-action';
 
 export * from './api';
