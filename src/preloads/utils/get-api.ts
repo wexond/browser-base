621233083fbfb28d058f8d3fe689ec6144dd3948
Sential <sentialx@gmail.com>
2018-09-02 20:08:22 +0200

    Refactor i18n API
    Former-commit-id: 87e7b5ca69214e1c4b5ce4db195f64103c844427 [formerly 87e7b5ca69214e1c4b5ce4db195f64103c844427 [formerly 3703beaa4fc6c602a350c92ff76cc199e91788f0]]
    Former-commit-id: 24d7d338e2d9159ec0a57301328307f7e86c145d
    Former-commit-id: 71cb5a2c765165d986d3fab94dd76e8248a51d98

--- src/preloads/utils/get-api.ts
+++ src/preloads/utils/get-api.ts
@@ -24,58 +24,7 @@ export const getAPI = (manifest: Manifest) => {
     // https://developer.chrome.com/extensions/webRequest
     webRequest: {},
 
-    // https://developer.chrome.com/extensions/i18n
-    i18n: {
-      getAcceptLanguages: (cb: any) => {
-        if (cb) {
-          cb(navigator.languages);
-        }
-      },
-      getMessage: (messageName: string, substitutions?: any) => {
-        if (messageName === '@@ui_locale') return 'en_US';
-
-        const { extensionId } = manifest;
-        const locale = remote.getGlobal('extensionsLocales')[extensionId];
-        const substitutionsArray = substitutions instanceof Array;
-
-        const item = locale[messageName];
-
-        if (item == null) return '';
-        if (substitutionsArray && substitutions.length >= 9) return null;
-
-        let message = item.message;
-
-        if (typeof item.placeholders === 'object') {
-          for (const placeholder in item.placeholders) {
-            message = replaceAll(
-              message,
-              `$${placeholder}$`,
-              item.placeholders[placeholder].content,
-            );
-          }
-        }
-
-        if (substitutionsArray) {
-          for (let i = 0; i < 9; i++) {
-            message = replaceAll(message, `$${i + 1}`, substitutions[i] || ' ');
-          }
-        }
-
-        return message;
-      },
-      getUILanguage: () => {
-        return navigator.language;
-      },
-      detectLanguage: (text: string, cb: any) => {
-        // TODO
-        if (cb) {
-          cb({
-            isReliable: false,
-            languages: [],
-          });
-        }
-      },
-    },
+    i18n: {},
 
     browserAction: {
       onClicked: {
