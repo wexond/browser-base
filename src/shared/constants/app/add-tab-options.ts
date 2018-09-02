f311c28f4a948ef836f2dea927d82ed16dee27ff
Sential <sentialx@gmail.com>
2018-09-02 21:58:51 +0200

    Fix extensions API

--- src/shared/constants/app/add-tab-options.ts
+++ src/shared/constants/app/add-tab-options.ts
@@ -1,4 +1,7 @@
-import { NEWTAB_URL } from '@/constants/app';
+export const NEWTAB_URL =
+  process.env.ENV === 'dev'
+    ? 'http://localhost:8080/newtab.html'
+    : 'wexond://newtab';
 
 export const defaultAddTabOptions: chrome.tabs.CreateProperties = {
   url: NEWTAB_URL,
