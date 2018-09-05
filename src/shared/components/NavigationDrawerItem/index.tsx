5b314fecdfc0d3cdf2ea9521118892c49ed8fe5b
unknown <xnerhu@gmail.com>
2018-09-05 20:14:10 +0200

    Some fixes

--- src/shared/components/NavigationDrawerItem/index.tsx
+++ src/shared/components/NavigationDrawerItem/index.tsx
@@ -7,36 +7,22 @@ interface Props {
   icon: string;
   title: string;
   visible?: boolean;
-  selected?: boolean;
-  id?: number;
   onClick?: (e?: React.MouseEvent<HTMLDivElement>) => void;
-  onClick2?: (
-    e?: React.MouseEvent<HTMLDivElement>,
-    element?: NavigationDrawerItem,
-  ) => void;
 }
 
 @observer
 export default class NavigationDrawerItem extends React.Component<Props, {}> {
   public static defaultProps = {
     visible: true,
-    selected: false,
-  };
-
-  private onClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    const { onClick, onClick2 } = this.props;
-
-    if (typeof onClick === 'function') onClick(e);
-    if (typeof onClick2 === 'function') onClick2(e, this);
   };
 
   public render() {
-    const { icon, title, selected } = this.props;
+    const { icon, title, onClick } = this.props;
 
     return (
-      <StyledItem onClick={this.onClick} selected={selected}>
-        <Icon selected={selected} src={icon} />
-        <Title selected={selected}>{title}</Title>
+      <StyledItem onClick={onClick}>
+        <Icon src={icon} />
+        <Title>{title}</Title>
       </StyledItem>
     );
   }
