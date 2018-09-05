c6a582795c9ab8b8fce6c174436a6e674019e39e
Sential <sentialx@gmail.com>
2018-09-05 20:10:48 +0200

    Fix history loading in production build
    Former-commit-id: ee00edef8d98971b3889e88a01e90f2c0f325dd9 [formerly ee00edef8d98971b3889e88a01e90f2c0f325dd9 [formerly 9c90d671262ed7d22a0e91c0337f390bceda8c01]]
    Former-commit-id: 6d126bb8f6a1b74bc1219db6614b0b825159516a
    Former-commit-id: 6f664286d89de6155dd775138d91904399150792

--- src/renderer/app/components/Menu/index.tsx
+++ src/renderer/app/components/Menu/index.tsx
@@ -15,7 +15,7 @@ interface Props {
 export default class extends React.Component<Props, {}> {
   public openTab = (page: string) => {
     store.tabsStore.addTab({
-      url: `http://localhost:8080/${page}.html`,
+      url: process.env.ENV === 'dev' ? `http://localhost:8080/${page}.html` : 'wexond://history',
       active: true,
     });
   };
