be05bdb8e6c5d550533ce329813bd6c5d2dcb1b2
unknown <xnerhu@gmail.com>
2018-09-07 18:23:51 +0200

    Add input in bookmarks

--- src/renderer/bookmarks/components/Item/index.tsx
+++ src/renderer/bookmarks/components/Item/index.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 import store from '@bookmarks/store';
 import { transparency, icons } from '~/shared/constants/renderer';
 import { Bookmark } from '~/shared/interfaces';
-import { Icon, PageItem } from '~/shared/components/PageItem';
-import { ActionIcon, Title } from './styles';
+import { Icon } from '~/shared/components/PageItem';
+import { Root, ActionIcon, Title, Input } from './styles';
 
 export interface Props {
   data: Bookmark;
@@ -13,12 +13,16 @@ export interface Props {
 
 @observer
 export default class BookmarkItem extends React.Component<Props> {
-  public onMouseEnter = () => {
-    this.props.data.hovered = true;
+  private input: HTMLInputElement;
+
+  public onTitleClick = () => {
+    this.props.data.inputVisible = true;
+    this.input.focus();
   };
 
-  public onMouseLeave = () => {
-    this.props.data.hovered = false;
+  public onInputBlur = () => {
+    this.props.data.inputVisible = false;
+    console.log(this.input.value);
   };
 
   public onRemoveClick = (e: React.MouseEvent<HTMLDivElement>) => {
@@ -30,8 +34,8 @@ export default class BookmarkItem extends React.Component<Props> {
 
   public render() {
     const { data } = this.props;
-
     const isFolder = data.type === 'folder';
+
     let opacity = 1;
     let favicon = data.favicon; // TODO
 
@@ -43,21 +47,23 @@ export default class BookmarkItem extends React.Component<Props> {
     const selected = store.selectedItems.indexOf(data._id) !== -1;
 
     return (
-      <PageItem
-        onMouseOver={this.onMouseEnter}
-        onMouseLeave={this.onMouseLeave}
-        selected={selected}
-      >
-        <Icon style={{ opacity }} icon={favicon} />
+      <Root selected={selected}>
+        <Icon icon={favicon} style={{ opacity }} />
         <div style={{ flex: 1 }}>
-          <Title>{data.title}</Title>
+          <Title onClick={this.onTitleClick}>{data.title}</Title>
         </div>
+        <Input
+          innerRef={r => (this.input = r)}
+          onBlur={this.onInputBlur}
+          visible={data.inputVisible}
+          placeholder="Name"
+        />
         <ActionIcon
+          className="DELETE-ICON"
           icon={icons.delete}
           onClick={this.onRemoveClick}
-          visible={data.hovered}
         />
-      </PageItem>
+      </Root>
     );
   }
 }
