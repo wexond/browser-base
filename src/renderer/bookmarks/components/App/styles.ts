fac82c921ff0cebddf34479dd938cd180c8e0c5f
unknown <xnerhu@gmail.com>
2018-09-07 19:24:09 +0200

    Improve bookmarks design

--- src/renderer/bookmarks/components/App/styles.ts
+++ src/renderer/bookmarks/components/App/styles.ts
@@ -13,3 +13,14 @@ export const PageContainer = styled.div`
   margin-left: 256px;
   position: relative;
 `;
+
+export const Content = styled.div`
+  width: calc(100% - 64px);
+  max-width: 640px;
+  margin: 24px auto;
+  overflow: hidden;
+  background-color: #fff;
+  border-radius: 4px;
+  box-shadow: 0 1px 1px 0 rgba(60, 64, 67, 0.08),
+    0 1px 3px 1px rgba(60, 64, 67, 0.16);
+`;
