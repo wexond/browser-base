843fb5e56038bb6ebf52119488455e48eca4163f
Sential <sentialx@gmail.com>
2018-09-30 17:57:03 +0200

    Small design fixes
    Former-commit-id: b74c76c3d944b03304fdd2d7f876ddabdd347892 [formerly b74c76c3d944b03304fdd2d7f876ddabdd347892 [formerly 3abaed61dde5c524b1b03564c1279827ce4bbd6f]]
    Former-commit-id: e10f8245f18e7dbaac8129b5f87eecdca2d1c1fd
    Former-commit-id: 784c9621816c42924515c28357e99a9910b8a04d

--- src/renderer/app/components/Dial/styles.ts
+++ src/renderer/app/components/Dial/styles.ts
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components';
 
 export const StyledDial = styled.div`
-  padding: 0px 8px 0px 8px;
+  padding: 0px 8px 8px 8px;
   flex-flow: row wrap;
 
   ${({ visible }: { visible: boolean }) => css`
