455a57b8eab50226c5b4d44468b6fd7fbd24d18e
Sential <sentialx@gmail.com>
2018-09-30 17:33:21 +0200

    Some minor fixes, improve popups animations

--- src/renderer/app/components/Dial/styles.ts
+++ src/renderer/app/components/Dial/styles.ts
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components';
 
 export const StyledDial = styled.div`
   padding: 0px 8px 0px 8px;
+  flex-flow: row wrap;
 
   ${({ visible }: { visible: boolean }) => css`
     display: ${visible ? 'flex' : 'none'};
