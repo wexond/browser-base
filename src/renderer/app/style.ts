a36532e747e6ba688d953f78a7074f589966815d
Sential <sentialx@gmail.com>
2018-12-25 19:46:37 +0100

    Fix fonts flickering
    Former-commit-id: 427dc79e826968772de0422f2367bd0013180da1 [formerly 245a48d9ec107eb204103eb9e7c107ffb8a7d916]
    Former-commit-id: 0d743aa5187f215331648d272949eaf1bf93747a

--- src/renderer/app/style.ts
+++ src/renderer/app/style.ts
@@ -1,28 +1,8 @@
 import { css } from 'styled-components';
 
-import { fonts } from '~/renderer/constants/fonts';
 import { body2 } from '~/shared/mixins';
 
 export const Style = css`
-  @font-face {
-    font-family: 'Roboto';
-    font-style: normal;
-    font-weight: 400;
-    src: url(${fonts.robotoRegular}) format('woff2');
-  }
-  @font-face {
-    font-family: 'Roboto';
-    font-style: normal;
-    font-weight: 500;
-    src: url(${fonts.robotoMedium}) format('woff2');
-  }
-  @font-face {
-    font-family: 'Roboto';
-    font-style: normal;
-    font-weight: 300;
-    src: url(${fonts.robotoLight}) format('woff2');
-  }
-
   body {
     user-select: none;
     cursor: default;
