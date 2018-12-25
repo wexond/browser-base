4ffe00f8f495c6326fa8b498dde1f68cdc572534
Sential <sentialx@gmail.com>
2018-12-25 19:46:37 +0100

    Fix fonts flickering

--- src/renderer/app/index.tsx
+++ src/renderer/app/index.tsx
@@ -3,6 +3,32 @@ import * as ReactDOM from 'react-dom';
 
 import App from './components/App';
 import store from './store';
+import { fonts } from '../constants';
+
+const styleElement = document.createElement('style');
+
+styleElement.textContent = `
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  src: url(${fonts.robotoRegular}) format('woff2');
+}
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 500;
+  src: url(${fonts.robotoMedium}) format('woff2');
+}
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 300;
+  src: url(${fonts.robotoLight}) format('woff2');
+}
+`;
+
+document.head.appendChild(styleElement);
 
 const render = (AppComponent: any) => {
   ReactDOM.render(<AppComponent />, document.getElementById('app'));
