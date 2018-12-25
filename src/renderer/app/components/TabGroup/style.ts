dd48cece1c67d9af36ef0f19ea58d27362f4a189
Sential <sentialx@gmail.com>
2018-12-25 18:04:50 +0100

    Small fixes for tabs animations
    Former-commit-id: b78854f118b1b7e3f4f930110ac4457362a09663 [formerly b78854f118b1b7e3f4f930110ac4457362a09663 [formerly ee8cc665581673746af97bc81fc0b9e41644aa84]]
    Former-commit-id: c2a7d51cabb76bbb1f9b028e0fa2b2aac96115a1
    Former-commit-id: cbd7da0aee4bed71332a54359da30c4fc2041a17

--- src/renderer/app/components/TabGroup/style.ts
+++ src/renderer/app/components/TabGroup/style.ts
@@ -1,6 +1,10 @@
 import styled, { css } from 'styled-components';
 
 export const StyledTabGroup = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+
   ${({ visible }: { visible: boolean }) => css`
     visibility: ${visible ? 'visible' : 'hidden'};
     opacity: ${visible ? 1 : 0};
