a7a69372a9280c182cc9b88a44358932da732533
Sential <sentialx@gmail.com>
2018-12-26 19:48:58 +0100

    Add windows controls, fix some styles

--- src/renderer/app/index.ts
+++ src/renderer/app/index.ts
@@ -1,8 +1,36 @@
 import './scss/index.scss';
 import { TabsStore } from './tabs';
+import { platform } from 'os';
+import { closeWindow, maximizeWindow, minimizeWindow } from './utils';
+import { Tab } from './models';
 
 export class App {
   public tabs = new TabsStore();
+
+  public windowsButtons = document.getElementById('windows-buttons');
+  public windowsCloseButton = document.getElementById('window-close');
+  public windowsMaximizeButton = document.getElementById('window-maximize');
+  public windowsMinimizeButton = document.getElementById('window-minimize');
+
+  constructor() {
+    if (platform() === 'darwin') {
+      this.windowsButtons.style.display = 'none';
+    }
+
+    this.windowsCloseButton.onclick = () => {
+      closeWindow();
+    };
+
+    this.windowsMaximizeButton.onclick = () => {
+      maximizeWindow();
+    };
+
+    this.windowsMinimizeButton.onclick = () => {
+      minimizeWindow();
+    };
+
+    new Tab(true);
+  }
 
   public mouse = {
     x: 0,
