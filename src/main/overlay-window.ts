a4cd07ff4e2dc19843e5a12b79c321148334d93b
Sential <sentialx@gmail.com>
2019-02-11 00:55:36 +0100

    Add basic overlay window

--- src/main/overlay-window.ts
+++ src/main/overlay-window.ts
@@ -1,7 +1,52 @@
-import { BrowserWindow } from 'electron';
+import { BrowserWindow, app, ipcMain } from 'electron';
+import { platform } from 'os';
+import { resolve, join } from 'path';
+import { appWindow } from '.';
 
 export class OverlayWindow {
   public window: BrowserWindow;
 
-  constructor() {}
+  constructor() {
+    this.createWindow();
+
+    ipcMain.on('show-overlay', () => {
+      this.window.setBounds(appWindow.window.getContentBounds());
+      this.window.focus();
+    });
+
+    ipcMain.on('hide-overlay', () => {
+      this.window.setSize(0, 0);
+      appWindow.window.focus();
+    });
+  }
+
+  public createWindow() {
+    const windowData: Electron.BrowserWindowConstructorOptions = {
+      frame: false,
+      width: 0,
+      height: 0,
+      show: true,
+      parent: appWindow.window,
+      transparent: true,
+      skipTaskbar: true,
+      thickFrame: false,
+      titleBarStyle: 'hiddenInset',
+      webPreferences: {
+        plugins: true,
+        nodeIntegration: true,
+      },
+      icon: resolve(app.getAppPath(), 'static/app-icons/icon.png'),
+    };
+
+    this.window = new BrowserWindow(windowData);
+
+    if (process.env.ENV === 'dev') {
+      this.window.webContents.openDevTools({ mode: 'detach' });
+      this.window.loadURL('http://localhost:8080/overlay.html');
+    } else {
+      this.window.loadURL(
+        join('file://', app.getAppPath(), 'build/overlay.html'),
+      );
+    }
+  }
 }
