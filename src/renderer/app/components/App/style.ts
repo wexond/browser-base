1b8dd1f7b0f101ba829b7f25cc7b2bea08198abb
Sential <sentialx@gmail.com>
2019-02-11 16:23:24 +0100

    Add tab groups section to the Bottom Sheet
    Former-commit-id: 3490066180014db1331b2104a267d2833a657c1d [formerly ec97a98340f0fb7e4e83def59bb1513e7d1e060c]
    Former-commit-id: da25cbab4b91a9d22219dd9190f160b56101e6a3

--- src/renderer/app/components/App/style.ts
+++ src/renderer/app/components/App/style.ts
@@ -6,20 +6,3 @@ export const Line = styled.div`
   height: 1px;
   width: 100%;
 `;
-
-export const Overlay = styled.div`
-  background-color: rgba(0, 0, 0, 0.6);
-  color: white;
-  position: absolute;
-  left: 0;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  z-index: 9999;
-  transition: 0.2s opacity;
-
-  ${({ visible }: { visible: boolean }) => css`
-    opacity: ${visible ? 1 : 0};
-    pointer-events: ${visible ? 'auto' : 'none'};
-  `};
-`;
