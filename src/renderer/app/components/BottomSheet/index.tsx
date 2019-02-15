9b5f21838796ea0636156be5b92cdcac8ae8e30c
Sential <sentialx@gmail.com>
2019-02-15 17:18:48 +0100

    Use different BottomSheet scrolling technique
    Former-commit-id: 0aaf6e306fe82cf220c529c5ab4c1c5c88799aa7 [formerly efafe9793fda66cb5e11e129f8a4fa961a88df61]
    Former-commit-id: b3d9a1484e788903b43953367c755a7c3be28394
    Former-commit-id: 17d5fed8ae33e6de20964bc532b21a67603b177d

--- src/renderer/app/components/BottomSheet/index.tsx
+++ src/renderer/app/components/BottomSheet/index.tsx
@@ -5,32 +5,17 @@ interface Props {
   visible?: boolean;
   children?: any;
   onClick?: any;
-  bottom?: number;
   innerRef?: any;
-  transition?: boolean;
 }
 
 export const BottomSheet = ({
   visible,
   children,
-  bottom,
   onClick,
   innerRef,
-  transition,
 }: Props) => {
-  const transform = `translate(-50%, ${
-    visible ? `calc(100% - ${bottom}px)` : '100%'
-  })`;
-
   return (
-    <StyledBottomSheet
-      onClick={onClick}
-      style={{
-        transform,
-        transition: transition ? '0.2s transform' : '',
-      }}
-      ref={innerRef}
-    >
+    <StyledBottomSheet onClick={onClick} ref={innerRef}>
       <SmallBar />
       {children}
     </StyledBottomSheet>
