3a023b80f3c99c02ed16ecf5b81200ea8c0852da
Sential <sentialx@gmail.com>
2019-02-16 19:20:57 +0100

    Add suggestions logic
    Former-commit-id: 80dbe7fedcc46b06a07241035dd246df00706135 [formerly 5248efa6aafc2269a15a88b92d6f477a7b5aab20]
    Former-commit-id: e8a6cd9d708de81eed3723238b26359c64d9784b

--- src/renderer/app/models/index.ts
+++ src/renderer/app/models/index.ts
@@ -2,3 +2,4 @@ export * from './tab';
 export * from './tab-group';
 export * from './favicon';
 export * from './history-item';
+export * from './suggestion';
---
+++ src/renderer/app/store/suggestions.ts
@@ -0,0 +1,98 @@
+import { observable } from 'mobx';
+import { getHistorySuggestions, getSearchSuggestions } from '../utils';
+import store from '.';
+import { icons } from '../constants';
+import { isURL } from '~/shared/utils/url';
+import { Suggestion } from '../models';
+
+let searchSuggestions: Suggestion[] = [];
+
+export class SuggestionsStore {
+  @observable
+  public suggestions: Suggestion[] = [];
+
+  @observable
+  public selected = 0;
+
+  public load(input: HTMLInputElement) {
+    return new Promise(async (resolve: (result: string) => void, reject) => {
+      const filter = input.value.substring(0, input.selectionStart);
+      const history = getHistorySuggestions(filter);
+
+      const historySuggestions: Suggestion[] = [];
+
+      if ((!history[0] || !history[0].canSuggest) && filter.trim() !== '') {
+        historySuggestions.unshift({
+          primaryText: filter,
+          secondaryText: 'search in Google',
+          favicon: icons.search,
+          isSearch: true,
+        });
+        if (isURL(filter)) {
+          historySuggestions.unshift({
+            primaryText: filter,
+            secondaryText: 'open website',
+            favicon: icons.page,
+          });
+        }
+      }
+
+      for (const item of history) {
+        if (!item.isSearch) {
+          historySuggestions.push({
+            primaryText: item.url,
+            secondaryText: item.title,
+            favicon: store.faviconsStore.favicons[item.favicon],
+            canSuggest: item.canSuggest,
+          });
+        } else {
+          historySuggestions.push({
+            primaryText: item.url,
+            secondaryText: 'search in Google',
+            favicon: icons.search,
+            canSuggest: item.canSuggest,
+          });
+        }
+      }
+
+      let suggestions: Suggestion[] =
+        input.value === ''
+          ? []
+          : historySuggestions.concat(searchSuggestions).slice(0, 6);
+
+      for (let i = 0; i < suggestions.length; i++) {
+        suggestions[i].id = i;
+      }
+
+      this.suggestions = suggestions;
+
+      if (historySuggestions.length > 0 && historySuggestions[0].canSuggest) {
+        resolve(historySuggestions[0].primaryText);
+      }
+
+      const searchData = await getSearchSuggestions(filter);
+
+      if (input.value.substring(0, input.selectionStart) === filter) {
+        searchSuggestions = [];
+        for (const item of searchData) {
+          searchSuggestions.push({
+            primaryText: item,
+            favicon: icons.search,
+            isSearch: true,
+          });
+        }
+
+        suggestions =
+          input.value === ''
+            ? []
+            : historySuggestions.concat(searchSuggestions).slice(0, 6);
+
+        for (let i = 0; i < suggestions.length; i++) {
+          suggestions[i].id = i;
+        }
+
+        this.suggestions = suggestions;
+      }
+    });
+  }
+}
