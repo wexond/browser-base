e0e25570f8a26b1507dd095a92074d1d4b995f78
Sential <sentialx@gmail.com>
2019-02-16 21:16:37 +0100

    Add suggestions
    Former-commit-id: 92c2a702d38d6c66f8d8a0cda97b08ee18e9eaea [formerly ba57570039b9bf26d6e8e1a398590b4b7ec029da]
    Former-commit-id: 44059c4c1127aecfe989697cb351cd35fa187cd7

---
+++ src/renderer/app/components/Suggestions/index.tsx
@@ -0,0 +1,24 @@
+import { observer } from 'mobx-react';
+import * as React from 'react';
+
+import { StyledSuggestions } from './style';
+import store from '../../store';
+import { SuggestionComponent } from '../Suggestion';
+
+interface Props {
+  visible: boolean;
+}
+
+const onMouseDown = (e: React.MouseEvent) => {
+  e.stopPropagation();
+};
+
+export const Suggestions = observer(({ visible }: Props) => {
+  return (
+    <StyledSuggestions visible={visible} onMouseDown={onMouseDown}>
+      {store.suggestionsStore.suggestions.map(suggestion => (
+        <SuggestionComponent suggestion={suggestion} key={suggestion.id} />
+      ))}
+    </StyledSuggestions>
+  );
+});
