e0e22fe84ad9adb9a33a4e7b80ef35a7802ecf6f
Sential <sentialx@gmail.com>
2019-02-16 14:39:09 +0100

    Navigate on search box enter key press
    Former-commit-id: a47d2229ea9e353e405604ffc2ccae0864c04385 [formerly 22c48869401592ae856e227327a014fdaf3fa7ae]
    Former-commit-id: d6cc8e30a3dc08fabf43f1cbb2144d3c3bc62f0b

---
+++ src/shared/utils/url.ts
@@ -0,0 +1,8 @@
+export const isURL = (input: string): boolean => {
+  const pattern = /^(?:\w+:)?\/\/([^\s.]+\.\S{2}|localhost[:?\d]*)\S*$/;
+
+  if (pattern.test(input)) {
+    return true;
+  }
+  return pattern.test(`http://${input}`);
+};
