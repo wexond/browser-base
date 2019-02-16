dbf46995b0c4bc5cfa6b233316c075dde23e39a5
Sential <sentialx@gmail.com>
2019-02-16 20:05:01 +0100

    Separate SearchBox to another component
    Former-commit-id: 31aba82821a65bc952b79a7b042eb870366853d9 [formerly 1815f491ea3c67c651e098deae83480e9a45c967]
    Former-commit-id: a6d53ee4fec942228fdcad5236de09c398fae467
    Former-commit-id: 69f99a80d640533c7bc6ea592e9889602f7bb46f

---
+++ src/renderer/app/components/SearchBox/style.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import { centerImage } from '~/shared/mixins';
+import { icons } from '../../constants';
+
+export const StyledSearchBox = styled.div`
+  position: absolute;
+  top: 15%;
+  left: 50%;
+  width: 700px;
+  z-index: 2;
+  background-color: white;
+  border-radius: 30px;
+  transform: translateX(-50%);
+  display: flex;
+  flex-flow: column;
+`;
+
+export const SearchIcon = styled.div`
+  ${centerImage('16px', '16px')};
+  background-image: url(${icons.search});
+  height: 16px;
+  min-width: 16px;
+  margin-left: 16px;
+`;
+
+export const Input = styled.input`
+  height: 100%;
+  flex: 1;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  color: black;
+  margin-left: 16px;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  height: 42px;
+`;
