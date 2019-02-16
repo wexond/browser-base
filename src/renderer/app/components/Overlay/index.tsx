465ce0fdabec15b3f5488eb5ec7b6ce91e6a0baf
Sential <sentialx@gmail.com>
2019-02-16 20:05:01 +0100

    Separate SearchBox to another component

--- src/renderer/app/components/Overlay/index.tsx
+++ src/renderer/app/components/Overlay/index.tsx
@@ -8,7 +8,6 @@ import {
   StyledTabGroups,
   AddTabGroup,
   HeaderArrow,
-  SelectedIndicator,
   Separator,
   Section,
   Menu,
@@ -16,18 +15,13 @@ import {
   Title,
   Icon,
   Scrollable,
-  SearchBox,
-  Input,
-  SearchIcon,
-  InputContainer,
   TabGroupContent,
   TabGroupClose,
 } from './style';
 import { BottomSheet } from '../BottomSheet';
 import { colors } from '~/renderer/constants';
 import { TweenLite } from 'gsap';
-import { callBrowserViewMethod } from '~/shared/utils/browser-view';
-import { isURL } from '~/shared/utils/url';
+import { SearchBox } from '../SearchBox';
 
 const Header = ({ children }: any) => {
   return (
@@ -73,113 +67,65 @@ const MenuItem = ({ children }: any) => {
   );
 };
 
-@observer
-export class Overlay extends React.Component {
-  onWheel = (e: React.WheelEvent<HTMLDivElement>) => {
-    const target = e.currentTarget;
-    const bsHeight = store.overlayStore.bsRef.getBoundingClientRect().height;
+const onWheel = (e: React.WheelEvent<HTMLDivElement>) => {
+  const target = e.currentTarget;
+  const bsHeight = store.overlayStore.bsRef.getBoundingClientRect().height;
 
-    if (bsHeight <= window.innerHeight) {
-      if (e.deltaY > 0) {
-        if (target.scrollTop === 0) {
-          TweenLite.to(target, 0.2, {
-            scrollTop: bsHeight - 275,
-          });
-        }
-      } else {
-        if (target.scrollTop === target.scrollHeight - window.innerHeight) {
-          TweenLite.to(target, 0.2, {
-            scrollTop: 0,
-          });
-        }
+  if (bsHeight <= window.innerHeight) {
+    if (e.deltaY > 0) {
+      if (target.scrollTop === 0) {
+        TweenLite.to(target, 0.2, {
+          scrollTop: bsHeight - 275,
+        });
+      }
+    } else {
+      if (target.scrollTop === target.scrollHeight - window.innerHeight) {
+        TweenLite.to(target, 0.2, {
+          scrollTop: 0,
+        });
       }
     }
-  };
-
-  onClick = () => {
-    store.overlayStore.visible = false;
-  };
-
-  onBsClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.stopPropagation();
-  };
-
-  onSearchClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.stopPropagation();
-  };
-
-  public onKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.which === 13) {
-      // Enter.
-      const tab = store.tabsStore.selectedTab;
-
-      e.preventDefault();
-
-      const text = e.currentTarget.value;
-      let url = text;
-
-      if (isURL(text) && !text.includes('://')) {
-        url = `http://${text}`;
-      } else if (!text.includes('://')) {
-        url = `https://www.google.com/search?q=${text}`;
-      }
-
-      store.overlayStore.inputRef.current.value = url;
-      tab.url = url;
-      callBrowserViewMethod(tab.id, 'loadURL', url);
-
-      store.overlayStore.visible = false;
-    }
-  };
-
-  public onInputFocus = () => {
-    store.overlayStore.inputRef.current.select();
-  };
-
-  render() {
-    return (
-      <StyledOverlay
-        visible={store.overlayStore.visible}
-        onClick={this.onClick}
-      >
-        <SearchBox onClick={this.onSearchClick}>
-          <InputContainer>
-            <SearchIcon />
-            <Input
-              placeholder="Search or type in URL"
-              onKeyPress={this.onKeyPress}
-              onFocus={this.onInputFocus}
-              ref={store.overlayStore.inputRef}
-            />
-          </InputContainer>
-        </SearchBox>
-        <Scrollable onWheel={this.onWheel} ref={store.overlayStore.scrollRef}>
-          <BottomSheet
-            visible={store.overlayStore.visible}
-            onClick={this.onBsClick}
-            innerRef={(r: any) => (store.overlayStore.bsRef = r)}
-          >
-            <Section>
-              <Header>Tab groups</Header>
-              <TabGroups />
-            </Section>
-            <Separator />
-            <Section>
-              <Menu>
-                <MenuItem>History</MenuItem>
-                <MenuItem>Bookmarks</MenuItem>
-                <MenuItem>Downloads</MenuItem>
-                <MenuItem>Settings</MenuItem>
-                <MenuItem>Extensions</MenuItem>
-                <MenuItem>New window</MenuItem>
-                <MenuItem>New incognito window</MenuItem>
-                <MenuItem>Find</MenuItem>
-                <MenuItem>More tools</MenuItem>
-              </Menu>
-            </Section>
-          </BottomSheet>
-        </Scrollable>
-      </StyledOverlay>
-    );
   }
-}
+};
+
+const onClick = () => {
+  store.overlayStore.visible = false;
+};
+
+const onBsClick = (e: React.MouseEvent<HTMLDivElement>) => {
+  e.stopPropagation();
+};
+
+export const Overlay = observer(() => {
+  return (
+    <StyledOverlay visible={store.overlayStore.visible} onClick={onClick}>
+      <SearchBox />
+      <Scrollable onWheel={onWheel} ref={store.overlayStore.scrollRef}>
+        <BottomSheet
+          visible={store.overlayStore.visible}
+          onClick={onBsClick}
+          innerRef={(r: any) => (store.overlayStore.bsRef = r)}
+        >
+          <Section>
+            <Header>Tab groups</Header>
+            <TabGroups />
+          </Section>
+          <Separator />
+          <Section>
+            <Menu>
+              <MenuItem>History</MenuItem>
+              <MenuItem>Bookmarks</MenuItem>
+              <MenuItem>Downloads</MenuItem>
+              <MenuItem>Settings</MenuItem>
+              <MenuItem>Extensions</MenuItem>
+              <MenuItem>New window</MenuItem>
+              <MenuItem>New incognito window</MenuItem>
+              <MenuItem>Find</MenuItem>
+              <MenuItem>More tools</MenuItem>
+            </Menu>
+          </Section>
+        </BottomSheet>
+      </Scrollable>
+    </StyledOverlay>
+  );
+});
