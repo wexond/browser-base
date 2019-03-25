0eaef8b6afd47dd6b88dec147870342c779be94a
Sential <sentialx@gmail.com>
2019-03-25 16:12:38 +0100

    refactor: webRequest service

--- src/main/services/web-request.ts
+++ src/main/services/web-request.ts
@@ -6,6 +6,18 @@ import { USER_AGENT } from '~/shared/constants';
 
 const eventListeners: any = {};
 
+const getTabByWebContentsId = (window: AppWindow, id: number) => {
+  for (const key in window.viewManager.views) {
+    const view = window.viewManager.views[key];
+
+    if (view.webContents.id === id) {
+      return view.tabId;
+    }
+  }
+
+  return -1;
+};
+
 const getRequestType = (type: string): any => {
   if (type === 'mainFrame') return 'main_frame';
   if (type === 'subFrame') return 'sub_frame';
@@ -13,16 +25,21 @@ const getRequestType = (type: string): any => {
   return type;
 };
 
-const getDetails = (details: any) => {
-  return {
+const getDetails = (details: any, window: AppWindow, isTabRelated: boolean) => {
+  const newDetails = {
+    ...details,
     requestId: details.id.toString(),
-    url: details.url,
-    method: details.method,
     frameId: 0,
     parentFrameId: -1,
     type: getRequestType(details.resourceType),
     timeStamp: Date.now(),
+    tabId: isTabRelated
+      ? getTabByWebContentsId(window, details.webContentsId)
+      : -1,
+    error: '',
   };
+
+  return newDetails;
 };
 
 const objectToArray = (obj: any): any[] => {
@@ -67,23 +84,64 @@ const interceptRequest = (
   eventName: string,
   details: any,
   callback: any = null,
-): boolean => {
+) => {
   let isIntercepted = false;
 
-  if (Array.isArray(eventListeners[eventName])) {
-    for (const event of eventListeners[eventName]) {
-      if (!matchesFilter(event.filters, details.url)) continue;
+  const defaultRes = {
+    cancel: false,
+    requestHeaders: details.requestHeaders,
+    responseHeaders: details.responseHeaders,
+  };
 
+  const cb = getCallback(callback);
+
+  if (Array.isArray(eventListeners[eventName]) && callback) {
+    for (const event of eventListeners[eventName]) {
+      if (!matchesFilter(event.filters, details.url)) {
+        continue;
+      }
       const id = makeId(32);
 
-      if (callback) {
-        ipcMain.once(
-          `api-webRequest-response-${eventName}-${event.id}-${id}`,
-          (e: any, res: any) => {
-            callback(res);
-          },
-        );
-      }
+      ipcMain.once(
+        `api-webRequest-response-${eventName}-${event.id}-${id}`,
+        (e: any, res: any) => {
+          if (res) {
+            if (res.cancel) {
+              return cb({ cancel: true });
+            }
+
+            if (res.redirectURL) {
+              return cb({
+                cancel: false,
+                redirectURL: res.redirectUrl,
+              });
+            }
+
+            if (
+              res.requestHeaders &&
+              (eventName === 'onBeforeSendHeaders' ||
+                eventName === 'onSendHeaders')
+            ) {
+              const requestHeaders = arrayToObject(res.requestHeaders);
+              return cb({ cancel: false, requestHeaders });
+            }
+
+            if (res.responseHeaders) {
+              const responseHeaders = {
+                ...details.responseHeaders,
+                ...arrayToObject(res.responseHeaders),
+              };
+
+              return cb({
+                responseHeaders,
+                cancel: false,
+              });
+            }
+          }
+
+          cb(defaultRes);
+        },
+      );
 
       const contents = webContents.fromId(event.webContentsId);
       contents.send(
@@ -96,181 +154,68 @@ const interceptRequest = (
     }
   }
 
-  return isIntercepted;
-};
-
-const getTabByWebContentsId = (window: AppWindow, id: number) => {
-  for (const key in window.viewManager.views) {
-    const view = window.viewManager.views[key];
-
-    if (view.webContents.id === id) {
-      return view.tabId;
-    }
+  if (!isIntercepted && callback) {
+    cb(defaultRes);
   }
-
-  return -1;
 };
 
 export const runWebRequestService = (window: AppWindow) => {
   const webviewRequest = session.fromPartition('persist:view').webRequest;
-  const defaultRequest = session.defaultSession.webRequest;
-
-  session
-    .fromPartition('persist:wexond_extension')
-    .webRequest.onBeforeSendHeaders((details: any, callback: any) => {
-      details.requestHeaders['User-Agent'] = USER_AGENT;
-      callback({ requestHeaders: details.requestHeaders, cancel: false });
-    });
 
   // onBeforeSendHeaders
 
-  const onBeforeSendHeaders = async (
-    details: any,
-    callback: any,
-    isTabRelated: boolean,
-  ) => {
+  const onBeforeSendHeaders = async (details: any, callback: any) => {
     const requestHeaders = objectToArray(details.requestHeaders);
 
     const newDetails: any = {
-      ...getDetails(details),
-      tabId: isTabRelated
-        ? getTabByWebContentsId(window, details.webContentsId)
-        : -1,
+      ...getDetails(details, window, true),
       requestHeaders,
     };
 
-    const cb = getCallback(callback);
-
-    const isIntercepted = interceptRequest(
-      'onBeforeSendHeaders',
-      newDetails,
-      (res: any) => {
-        if (res) {
-          if (res.cancel) {
-            cb({ cancel: true });
-          } else if (res.requestHeaders) {
-            const requestHeaders = arrayToObject(res.requestHeaders);
-            cb({ requestHeaders, cancel: false });
-          }
-        }
-        cb({ cancel: false, requestHeaders: details.requestHeaders });
-      },
-    );
-
-    if (!isIntercepted) {
-      cb({ cancel: false, requestHeaders: details.requestHeaders });
-    }
+    interceptRequest('onBeforeSendHeaders', newDetails, callback);
   };
 
   webviewRequest.onBeforeSendHeaders(async (details: any, callback: any) => {
     details.requestHeaders['User-Agent'] = USER_AGENT;
     details.requestHeaders['DNT'] = '1';
 
-    await onBeforeSendHeaders(details, callback, true);
+    await onBeforeSendHeaders(details, callback);
   });
 
   // onBeforeRequest
 
-  const onBeforeRequest = async (
-    details: any,
-    callback: any,
-    isTabRelated: boolean,
-  ) => {
-    const newDetails: any = {
-      ...getDetails(details),
-      tabId: isTabRelated
-        ? getTabByWebContentsId(window, details.webContentsId)
-        : -1,
-    };
-    const cb = getCallback(callback);
-
-    const isIntercepted = interceptRequest(
-      'onBeforeRequest',
-      newDetails,
-      (res: any) => {
-        if (res) {
-          if (res.cancel) {
-            cb({ cancel: true });
-          } else if (res.redirectUrl) {
-            cb({ cancel: false, redirectURL: res.redirectUrl });
-          }
-        }
-        cb({ cancel: false });
-      },
-    );
-
-    if (!isIntercepted) {
-      cb({ cancel: false });
-    }
+  const onBeforeRequest = async (details: any, callback: any) => {
+    const newDetails: any = getDetails(details, window, true);
+    interceptRequest('onBeforeRequest', newDetails, callback);
   };
 
   webviewRequest.onBeforeRequest(async (details: any, callback: any) => {
-    await onBeforeRequest(details, callback, true);
+    await onBeforeRequest(details, callback);
   });
 
   // onHeadersReceived
 
-  const onHeadersReceived = async (
-    details: any,
-    callback: any,
-    isTabRelated: boolean,
-  ) => {
+  const onHeadersReceived = async (details: any, callback: any) => {
     const responseHeaders = objectToArray(details.responseHeaders);
 
     const newDetails: any = {
-      ...getDetails(details),
-      tabId: isTabRelated
-        ? getTabByWebContentsId(window, details.webContentsId)
-        : -1,
+      ...getDetails(details, window, true),
       responseHeaders,
-      statusLine: details.statusLine,
-      statusCode: details.statusCode,
     };
 
-    const cb = getCallback(callback);
-
-    const isIntercepted = interceptRequest(
-      'onHeadersReceived',
-      newDetails,
-      (res: any) => {
-        if (res) {
-          if (res.cancel) {
-            cb({ cancel: true });
-          } else if (res.responseHeaders) {
-            const responseHeaders = Object.assign(
-              {},
-              details.responseHeaders,
-              arrayToObject(res.responseHeaders),
-            );
-
-            cb({
-              responseHeaders,
-              cancel: false,
-            });
-          }
-        }
-        cb({ cancel: false, responseHeaders: details.responseHeaders });
-      },
-    );
-
-    if (!isIntercepted) {
-      cb({ cancel: false, responseHeaders: details.responseHeaders });
-    }
+    interceptRequest('onHeadersReceived', newDetails, callback);
   };
 
   webviewRequest.onHeadersReceived(async (details: any, callback: any) => {
-    await onHeadersReceived(details, callback, true);
+    await onHeadersReceived(details, callback);
   });
 
   // onSendHeaders
 
-  const onSendHeaders = async (details: any, isTabRelated: boolean) => {
+  const onSendHeaders = async (details: any) => {
     const requestHeaders = objectToArray(details.requestHeaders);
     const newDetails: any = {
-      ...getDetails(details),
-      tabId: isTabRelated
-        ? getTabByWebContentsId(window, details.webContentsId)
-        : -1,
+      ...getDetails(details, window, true),
       requestHeaders,
     };
 
@@ -278,47 +223,29 @@ export const runWebRequestService = (window: AppWindow) => {
   };
 
   webviewRequest.onSendHeaders(async (details: any) => {
-    await onSendHeaders(details, true);
+    await onSendHeaders(details);
   });
 
   // onCompleted
 
-  const onCompleted = async (details: any, isTabRelated: boolean) => {
-    const newDetails: any = {
-      ...getDetails(details),
-      tabId: isTabRelated
-        ? getTabByWebContentsId(window, details.webContentsId)
-        : -1,
-      statusLine: details.statusLine,
-      statusCode: details.statusCode,
-      fromCache: details.fromCache,
-      error: '',
-    };
-
+  const onCompleted = async (details: any) => {
+    const newDetails: any = getDetails(details, window, true);
     interceptRequest('onCompleted', newDetails);
   };
 
   webviewRequest.onCompleted(async (details: any) => {
-    await onCompleted(details, true);
+    await onCompleted(details);
   });
 
   // onErrorOccurred
 
-  const onErrorOccurred = async (details: any, isTabRelated: boolean) => {
-    const newDetails: any = {
-      ...getDetails(details),
-      tabId: isTabRelated
-        ? getTabByWebContentsId(window, details.webContentsId)
-        : -1,
-      fromCache: details.fromCache,
-      error: details.error,
-    };
-
+  const onErrorOccurred = async (details: any) => {
+    const newDetails: any = getDetails(details, window, true);
     interceptRequest('onErrorOccurred', newDetails);
   };
 
   webviewRequest.onErrorOccurred(async (details: any) => {
-    await onErrorOccurred(details, true);
+    await onErrorOccurred(details);
   });
 
   // Handle listener add and remove.
