40e7d49d0c568375d5296188c56807d42f9f463d
Sential <sentialx@gmail.com>
2019-03-25 14:05:50 +0100

    fix: navigation buttons positioning
    Former-commit-id: 7c06ca069656f06522e1efe1c7cba9658dc42884 [formerly dc13b89ce74a888f77ff1852e13dec11b699fc3e]
    Former-commit-id: cdb173176f84938f1e89bcff2bfdffebe358ba6d

--- src/renderer/app/components/NavigationButtons/style.ts
+++ src/renderer/app/components/NavigationButtons/style.ts
@@ -5,6 +5,6 @@ export const StyledContainer = styled.div`
   display: flex;
   -webkit-app-region: no-drag;
   ${({ isFullscreen }: { isFullscreen: boolean }) => css`
-    margin-left: ${platform() === 'darwin' && !isFullscreen ? 72 : 0}px;
+    margin-left: ${platform() === 'darwin' && !isFullscreen ? 68 : 0}px;
   `};
 `;
