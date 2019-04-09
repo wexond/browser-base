292688119b54d0a9f404690d9d3ff5aa3419399a
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-04-09 23:45:32 +0200

    Add months and days list

---
+++ src/renderer/constants/dictionary.ts
@@ -0,0 +1,24 @@
+export const MonthsList = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+export const DaysList = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
