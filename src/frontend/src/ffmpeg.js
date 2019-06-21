817c6b581e904b14897762ab3899ea41c1a57f21
Denis Hamann <dh@taktik.com>
2019-06-21 12:39:53 +0200

    feat(player): support audio play without ffplay

--- src/frontend/src/ffmpeg.js
+++ src/frontend/src/ffmpeg.js
@@ -13,7 +13,7 @@ function mapstreams(videoStream,audioStream,subtitleStream){
     // return (subtitleStream>-1)?['-map 0:'+videoStream, '-map 0:'+audioStream]: ' -filter_complex "[0:v][0:s:1]overlay[v]" -map "[v]" -map 0:a:0'
 }
 
-function initializeCmd(input){
+function initializeVideoCmd(input){
      return ffmpeg()
             .input(input)
             .inputOptions('-probesize 700k')
@@ -22,6 +22,10 @@ function initializeCmd(input){
             .outputOptions('-g 30')
             .outputOptions('-tune zerolatency')
             // .outputOptions('-flush_packets -1')
+}
+function initializeAudioCmd(input){
+    return ffmpeg()
+        .input(input)
 }
 function _appendCmdWithoutSubtitle(ffmpegCmd,videoStream,inputvideoCodec,audioStream){
     let newCmd
@@ -54,12 +58,12 @@ function _appendCmdWithSubtitle(ffmpegCmd,videoStream,audioStream,subtitleStream
     // ffmpegCmd.outputOptions(['-filter_complex "[0:v][0:'+subtitleStream+']overlay[v]" -map "[v]"',' -map 0:'+audioStream])
 }
 
-function getMpegtsPipeline(input, audioStream,videoStream, inputvideoCodec,subtitleStream,isDeinterlacingEnabled,handleConversionError,handleErroneousStreamError) {
+function getVideoMpegtsPipeline(input, audioStream,videoStream, inputvideoCodec,subtitleStream,isDeinterlacingEnabled,handleConversionError,handleErroneousStreamError) {
     if((process.platform === 'darwin')){
         input += '?fifo_size=13160&overrun_nonfatal=1&buffer_size=/18800&pkt_size=188'
     }
     console.log('run url: ',input)
-    let ffmpegCmd = initializeCmd(input)
+    let ffmpegCmd = initializeVideoCmd(input)
 
     if (subtitleStream && subtitleStream>-1){
         console.log('-------- _appendCmdWithSubtitle')
@@ -102,4 +106,28 @@ function getMpegtsPipeline(input, audioStream,videoStream, inputvideoCodec,subti
             })
 }
 
-exports.getMpegtsPipeline = getMpegtsPipeline
+function getAudioMpegtsPipeline(input, handleConversionError, handleErroneousStreamError) {
+    if (process.platform === 'darwin') {
+        // input += '?fifo_size=13160&overrun_nonfatal=1&buffer_size=/18800&pkt_size=188'
+    }
+    console.log('run url: ', input)
+    const ffmpegCmd = initializeAudioCmd(input)
+    return ffmpegCmd.format('mp3')
+        .on('start', commandLine => {
+            console.log('Spawned Ffmpeg with command: ' + commandLine)
+        })
+        .on('error', (err, stdout, stderr) => {
+            console.log('_________err' + new Date(), err, stdout, stderr)
+            if (stderr.indexOf('Conversion failed') > - 1) {
+                handleConversionError()
+            } else if (stderr.indexOf('Stream specifier') > - 1) {
+                handleErroneousStreamError()
+            }
+        })
+        .on('stderr', (stderr) => {
+            console.log(stderr)
+        })
+}
+
+exports.getVideoMpegtsPipeline = getVideoMpegtsPipeline
+exports.getAudioMpegtsPipeline = getAudioMpegtsPipeline
