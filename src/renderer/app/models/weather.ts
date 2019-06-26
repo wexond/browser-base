59a113c1570964e73e090bbd5f61b49674312190
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-06-26 17:51:37 +0200

    Add more weather models

--- src/renderer/app/models/weather.ts
+++ src/renderer/app/models/weather.ts
@@ -6,13 +6,48 @@ export interface ForecastRequest {
 
 export interface ForecastItem {
   name?: string;
-  dayTemp: number;
+  dayTemp?: number;
   nightTemp?: number;
-  description: string;
-  weather: string;
+  description?: string;
+  weather?: string;
 }
 
 export interface Forecast {
-  today: ForecastItem;
-  items: ForecastItem[],
+  today?: ForecastItem;
+  items?: ForecastItem[];
 }
+
+export type WeatherCondition = 'clear' | 'few-clouds' | 'rain' | 'showers' | 'snow' | 'storm';
+
+/* tslint:disable */
+export interface OpenWeatherItem {
+  clouds: {
+    all: number;
+  },
+  dt: number;
+  dt_txt: string;
+  main: {
+    grnd_level: number;
+    humidity: number;
+    pressure: number;
+    sea_level: number;
+    temp: number;
+    temp_kf: number;
+    temp_max: number;
+    temp_min: number;
+  },
+  sys: {
+    pod: 'd' | 'n';
+  }
+  weather: {
+    description: string;
+    icon: string;
+    id: number;
+    main: WeatherCondition
+  }[],
+  wind: {
+    deg: number;
+    speed: number;
+  }
+}
+/* tslint:enable */
