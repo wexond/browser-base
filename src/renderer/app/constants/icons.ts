7587996e62e4279d537e097c54e6dab236b18ef7
Sential <sentialx@gmail.com>
2019-06-26 12:24:05 +0200

    chore: remove unused icons

--- src/renderer/app/constants/icons.ts
+++ src/renderer/app/constants/icons.ts
@@ -1,6 +1,3 @@
-import windowsClose from '~/shared/resources/icons/windows/close.svg';
-import windowsMaximize from '~/shared/resources/icons/windows/maximize.svg';
-import windowsMinimize from '~/shared/resources/icons/windows/minimize.svg';
 import add from '~/shared/resources/icons/add.svg';
 import back from '~/shared/resources/icons/back.svg';
 import forward from '~/shared/resources/icons/forward.svg';
@@ -30,9 +27,6 @@ import night from '~/shared/resources/icons/night.svg';
 import fewClouds from '~/shared/resources/icons/weather/few-clouds.png';
 
 export const icons = {
-  windowsClose,
-  windowsMaximize,
-  windowsMinimize,
   add,
   back,
   forward,
