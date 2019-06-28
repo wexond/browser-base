c100456d7f1b921720cdf5373b00547827c8e9bd
Sential <sentialx@gmail.com>
2019-06-28 19:28:42 +0200

    refactor: improve directory structure

--- src/renderer/views/permissions/index.tsx
+++ src/renderer/views/permissions/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import { App } from './components/App';
-import { fonts } from '../constants';
+import { fonts } from '../../constants';
 import { ipcRenderer } from 'electron';
 
 ipcRenderer.setMaxListeners(0);
