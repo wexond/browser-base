df04968f95a9cbcf251205ffbba75a571b974c8b
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-06-30 17:39:38 +0200

    Add DropdownItem

--- src/renderer/components/Dropdown/styles.ts
+++ src/renderer/components/Dropdown/styles.ts
@@ -110,3 +110,26 @@ export const Value = styled.div`
   color: rgba(0, 0, 0, ${transparency.text.high});
   ${centerVertical()};
 `;
+
+export const DropdownItem = styled.div`
+  width: 100%;
+  height: 32px;
+  padding-left: 16px;
+  padding-right: 8px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  color: #000;
+  cursor: pointer;
+
+  ${({ selected }: { selected: boolean }) => css`
+    background-color: ${selected ? 'rgba(0, 0, 0, 0.04)' : '#fff'};
+  `}
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.04);
+  }
+`;
