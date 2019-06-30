6d880896329ef9adcb32b7f94237f02d0b811824
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-06-30 17:34:10 +0200

    Add coloredCursor mixin

--- src/renderer/mixins/cursors.ts
+++ src/renderer/mixins/cursors.ts
@@ -1,3 +1,5 @@
+import { transparency } from '../constants';
+
 export const pointer = () => `
     user-select: none;
     cursor: pointer;
@@ -7,3 +9,9 @@ export const defaultCursor = () => `
     user-select: none;
     cursor: default;
   `;
+
+export const coloredCursor = (cursorColor: string, color = 0) => `
+  -webkit-text-fill-color: transparent;
+  text-shadow: 0px 0px 0px rgba(${color}, ${color}, ${color}, ${transparency.text.high});
+  color: ${cursorColor};
+`;
