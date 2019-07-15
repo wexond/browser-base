f5cf78af00daba03c379b7ca352159fd3841bd2d
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-07-15 20:37:16 +0200

    feat: add radiobutton component

---
+++ src/renderer/components/RadioButtons/index.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+
+interface Props {
+  defaultValue?: any;
+  children?: any;
+}
+
+interface State {
+  selectedItem: any;
+}
+
+export default class RadioButtons extends React.PureComponent<Props, State> {
+  public state: State = {
+    selectedItem: null,
+  };
+
+  public onSelect = (value: any) => {
+    this.value = value;
+  };
+
+  public get value() {
+    const { defaultValue } = this.props;
+    const { selectedItem } = this.state;
+    return selectedItem || defaultValue;
+  }
+
+  public set value(val: any) {
+    this.setState({ selectedItem: val });
+  }
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <>
+        {React.Children.map(children, child => {
+          return React.cloneElement(child, {
+            selected: this.value === child.props.value,
+            onSelect: this.onSelect,
+          });
+        })}
+      </>
+    );
+  }
+}
