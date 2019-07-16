682ee744c33f1447271f16d32d6f112ce77161a4
Sential <sentialx@gmail.com>
2019-07-16 18:54:42 +0200

    fix: bookmark items hover colors

--- src/renderer/views/app/components/Overlay/components/ListItem/index.ts
+++ src/renderer/views/app/components/Overlay/components/ListItem/index.ts
@@ -13,12 +13,12 @@ export const ListItem = styled.div`
     background-color: ${selected
       ? theme['overlay.foreground'] === 'light'
         ? 'rgba(255, 255, 255, 0.12)'
-        : 'rgba(0, 0, 0, 0.04)'
+        : 'rgba(0, 0, 0, 0.08)'
       : 'transparent'};
 
     &:hover {
       background-color: ${theme['overlay.foreground'] === 'light'
-        ? `rgba(255, 255, 255, ${selected ? 0.15 : 0.08})`
+        ? `rgba(255, 255, 255, ${selected ? 0.12 : 0.08})`
         : `rgba(0, 0, 0, ${selected ? 0.08 : 0.04})`};
     }
   `};
