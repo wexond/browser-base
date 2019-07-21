5d96ba8ae98180038fdce2ebbfa4167628091d48
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-07-22 00:15:40 +0200

    small code clean up in form fill

--- src/preloads/constants/form-fill.ts
+++ src/preloads/constants/form-fill.ts
@@ -1,4 +1,4 @@
-export const formInputFilters = {
+export const formFieldFilters = {
   type: /text|email|password/i,
   name: /login|username|email|password|name|fname|mname|lname|phone|mobile|address|city|country/i,
 }
