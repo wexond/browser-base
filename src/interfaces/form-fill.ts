a87a166850525efab27d0c185b791ba71502b6cd
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-07-21 23:58:24 +0200

    feat: form field inserting

--- src/interfaces/form-fill.ts
+++ src/interfaces/form-fill.ts
@@ -1,20 +1,16 @@
 export interface IFormFillData {
   _id?: string;
   type: 'password' | 'address';
-  fields?: IFormFillPassword | IFormFillAddress;
-}
-
-export interface IFormFillPassword {
-  username?: string;
-  password?: string;
-  url?: string;
-}
-
-export interface IFormFillAddress {
-  name?: string;
-  address?: string;
-  postCode?: string;
-  city?: string;
-  phone?: string;
-  email?: string;
+  fields?: {
+    username?: string;
+    password?: string;
+    url?: string;
+    name?: string;
+    address?: string;
+    postCode?: string;
+    city?: string;
+    phone?: string;
+    email?: string;
+    country?: string;
+  }
 }
--- src/preloads/utils/form-fill.ts
+++ src/preloads/utils/form-fill.ts
@@ -1,5 +1,6 @@
 import { searchElements, isVisible } from './dom';
 import { formInputFilters } from '../constants';
+import { IFormFillData } from '~/interfaces';
 
 export const getFormInputs = (form: HTMLFormElement) => {
   const formId = form.getAttribute('id');
@@ -16,4 +17,56 @@ export const filterFormInputs = (...inputs: HTMLElement[] | HTMLInputElement[]) 
 
     return isVisible(input) && formInputFilters.type.test(type) && formInputFilters.name.test(name);
   }) as HTMLInputElement[];
+}
+
+export const insertData = (input: HTMLInputElement, data: IFormFillData) => {
+  const { fields } = data;
+  const name = input.getAttribute('name').toLowerCase().trim();
+  const value = getData(name, data);
+
+  if (value) {
+    input.value = value;
+  }
+}
+
+const getData = (name: string, data: IFormFillData) => {
+  const { fields } = data;
+  const fullName = (fields.name || '').split(' ');
+
+  try {
+    switch (name) {
+      case 'username':
+        return fields.username;
+      case 'login':
+        return fields.username;
+      case 'email':
+        return fields.email;
+      case 'password':
+        return fields.password;
+      case 'name': // full name
+        return fields.name;
+      case 'fname': // first name
+        return fullName[0];
+      case 'mname':  // middle name
+        return fullName.length >= 3 && fullName[fullName.length - 2];
+      case 'lname':// last name
+        return fullName[fullName.length - 1];
+      case 'address':
+        return fields.address;
+      case 'city':
+        return fields.city;
+      case 'postal':
+        return fields.postCode;
+      case 'country':
+        return fields.country;
+      case 'phone':
+        return fields.phone;
+      case 'mobile':
+        return fields.phone;
+    }
+  } catch (err) {
+
+  }
+
+  return null;
 }
