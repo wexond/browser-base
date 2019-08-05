99fec0862e5ba227bf381969eefb1cb20eb68087
Sential <sentialx@gmail.com>
2019-08-05 22:47:43 +0200

    fix: fonts loading

--- src/renderer/constants/fonts.ts
+++ src/renderer/constants/fonts.ts
@@ -1,9 +1,5 @@
-import robotoRegular from '~/renderer/resources/fonts/roboto-regular.woff2';
-import robotoMedium from '~/renderer/resources/fonts/roboto-medium.woff2';
-import robotoLight from '~/renderer/resources/fonts/roboto-light.woff2';
-
 export const fonts = {
-  robotoRegular,
-  robotoMedium,
-  robotoLight,
+  robotoRegular: require('~/renderer/resources/fonts/roboto-regular.woff2'),
+  robotoMedium: require('~/renderer/resources/fonts/roboto-medium.woff2'),
+  robotoLight: require('~/renderer/resources/fonts/roboto-light.woff2'),
 };
