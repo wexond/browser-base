c73c1017d3a1341a03e430e1c079f2281f7da11e
Sential <sentialx@gmail.com>
2019-08-06 21:54:34 +0200

    fix: small fixes

--- src/renderer/views/app/components/Toolbar/AddressBar/style.ts
+++ src/renderer/views/app/components/Toolbar/AddressBar/style.ts
@@ -16,7 +16,7 @@ export const StyledAddressBar = styled.input`
   background-color: rgba(0, 0, 0, 0.03);
   border: none;
   width: 100%;
-  height: 26px;
+  height: 28px;
   max-width: 1200px;
   border-radius: 4px;
   overflow: hidden;
