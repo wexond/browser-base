72865ad0bf32615b7f96248ae5cdf530e1961255
Sential <sentialx@gmail.com>
2019-08-07 14:54:53 +0200

    feat: add more search engines

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -11,6 +11,26 @@ export const DEFAULT_SETTINGS = {
       url: 'https://www.google.com/search?q=%s',
       keywordsUrl: 'http://google.com/complete/search?client=chrome&q=%s',
     },
+    {
+      name: 'Bing',
+      url: 'https://www.bing.com/search?q=%s',
+      keywordsUrl: 'http://google.com/complete/search?client=chrome&q=%s',
+    },
+    {
+      name: 'Ecosia',
+      url: 'https://www.ecosia.org/search?q=%s',
+      keywordsUrl: 'http://google.com/complete/search?client=chrome&q=%s',
+    },
+    {
+      name: 'DuckDuckGo',
+      url: 'https://duckduckgo.com/?q=%s',
+      keywordsUrl: 'http://google.com/complete/search?client=chrome&q=%s',
+    },
+    {
+      name: 'Yahoo!',
+      url: 'https://search.yahoo.com/search?p=%s',
+      keywordsUrl: 'http://google.com/complete/search?client=chrome&q=%s',
+    },
   ],
   searchEngine: 0,
   startupBehavior: {
