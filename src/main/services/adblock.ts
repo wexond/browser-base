7c7009465a5e448faabfbe807627ffae238c151a
Rémi <remusao@users.noreply.github.com>
2019-08-16 17:23:20 +0200

    chore: update @cliqz/adblocker to latest (#293)

--- src/main/services/adblock.ts
+++ src/main/services/adblock.ts
@@ -1,120 +1,65 @@
 import { Session } from 'electron';
-import { existsSync, readFile, writeFile } from 'fs';
+import { existsSync, promises as fs } from 'fs';
 import { resolve } from 'path';
-import { windowsManager } from '..';
-import Axios from 'axios';
+import fetch from 'node-fetch';
 
-import { FiltersEngine, Request, RequestType } from '@cliqz/adblocker';
+import { windowsManager } from '..';
+import { ElectronBlocker, Request } from '@cliqz/adblocker-electron';
 import { getPath } from '~/utils';
 
-const lists: { [key: string]: string } = {
-  easylist: 'https://easylist.to/easylist/easylist.txt',
-  easyprivacy: 'https://easylist.to/easylist/easyprivacy.txt',
-
-  'plowe-0':
-    'https://pgl.yoyo.org/adservers/serverlist.php?hostformat=adblockplus&showintro=0&mimetype=plaintext',
-
-  'ublock-filters':
-    'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt',
-  'ublock-badware':
-    'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt',
-  'ublock-privacy':
-    'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt',
-  'ublock-unbreak':
-    'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/unbreak.txt',
-  'ublock-abuse':
-    'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resource-abuse.txt',
-};
-
-export let engine: FiltersEngine;
+export let engine: ElectronBlocker;
 
 const loadFilters = async () => {
   const path = resolve(getPath('adblock/cache.dat'));
 
-  const downloadFilters = () => {
-    const ops = [];
+  const downloadFilters = async () => {
+    // Load lists to perform ads and tracking blocking:
+    //
+    //  - https://easylist.to/easylist/easylist.txt
+    //  - https://pgl.yoyo.org/adservers/serverlist.php?hostformat=adblockplus&showintro=1&mimetype=plaintext
+    //  - https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resource-abuse.txt
+    //  - https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt
+    //  - https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt
+    //  - https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/unbreak.txt
+    //
+    //  - https://easylist.to/easylist/easyprivacy.txt
+    //  - https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt
+    engine = await ElectronBlocker.fromPrebuiltAdsAndTracking(fetch);
 
-    for (const key in lists) {
-      ops.push(Axios.get(lists[key]));
+    try {
+      await fs.writeFile(path, engine.serialize());
+    } catch (err) {
+      if (err) return console.error(err);
     }
-
-    Axios.all(ops).then(async res => {
-      let data = '';
-
-      for (const res1 of res) {
-        data += res1.data;
-      }
-
-      engine = FiltersEngine.parse(data);
-
-      const resources = (await Axios.get(
-        'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resources.txt',
-      )).data;
-
-      engine.updateResources(resources, `${resources.length}`);
-
-      writeFile(path, engine.serialize(), err => {
-        if (err) return console.error(err);
-      });
-    });
   };
 
   if (existsSync(path)) {
-    readFile(resolve(path), (err, buffer) => {
-      if (err) return console.error(err);
+    try {
+      const buffer = await fs.readFile(resolve(path));
 
       try {
-        engine = FiltersEngine.deserialize(buffer);
+        engine = ElectronBlocker.deserialize(buffer);
       } catch (e) {
-        downloadFilters();
+        return downloadFilters();
       }
-
-      /*const { networkFilters, cosmeticFilters } = parseFilters(
-        data,
-        engine.config,
-      );
-      engine.update({
-        newNetworkFilters: networkFilters,
-        newCosmeticFilters: cosmeticFilters,
-      });*/
-    });
+    } catch (err) {
+      return console.error(err);
+    }
   } else {
-    downloadFilters();
+    return downloadFilters();
   }
 };
 
 export const runAdblockService = (ses: Session) => {
-  const { webRequest } = ses;
+  const emitBlockedEvent = (request: Request) => {
+    for (const window of windowsManager.list) {
+      window.webContents.send(`blocked-ad-${request.tabId}`);
+    }
+  };
 
-  loadFilters();
-
-  webRequest.onBeforeRequest(
-    { urls: ['<all_urls>'] },
-    async (details: Electron.OnBeforeRequestDetails, callback) => {
-      if (engine && windowsManager.settings.object.shield) {
-        const { match, redirect } = engine.match(
-          Request.fromRawDetails({
-            type: details.resourceType as RequestType,
-            url: details.url,
-          }),
-        );
-
-        if (match || redirect) {
-          if (redirect) {
-            callback({ redirectURL: redirect.dataUrl });
-          } else {
-            callback({ cancel: true });
-          }
-
-          for (const window of windowsManager.list) {
-            window.webContents.send(`blocked-ad-${details.webContentsId}`);
-          }
-
-          return;
-        }
-      }
-
-      callback({ cancel: false });
-    },
-  );
+  loadFilters().then(() => {
+    engine.enableBlockingInSession(ses);
+    engine.on('request-blocked', emitBlockedEvent);
+    engine.on('request-redirected', emitBlockedEvent);
+  });
 };
