6929efc36a21271615c803b7cc1971abc03bcf7f
Sential <sentialx@gmail.com>
2019-08-29 16:25:27 +0200

    fix: compile script

--- scripts/compile.js
+++ scripts/compile.js
@@ -3,20 +3,12 @@ const { platform } = require('os');
 
 const os = platform();
 
-let npm = os === 'win32' ? 'npm.cmd' : 'npm';
+const npm = os === 'win32' ? 'npm.cmd' : 'npm';
 
 const runScript = script => {
   spawn(npm, ['run', script], { cwd: __dirname, stdio: 'inherit' });
 };
 
-const { TRAVIS_COMMIT_MESSAGE } = process.env;
-
-if (
-  (TRAVIS_COMMIT_MESSAGE &&
-    TRAVIS_COMMIT_MESSAGE.startsWith('chore: bump version')) ||
-  !TRAVIS_COMMIT_MESSAGE
-) {
-  if (os === 'win32') runScript('compile-win32');
-  else if (os === 'darwin') runScript('compile-darwin');
-  else if (os === 'linux') runScript('compile-linux');
-}
+if (os === 'win32') runScript('compile-win32');
+else if (os === 'darwin') runScript('compile-darwin');
+else if (os === 'linux') runScript('compile-linux');
