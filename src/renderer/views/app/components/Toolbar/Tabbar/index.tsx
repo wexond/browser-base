a0915901b69a5abd9edf0479a45a2ffb063addeb
Sential <sentialx@gmail.com>
2019-08-30 20:00:15 +0200

    chore: bring horizontal scrollbar back

--- src/renderer/views/app/components/Toolbar/Tabbar/index.tsx
+++ src/renderer/views/app/components/Toolbar/Tabbar/index.tsx
@@ -5,6 +5,9 @@ import { AddTab, StyledTabbar, TabsContainer } from './style';
 import { Tabs } from '../Tabs';
 import store from '../../../store';
 import { icons } from '~/renderer/constants';
+import HorizontalScrollbar from '~/renderer/components/HorizontalScrollbar';
+
+const getContainer = () => store.tabs.containerRef.current;
 
 const onMouseEnter = () => (store.tabs.scrollbarVisible = true);
 
@@ -28,6 +31,12 @@ export const Tabbar = observer(() => {
         icon={icons.add}
         onClick={onAddTabClick}
         divRef={(r: any) => (store.addTab.ref = r)}
+      />
+      <HorizontalScrollbar
+        ref={store.tabs.scrollbarRef}
+        enabled={store.tabs.scrollable}
+        visible={store.tabs.scrollbarVisible}
+        getContainer={getContainer}
       />
     </StyledTabbar>
   );
