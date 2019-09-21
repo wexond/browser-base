5c4106b86ff15451871673fb808f77ba72423fb7
Sential <sentialx@gmail.com>
2019-09-21 14:54:07 +0200

    feat: revert history saving

--- src/renderer/views/app/store/history.ts
+++ src/renderer/views/app/store/history.ts
@@ -1,16 +1,7 @@
-import { observable, computed, action } from 'mobx';
+import { observable } from 'mobx';
 
-import { IHistoryItem, IHistorySection } from '~/interfaces';
-import { compareDates, getSectionLabel } from '../utils';
+import { IHistoryItem } from '~/interfaces';
 import { Database } from '~/models/database';
-
-export type QuickRange =
-  | 'all'
-  | 'today'
-  | 'yesterday'
-  | 'last-week'
-  | 'last-month'
-  | 'older';
 
 export class HistoryStore {
   public db = new Database<IHistoryItem>('history');
@@ -18,38 +9,8 @@ export class HistoryStore {
   @observable
   public items: IHistoryItem[] = [];
 
-  @observable
-  public itemsLoaded = this.getDefaultLoaded();
-
-  @observable
-  public selectedRange: QuickRange = 'all';
-
-  @observable
-  public searched = '';
-
-  @observable
-  public selectedItems: string[] = [];
-
-  public constructor() {
-    this.load();
-  }
-
-  public resetLoadedItems(): void {
-    this.itemsLoaded = this.getDefaultLoaded();
-  }
-
   public getById(id: string): IHistoryItem {
     return this.items.find(x => x._id === id);
-  }
-
-  public async load() {
-    const items = await this.db.get({});
-
-    items.sort(
-      (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime(),
-    );
-
-    this.items = items;
   }
 
   public async addItem(item: IHistoryItem) {
@@ -57,125 +18,5 @@ export class HistoryStore {
     item._id = doc._id;
     this.items.push(item);
     return doc._id;
-  }
-
-  public clear() {
-    this.items = [];
-    this.db.remove({}, true);
-  }
-
-  public removeItem(id: string) {
-    this.items = this.items.filter(x => x._id !== id);
-    this.db.remove({ _id: id });
-  }
-
-  @computed
-  public get sections() {
-    const list: IHistorySection[] = [];
-    let section: IHistorySection;
-    let loaded = 0;
-
-    for (let i = this.items.length - 1; i >= 0; i--) {
-      if (loaded > this.itemsLoaded) break;
-
-      const item = this.items[i];
-      const date = new Date(item.date);
-
-      if (
-        this.searched !== '' &&
-        !item.title.toLowerCase().includes(this.searched) &&
-        !item.url.includes(this.searched)
-      ) {
-        continue;
-      }
-
-      if (this.range) {
-        if (date.getTime() >= this.range.max) continue;
-        if (date.getTime() <= this.range.min) break;
-      }
-
-      if (compareDates(section && section.date, date)) {
-        section.items.push(item);
-      } else {
-        section = {
-          label: getSectionLabel(date),
-          items: [item],
-          date,
-        };
-        list.push(section);
-      }
-
-      loaded++;
-    }
-
-    return list;
-  }
-
-  @computed
-  public get range() {
-    const current = new Date();
-    const day = current.getDate();
-    const month = current.getMonth();
-    const year = current.getFullYear();
-
-    let minDate: Date;
-    let maxDate: Date;
-
-    switch (this.selectedRange) {
-      case 'today': {
-        minDate = new Date(year, month, day, 0, 0, 0, 0);
-        maxDate = new Date(year, month, day, 23, 59, 59, 999);
-        break;
-      }
-      case 'yesterday': {
-        minDate = new Date(year, month, day - 1, 0, 0, 0, 0);
-        maxDate = new Date(year, month, day - 1, 23, 59, 59, 999);
-        break;
-      }
-      case 'last-week': {
-        let currentDay = current.getDay() - 1;
-        if (currentDay === -1) currentDay = 6;
-        minDate = new Date(year, month, day - currentDay - 7, 0, 0, 0, 0);
-        maxDate = new Date(year, month, day - currentDay - 1, 0, 0, 0, 0);
-        break;
-      }
-      case 'last-month': {
-        minDate = new Date(year, month - 1, 1, 0, 0, 0, 0);
-        maxDate = new Date(year, month, 0, 0, 0, 0, 0);
-        break;
-      }
-      case 'older': {
-        let currentDay = current.getDay() - 1;
-        if (currentDay === -1) currentDay = 6;
-        minDate = new Date(0);
-        maxDate = new Date(year, month, day - currentDay - 7, 0, 0, 0, 0);
-        break;
-      }
-    }
-
-    return (
-      this.selectedRange !== 'all' && {
-        min: minDate.getTime(),
-        max: maxDate.getTime(),
-      }
-    );
-  }
-
-  @action
-  public search(str: string) {
-    this.searched = str.toLowerCase().toLowerCase();
-    this.itemsLoaded = this.getDefaultLoaded();
-  }
-
-  public getDefaultLoaded() {
-    return Math.floor(window.innerHeight / 56);
-  }
-
-  @action
-  public deleteSelected() {
-    for (const item of this.selectedItems) {
-      this.removeItem(item);
-    }
-    this.selectedItems = [];
   }
 }
