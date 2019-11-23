f88ba7ad2183fac1d940b49d6c77377d8afaabd6
Sential <sentialx@gmail.com>
2019-11-23 19:08:23 +0100

    feat: add scrollbar in downloads dialog

--- src/renderer/views/downloads/components/App/style.ts
+++ src/renderer/views/downloads/components/App/style.ts
@@ -5,16 +5,39 @@ export const StyledApp = styled.div`
   margin: 8px;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
   border-radius: 8px;
-  overflow: hidden;
+  overflow: overlay;
   position: relative;
   transition: 0.2s opacity, 0.2s margin-top;
   padding: 8px;
   font-size: 13px;
+  max-height: 512px;
 
-  ${({ visible, theme }: { visible: boolean; theme?: ITheme }) => css`
-    opacity: ${visible ? 1 : 0};
-    margin-top: ${visible ? 0 : 7}px;
-    background-color: ${theme['dialog.backgroundColor']};
-    color: ${theme['dialog.lightForeground'] ? 'white' : 'black'};
-  `}
+  &::-webkit-scrollbar {
+    width: 6px;
+    -webkit-app-region: no-drag;
+    background-color: transparent;
+  }
+
+  
+    ${({ theme, visible }: { theme?: ITheme; visible: boolean }) => css`
+      &:hover {
+        &::-webkit-scrollbar-thumb {
+          background-color: ${theme['dialog.lightForeground']
+            ? 'rgba(255, 255, 255, 0.2)'
+            : 'rgba(0, 0, 0, 0.2)'};
+
+          &:hover {
+            background-color: ${theme['dialog.lightForeground']
+              ? 'rgba(255, 255, 255, 0.3)'
+              : 'rgba(0, 0, 0, 0.3)'};
+          }
+        }
+      }
+
+      opacity: ${visible ? 1 : 0};
+      margin-top: ${visible ? 0 : 7}px;
+      background-color: ${theme['dialog.backgroundColor']};
+      color: ${theme['dialog.lightForeground'] ? 'white' : 'black'};
+    `};
+  }
 `;
