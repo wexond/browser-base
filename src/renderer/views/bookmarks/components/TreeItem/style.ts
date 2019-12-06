d6137e1af6006871c6b5bb5779b2e47d29fba2c5
Sential <sentialx@gmail.com>
2019-12-06 17:35:21 +0100

    feat: better theme integration in tree

--- src/renderer/views/bookmarks/components/TreeItem/style.ts
+++ src/renderer/views/bookmarks/components/TreeItem/style.ts
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components';
 
 import { centerIcon } from '~/renderer/mixins';
 import { icons, transparency } from '~/renderer/constants';
+import { ITheme } from '~/interfaces';
 
 export const StyledTreeItem = styled.div`
   width: 100%;
@@ -10,9 +11,14 @@ export const StyledTreeItem = styled.div`
   margin-top: 4px;
   align-items: center;
   cursor: pointer;
-  &:hover {
-    background-color: rgba(0, 0, 0, 0.04);
-  }
+
+  ${({ theme }: { theme?: ITheme }) => css`
+    &:hover {
+      background-color: ${theme['pages.lightForeground']
+        ? 'rgba(255, 255, 255, 0.04)'
+        : 'rgba(0, 0, 0, 0.04)'};
+    }
+  `}
 `;
 
 export const DropIcon = styled.div`
@@ -22,9 +28,18 @@ export const DropIcon = styled.div`
   background-image: url(${icons.dropDown});
   border-radius: 100%;
   ${centerIcon(20)};
-  ${({ visible, expanded }: { visible: boolean; expanded: boolean }) => css`
+  ${({
+    visible,
+    expanded,
+    theme,
+  }: {
+    visible: boolean;
+    expanded: boolean;
+    theme?: ITheme;
+  }) => css`
     opacity: ${visible ? transparency.icons.inactive : 0};
     transform: ${expanded ? 'rotate(-90deg)' : 'rotate(0deg)'};
+    filter: ${theme['pages.lightForeground'] ? 'invert(100%)' : ''};
   `}
 `;
 
@@ -34,6 +49,10 @@ export const FolderIcon = styled.div`
   opacity: ${transparency.icons.inactive};
   background-image: url(${icons.folder});
   ${centerIcon(20)}
+
+  ${({ theme }: { theme?: ITheme }) => css`
+    filter: ${theme['pages.lightForeground'] ? 'invert(100%)' : ''};
+  `}
 `;
 
 export const Label = styled.div`
