547471e68c5f69ad2dee150ace119843b7bf9138
sentialx <sentialx@gmail.com>
2020-03-06 19:27:34 +0100

    fix: showing downloads dialog

--- src/renderer/views/downloads/components/App/style.ts
+++ src/renderer/views/downloads/components/App/style.ts
@@ -1,18 +1,13 @@
 import styled, { css } from 'styled-components';
 import { ITheme } from '~/interfaces';
+import { DialogStyle } from '~/renderer/mixins/dialogs';
 
-// TODO(sentialx): extend DialogStyle
-export const StyledApp = styled.div`
-  margin: 8px;
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  border-radius: 6px;
+export const StyledApp = styled(DialogStyle)`
   overflow: overlay;
-  position: relative;
-  transition: 0.2s opacity, 0.2s margin-top;
   padding: 8px;
   font-size: 13px;
 
-  ${({ theme, visible }: { theme?: ITheme; visible: boolean }) => css`
+  ${({ theme }: { theme?: ITheme; visible: boolean }) => css`
     &::-webkit-scrollbar {
       width: 6px;
       -webkit-app-region: no-drag;
@@ -30,10 +25,6 @@ export const StyledApp = styled.div`
           : 'rgba(0, 0, 0, 0.3)'};
       }
     }
-
-    opacity: ${visible ? 1 : 0};
-    margin-top: ${visible ? 3 : 10}px;
-    background-color: ${theme['dialog.backgroundColor']};
     color: ${theme['dialog.lightForeground'] ? 'white' : 'black'};
   `};
 `;
