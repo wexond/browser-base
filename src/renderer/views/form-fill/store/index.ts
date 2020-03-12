ecf471640aaf05a1dbaaf2c5b6d4348311506b4d
sentialx <sentialx@gmail.com>
2020-03-12 13:59:37 +0100

    fix: formfill error

--- src/renderer/views/form-fill/store/index.ts
+++ src/renderer/views/form-fill/store/index.ts
@@ -2,14 +2,11 @@ import { ipcRenderer } from 'electron';
 import { observable } from 'mobx';
 
 import { IFormFillMenuItem } from '~/interfaces';
-import { getCurrentWindow } from '../../app/utils/windows';
 import { DialogStore } from '~/models/dialog-store';
 
 export class Store extends DialogStore {
   @observable
   public items: IFormFillMenuItem[] = [];
-
-  public windowId: number = getCurrentWindow().id;
 
   public constructor() {
     super({ hideOnBlur: false });
