0db59854dc6b66e63a9a5a64a27e66b230d1c143
sentialx <sentialx@gmail.com>
2020-03-15 14:47:27 +0100

    fix: hiding tab preview

--- src/renderer/views/app/components/Toolbar/Tab/index.tsx
+++ src/renderer/views/app/components/Toolbar/Tab/index.tsx
@@ -30,12 +30,13 @@ const onCloseMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
 const onMouseDown = (tab: ITab) => (e: React.MouseEvent<HTMLDivElement>) => {
   const { pageX, button } = e;
 
+  ipcRenderer.send(`hide-tab-preview-${store.windowId}`);
+
   if (button !== 0) return;
 
   if (!tab.isSelected) {
     tab.select();
   } else {
-    ipcRenderer.send(`hide-tab-preview-${store.windowId}`);
     store.canToggleMenu = true;
   }
 
