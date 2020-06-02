e83318d15140f9884301d6c70c074a2070241785
xnerhu <xnerhu@gmail.com>
2020-06-02 18:11:05 +0200

    feat: implement get and getChildren methods in b ookmark service

--- src/storage/loaders/communication.ts
+++ src/storage/loaders/communication.ts
@@ -1,7 +1,18 @@
 import { parentPort } from 'worker_threads';
 
+import { IStorageMessage } from '~/interfaces';
+import BookmarkService from '../services/bookmark';
+
 export default () => {
-  parentPort.on('message', (message: string) => {
-    console.log(message);
+  parentPort.on('message', (message: IStorageMessage) => {
+    const type = message?.type;
+
+    console.log(type);
+
+    if (type === 'bookmarks-get-children') {
+      const res = BookmarkService.get(['6', '2']);
+
+      console.log(res);
+    }
   });
 };
