b276a799ca7bc9c6af74c87e2da8fd5566fdb34f
xnerhu <xnerhu@gmail.com>
2020-06-02 14:37:34 +0200

    feat: storage basics

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -1,3 +1,14 @@
 import * as Database from 'better-sqlite3';
 
-const history = new Database('history.db', { verbose: console.log });
+const db = new Database('storage.db', { verbose: console.log });
+
+(async () => {
+  console.log('Storage loaded');
+
+  const query = db.prepare('SELECT * from history');
+  const items = query.run();
+
+  console.log(items);
+})();
+
+console.log('XDDDDDDDDDDDDDDDDDDDDDDDDDD');
