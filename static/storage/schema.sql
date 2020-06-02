462a17f2f81d454c7065682638c9e3924cfa0507
xnerhu <xnerhu@gmail.com>
2020-06-02 16:40:50 +0200

    feat: update db schema

--- static/storage/schema.sql
+++ static/storage/schema.sql
@@ -1,57 +1,23 @@
--- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: wexond
--- ------------------------------------------------------
--- Server version	8.0.19
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
+-- File generated with SQLiteStudio v3.2.1 on Tue Jun 2 16:33:50 2020
 --
--- Table structure for table `bookmarks`
+-- Text encoding used: UTF-8
 --
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS `bookmarks`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `bookmarks` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `parentId` int unsigned DEFAULT NULL,
-  `index` int unsigned DEFAULT '0',
-  `url` text,
-  `title` text NOT NULL,
-  `dateAdded` double DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `parentId` (`parentId`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- Table: bookmarks
+CREATE TABLE bookmarks (
+    id        INTEGER PRIMARY KEY ASC AUTOINCREMENT
+                      UNIQUE
+                      NOT NULL,
+    parentId  INTEGER REFERENCES bookmarks (id),
+    [index]   INTEGER,
+    url       STRING,
+    title     STRING  NOT NULL,
+    dateAdded DOUBLE  NOT NULL
+);
 
---
--- Dumping data for table `bookmarks`
---
 
-LOCK TABLES `bookmarks` WRITE;
-/*!40000 ALTER TABLE `bookmarks` DISABLE KEYS */;
-INSERT INTO `bookmarks` VALUES (1,NULL,0,'https://www.facebook.com/','Facebook',1591102579520),(2,NULL,1,'https://github.com','Github',1591102763036),(3,NULL,2,'http://wexond.net/','Wexond',1591102870037),(4,NULL,3,'https://github.com/qusly/qusly','Qusly',1591102896745),(5,NULL,4,'http://wexond.net:7070/','Top 10 stron',1591102952640);
-/*!40000 ALTER TABLE `bookmarks` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2020-06-02 16:11:33
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
