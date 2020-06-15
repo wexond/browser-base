983c6de1ff0a652e823ab9fa34a72dcea89904ed
sentialx <sentialx@gmail.com>
2020-06-15 15:47:33 +0200

    fix: usage of storage service worker

--- src/browser/storage-factory.ts
+++ src/browser/storage-factory.ts
@@ -1,10 +1,11 @@
 import { IStorageScope, IStorageMessage, IStorageResponse } from '~/interfaces';
 import { makeId } from '~/common/utils/string';
 import { StorageService } from './services/storage';
+import { Application } from './application';
 
 export class StorageFactory {
   private static get worker() {
-    return StorageService.instance.worker;
+    return Application.instance.storage.worker;
   }
 
   public static create = (scope: IStorageScope) => <T>(
