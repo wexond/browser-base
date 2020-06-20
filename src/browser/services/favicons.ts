87f815d4278510ee043c703038a6a3135bd2781f
sentialx <sentialx@gmail.com>
2020-06-20 14:56:07 +0200

    Revert "refactor: remove bufferFromUint8Array"
    This reverts commit acd0533599cee075f681971e0f1b41d7868ca198.

--- src/browser/services/favicons.ts
+++ src/browser/services/favicons.ts
@@ -1,6 +1,7 @@
 import { Worker } from 'worker_threads';
 
 import { WorkerMessengerFactory } from '~/common/worker-messenger-factory';
+import { bufferFromUint8Array } from '~/common/utils/buffer';
 
 export class FaviconsService {
   private invoker = WorkerMessengerFactory.createInvoker('favicons');
@@ -10,13 +11,13 @@ export class FaviconsService {
   }
 
   public getFavicon = async (pageUrl: string) => {
-    return Buffer.from(
+    return bufferFromUint8Array(
       await this.invoker.invoke<Uint8Array>('getFavicon', pageUrl),
     );
   };
 
   public saveFavicon = async (pageUrl: string, faviconUrl: string) => {
-    return Buffer.from(
+    return bufferFromUint8Array(
       await this.invoker.invoke<Uint8Array>('saveFavicon', pageUrl, faviconUrl),
     );
   };
