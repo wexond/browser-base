a23c136aa2c45b9ac0fd9f8b89fd15c57a08787b
sentialx <sentialx@gmail.com>
2020-06-28 19:03:01 +0200

    fix: remove extension-popup entry

--- webpack.config.renderer.js
+++ webpack.config.renderer.js
@@ -5,28 +5,12 @@ const {
   getBaseConfig,
 } = require('./webpack.config.base');
 const { join } = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 /* eslint-enable */
 
 const PORT = 4444;
 
 const appConfig = getConfig(getBaseConfig('app'), {
   target: 'web',
-
-  devServer: {
-    contentBase: join(__dirname, 'build'),
-    port: PORT,
-    hot: true,
-    inline: true,
-    disableHostCheck: true,
-  },
-});
-
-const extPopupConfig = getConfig({
-  target: 'web',
-
-  entry: {},
-  output: {},
 
   devServer: {
     contentBase: join(__dirname, 'build'),
@@ -58,20 +42,4 @@ applyEntries('app', appConfig, [
   // 'bookmarks',
 ]);
 
-if (process.env.ENABLE_EXTENSIONS) {
-  extPopupConfig.entry['extension-popup'] = [
-    `./src/renderer/views/extension-popup`,
-  ];
-  extPopupConfig.plugins.push(
-    new HtmlWebpackPlugin({
-      title: 'Wexond',
-      template: 'static/pages/extension-popup.html',
-      filename: `extension-popup.html`,
-      chunks: ['extension-popup'],
-    }),
-  );
-
-  module.exports = [appConfig, extPopupConfig];
-} else {
-  module.exports = appConfig;
-}
+module.exports = appConfig;
