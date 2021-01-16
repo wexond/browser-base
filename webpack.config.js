e4747a773a2e713f54251e0b0d70582d94dd5cfb
sentialx <sentialx@gmail.com>
2021-01-16 01:48:15 +0100

    fix: building

--- webpack.config.js
+++ webpack.config.js
@@ -28,8 +28,8 @@ const mainConfig = getConfig({
           from:
             'node_modules/@cliqz/adblocker-electron-preload/dist/preload.cjs.js',
           to: 'preload.js',
-          transform: (fileContent, path) => {
-            return terser.minify(fileContent.toString()).code.toString();
+          transform: async (fileContent, path) => {
+            return (await terser.minify(fileContent.toString())).code.toString();
           },
         },
       ],
